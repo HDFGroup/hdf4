216c7faf01fec5d750b9d117fae2c7dd831582aa
sxu <sxu@hdfgroup.org>
1992-04-27 17:31:08 +0000

    Initial revision

---
+++ hdf/test/tsdnnt.c
@@ -0,0 +1,376 @@
+#ifdef RCSID
+static char RcsId[] = "@(#)$Revision$";
+#endif
+/*
+$Header$
+
+$Log$
+Revision 1.1  1992/04/27 17:31:08  sxu
+Initial revision
+
+ * Revision 1.2  1992/03/24  20:42:04  sxu
+ * Changed output file names
+ *
+ * Revision 1.1  1992/02/26  20:07:41  mfolk
+ * Initial revision
+ *
+*/
+#include "/hdf/home/sxu/3.2tar/src/hdf.h"
+#include "/hdf/home/sxu/3.2tar/src/dfsd.h"
+
+float64 f64[10][10], tf64[10][10];
+float32 f32[10][10], tf32[10][10];
+int8 i8[10][10], ti8[10][10];
+uint8 ui8[10][10], tui8[10][10];
+int16 i16[10][10], ti16[10][10];
+uint16 ui16[10][10], tui16[10][10];
+int32 i32[10][10], ti32[10][10];
+uint32 ui32[10][10], tui32[10][10];
+
+int main()
+{
+    int i, j, err;
+    int rank;
+    int dims[2];
+    int number_failed = 0;
+
+    rank = 2;
+    dims[0] = 10;
+    dims[1] = 10;
+
+    printf("Creating arrays...\n");
+
+    for (i=0; i<10; i++) {
+        for (j=0; j<10; j++) {
+
+	    f64[i][j] = (i * 10) + j;	/* range: 0 ~ 4-billion */
+
+	    f32[i][j] = (i * 10) + j;	/* range: 0 ~ 4-billion */
+
+	    i8[i][j] = (i * 10) + j;    /* range: 0 ~ 100 */
+	    ui8[i][j] = (i * 20) + j;	/* range: 0 ~ 200 */
+
+	    i16[i][j] = (i * 10) + j;	/* range: 0 ~ 30000 */
+	    ui16[i][j] = (i * 20) + j;	/* range: 0 ~ 60000 */
+
+	    i32[i][j] = (i * 10L) + j;	/* range: 0 ~ 2-billion */
+	    ui32[i][j] = (i * 20) + j;	/* range: 0 ~ 4-billion */
+	}
+    }
+
+    DFSDsetdims(rank, dims);
+
+    /* individual files */
+    printf("Testing arrays in individual files...\n");
+
+    DFSDsetNT(DFNT_NFLOAT64);
+    err = DFSDadddata("con00", rank, dims, f64);
+    printf("Write: %d     ", err);		
+    err = DFSDgetdata("con00", rank, dims, tf64);
+    HEprint(stderr, 0);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++) {
+	    if (f64[i][j] != tf64[i][j])
+	        err = 1;
+	    tf64[i][j] = 0.0;
+	}
+    if (err == 1) {
+	printf(">>> Test failed for float64 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for float64 array.\n");	
+    
+    DFSDsetNT(DFNT_NFLOAT32);
+    err = DFSDadddata("con0", rank, dims, f32);
+    printf("Write: %d     ", err);		
+    err = DFSDgetdata("con0", rank, dims, tf32);
+    HEprint(stderr, 0);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++) {
+	    if (f32[i][j] != tf32[i][j])
+	        err = 1;
+	    tf32[i][j] = 0.0;
+	}
+    if (err == 1) {
+	printf(">>> Test failed for float32 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for float32 array.\n");	
+    
+
+    DFSDsetNT(DFNT_NINT8);
+    err = DFSDadddata("con1", rank, dims, i8);
+    printf("Write: %d     ", err);
+    err = DFSDgetdata("con1", rank, dims, ti8);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++) {
+	    if (i8[i][j] != ti8[i][j])
+	        err = 1;
+	    ti8[i][j] = 0;
+	}
+    if (err == 1) {
+	printf(">>> Test failed for int8 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for int8 array.\n");
+
+    DFSDsetNT(DFNT_NUINT8);
+    err = DFSDadddata("con2", rank, dims, ui8);
+    printf("Write: %d     ", err);
+    err = DFSDgetdata("con2", rank, dims, tui8);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++) {
+	  for (j=0; j<10; j++) {
+	    if (ui8[i][j] != tui8[i][j])
+	        err = 1;
+	    tui8[i][j] = 0;
+      }
+	}
+    if (err == 1 ) {
+	printf(">>> Test failed for uint8 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for uint8 array.\n");
+
+    DFSDsetNT(DFNT_NINT16);
+    err = DFSDadddata("con3", rank, dims, i16);
+    printf("Write: %d     ", err);
+    err = DFSDgetdata("con3", rank, dims, ti16);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++) {
+	    if (i16[i][j] != ti16[i][j])
+	        err = 1;
+	    ti16[i][j] = 0;
+	}
+    if (err == 1 ) {
+	printf(">>> Test failed for int16 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for int16 array.\n");
+
+    DFSDsetNT(DFNT_NUINT16);
+    err = DFSDadddata("con4", rank, dims, ui16);
+    printf("Write: %d     ", err);
+    err = DFSDgetdata("con4", rank, dims, tui16);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++) {
+	    if (ui16[i][j] != tui16[i][j])
+	        err = 1;
+	    tui16[i][j] = 0;
+	}
+    if (err == 1 ) {
+	printf(">>> Test failed for uint16 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for uint16 array.\n");
+
+    DFSDsetNT(DFNT_NINT32);
+    err = DFSDadddata("con5", rank, dims, i32);
+    printf("Write: %d     ", err);
+    err = DFSDgetdata("con5", rank, dims, ti32);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++) {
+	    if (i32[i][j] != ti32[i][j])
+	        err = 1;
+	    ti32[i][j] = 0;
+	}
+    if (err == 1 ) {
+	printf(">>> Test failed for int32 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for int32 array.\n");
+
+    DFSDsetNT(DFNT_NUINT32);
+    err = DFSDadddata("con6", rank, dims, ui32);
+    printf("Write: %d     ", err);
+    err = DFSDgetdata("con6", rank, dims, tui32);
+    printf("Read: %d\n", err);
+    err = 0;
+    for (i=0; i<10; i++) {
+	  for (j=0; j<10; j++) {
+	    if (ui32[i][j] != tui32[i][j])
+	        err = 1;
+	    tui32[i][j] = 0;
+      }
+	}
+    if (err == 1 ) {
+	printf(">>> Test failed for uint32 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for uint32 array.\n");
+
+
+    printf("Writing arrays to single file... ");
+
+    DFSDsetNT(DFNT_NFLOAT64);
+    err = DFSDadddata("con.hdf", rank, dims, f64);
+    printf("%d  ", err);
+
+    DFSDsetNT(DFNT_NFLOAT32);
+    err = DFSDadddata("con.hdf", rank, dims, f32);
+    printf("%d  ", err);
+    DFSDsetNT(DFNT_NINT8);
+    err = DFSDadddata("con.hdf", rank, dims, i8);
+    printf("%d  ", err);
+    DFSDsetNT(DFNT_NUINT8);
+    err = DFSDadddata("con.hdf", rank, dims, ui8);
+    printf("%d  ", err);
+    DFSDsetNT(DFNT_NINT16);
+    err = DFSDadddata("con.hdf", rank, dims, i16);
+    printf("%d  ", err);
+    DFSDsetNT(DFNT_NUINT16);
+    err = DFSDadddata("con.hdf", rank, dims, ui16);
+    printf("%d  ", err);
+    DFSDsetNT(DFNT_NINT32);
+    err = DFSDadddata("con.hdf", rank, dims, i32);
+    printf("%d  ", err);
+    DFSDsetNT(DFNT_NUINT32);
+    err = DFSDadddata("con.hdf", rank, dims, ui32);
+    printf("%d\n\n", err);
+
+    printf("Reading arrays from single file... ");
+
+    err = DFSDgetdata("con.hdf", rank, dims, tf64);
+    printf("%d  ", err);
+
+    err = DFSDgetdata("con.hdf", rank, dims, tf32);
+    printf("%d  ", err);
+    err = DFSDgetdata("con.hdf", rank, dims, ti8);
+    printf("%d  ", err);
+    err = DFSDgetdata("con.hdf", rank, dims, tui8);
+    printf("%d  ", err);
+    err = DFSDgetdata("con.hdf", rank, dims, ti16);
+    printf("%d  ", err);
+    err = DFSDgetdata("con.hdf", rank, dims, tui16);
+    printf("%d  ", err);
+    err = DFSDgetdata("con.hdf", rank, dims, ti32);
+    printf("%d  ", err);
+    err = DFSDgetdata("con.hdf", rank, dims, tui32);
+    printf("%d\n", err);
+
+    printf("Checking arrays from single file...\n\n");
+    
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (f64[i][j] != tf64[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for float64 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for float64 array.\n");
+
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (f32[i][j] != tf32[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for float32 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for float32 array.\n");
+
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (i8[i][j] != ti8[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for int8 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for int8 array.\n");
+
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (ui8[i][j] != tui8[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for uint8 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for uint8 array.\n");
+
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (i16[i][j] != ti16[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for int16 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for int16 array.\n");
+
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (ui16[i][j] != tui16[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for uint16 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for uint16 array.\n");
+
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (i32[i][j] != ti32[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for int32 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for int32 array.\n");
+
+    err = 0;
+    for (i=0; i<10; i++)
+	for (j=0; j<10; j++)
+	   if (ui32[i][j] != tui32[i][j])
+	       err = 1;
+    if (err == 1 ) {
+	printf(">>> Test failed for uint32 array.\n");
+    number_failed++;
+    }
+    else
+	printf("Test passed for uint32 array.\n");
+
+    if (number_failed > 0 ) {
+	printf("\n\t>>> %d TESTS FAILED <<<\n\n", number_failed);
+    }
+    else
+	printf("\n\t>>> ALL TESTS PASSED <<<\n\n");
+
+}
---
+++ hdf/test/tsdntf.f
@@ -0,0 +1,239 @@
+C
+C $Header$
+C
+C $Log$
+C Revision 1.1  1992/04/27 17:28:04  sxu
+C Initial revision
+C
+c Revision 1.1  1992/02/29  19:32:51  mfolk
+c Initial revision
+c
+C
+      program tdfsd_ntF
+C
+C
+C  Program to test writing SDSs with different types of data.
+C
+C  Input file:  none
+C  Output files:  o1,o2, ... o5
+C
+
+      integer dspdata, dsgdata, dsadata, dssdims, dssnt
+
+      real*8 f64(10,10), tf64(10,10)
+      real*4 f32(10,10), tf32(10,10)
+      character i8(10,10), ti8(10,10)
+      integer*2 i16(10,10), ti16(10,10)
+      integer*4 i32(10,10), ti32(10,10)
+      
+      integer i, j, err, err1, err2
+      integer rank
+      integer dims(2)
+      integer number_failed
+      integer DFNT_FLOAT64, DFNT_FLOAT32, DFNT_INT8
+      integer DFNT_INT16, DFNT_INT32
+  
+      number_failed = 0
+      DFNT_FLOAT64 = 6
+      DFNT_FLOAT32 = 5
+      DFNT_INT8 = 20
+      DFNT_INT16 = 22
+      DFNT_INT32 = 24
+      rank = 2
+      dims(1) = 10
+      dims(2) = 10
+  
+      print *, 'Creating arrays...'
+  
+      do 110 i=1,10
+          do 100 j=1,10
+            f64(i,j) = (i * 10) + j
+  	    f32(i,j) = (i * 10) + j
+  	    i8(i,j) = char( (i * 10) + j )
+  	    i16(i,j) = (i * 10) + j
+  	    i32(i,j) = (i * 10) + j
+  100     continue
+  110 continue
+  
+      err = dssdims(rank, dims)
+  
+C  individual files 
+      print *,'Testing arrays in individual files...'
+  
+      err = dssnt(DFNT_FLOAT64)
+      err1 = dspdata('o1', rank, dims, f64)
+      err2 = dsgdata('o1', rank, dims, tf64)
+      print *,'Write: ', err1, '    Read: ', err2
+      err = 0
+      do 160 i=1,10
+          do 150 j=1,10
+  	    if (f64(i,j).ne.tf64(i,j)) err = 1
+  	    tf64(i,j) = 0.0
+  150     continue
+  160 continue
+
+      call err_check(err, number_failed, 'float64')
+
+      err = dssnt(DFNT_FLOAT32)
+      err1 = dspdata('o2', rank, dims, f32)
+      err2 = dsgdata('o2', rank, dims, tf32)
+      print *,'Write: ', err1, '    Read: ', err2
+      err = 0
+      do 210 i=1,10
+          do 200 j=1,10
+  	    if (f32(i,j).ne.tf32(i,j)) err = 1
+  	    tf32(i,j) = 0.0
+  200     continue
+  210 continue
+
+      call err_check(err, number_failed, 'float32')
+
+      err = dssnt(DFNT_INT8)
+      err1 = dspdata('o3', rank, dims, i8)
+      err2 = dsgdata('o3', rank, dims, ti8)
+      print *,'Write: ', err1, '    Read: ', err2
+      err = 0
+      do 310 i=1,10
+          do 300 j=1,10
+  	    if (i8(i,j).ne.ti8(i,j)) err = 1
+  	    ti8(i,j) = char(0)
+  300     continue
+  310 continue
+
+      call err_check(err, number_failed, 'int8')
+
+      err = dssnt(DFNT_INT16)
+      err1 = dspdata('o4', rank, dims, i16)
+      err2 = dsgdata('o4', rank, dims, ti16)
+      print *,'Write: ', err1, '    Read: ', err2
+      err = 0
+      do 410 i=1,10
+          do 400 j=1,10
+  	    if (i16(i,j).ne.ti16(i,j)) err = 1
+  	    ti16(i,j) = 0
+  400     continue
+  410 continue
+
+      call err_check(err, number_failed, 'int16')
+
+      err = dssnt(DFNT_INT32)
+      err1 = dspdata('o5', rank, dims, i32)
+      err2 = dsgdata('o5', rank, dims, ti32)
+      print *,'Write: ', err1, '    Read: ', err2
+      err = 0
+      do 510 i=1,10
+          do 500 j=1,10
+  	    if (i32(i,j).ne.ti32(i,j)) err = 1
+  	    ti32(i,j) = 0
+  500     continue
+  510 continue
+
+      call err_check(err, number_failed, 'int32')
+
+
+C 
+      print *, 'Writing arrays to single file.'
+      print *, 'Error values: '
+C
+      err = dssnt(DFNT_FLOAT64)
+      print *,'Add float64 returns: ',dsadata('o.hdf',rank,dims,f64)
+
+      err = dssnt(DFNT_FLOAT32)
+      print *,'Add float32 returns: ',dsadata('o.hdf',rank,dims,f32)
+
+      err = dssnt(DFNT_INT8)
+      print *, 'Add int8 returns: ', dsadata('o.hdf', rank, dims, i8)
+
+      err = dssnt(DFNT_INT16)
+      print *, 'Add int16 returns: ', dsadata('o.hdf', rank, dims, i16)
+
+      err = dssnt(DFNT_INT32)
+      print *, 'Add int32 returns: ', dsadata('o.hdf', rank, dims, i32)
+
+C 
+      print *, 'Reading arrays from single file... '
+      print *, 'Error values: '
+C
+      print *, 'Get f64 returns: ', dsgdata('o.hdf', rank, dims, tf64)
+      print *, 'Get f32 returns: ', dsgdata('o.hdf', rank, dims, tf32)
+      print *, 'Get int8 returns: ', dsgdata('o.hdf', rank, dims, ti8)
+      print *, 'Get int16 returns: ', dsgdata('o.hdf', rank, dims, ti16)
+      print *, 'Get int32 returns: ', dsgdata('o.hdf', rank, dims, ti32)
+
+C 
+      print *, 'Checking arrays from single file...\n\n'
+
+      err = 0
+      do 910 i=1,10
+         do 900 j=1,10
+           if (f64(i,j) .ne. tf64(i,j)) err = 1
+  900    continue
+  910 continue
+
+      call err_check(err, number_failed, 'float64')
+C 
+
+      err = 0
+      do 1010 i=1,10
+         do 1000 j=1,10
+           if (f32(i,j) .ne. tf32(i,j)) err = 1
+ 1000    continue
+ 1010 continue
+
+      call err_check(err, number_failed, 'float32')
+C 
+      err = 0
+      do 1110 i=1,10
+         do 1100 j=1,10
+           if (i8(i,j) .ne. ti8(i,j)) err = 1
+ 1100    continue
+ 1110 continue
+
+      call err_check(err, number_failed, 'int8')
+C 
+      err = 0
+      do 1210 i=1,10
+         do 1200 j=1,10
+           if (i16(i,j) .ne. ti16(i,j)) err = 1
+ 1200    continue
+ 1210 continue
+
+      call err_check(err, number_failed, 'int16')
+C 
+      err = 0
+      do 1310 i=1,10
+         do 1300 j=1,10
+           if (i32(i,j) .ne. ti32(i,j)) err = 1
+ 1300    continue
+ 1310 continue
+
+      call err_check(err, number_failed, 'int32')
+C 
+      print *
+      if (number_failed .gt. 0 ) then
+  	print *,'        >>> ', number_failed, ' TESTS FAILED <<<'
+      else
+  	print *,'        >>> ALL TESTS PASSED <<<'
+      endif
+      print *
+      print *
+
+      stop
+      end  
+  
+C
+C
+      subroutine err_check(err, num_fail, type)
+      integer err, num_fail
+      character*(*) type
+
+      if (err .eq. 1) then 
+  	print *,'>>> Test failed for ',type, ' array.'
+        num_fail = num_fail+1
+      else
+  	print *,'Test passed for ', type, ' array.'
+      endif
+
+      return
+      end
+
