b5dadd726b6409759aeb82f16dd6622e178cb3c4
chouck <chouck@hdfgroup.org>
1992-06-25 19:41:58 +0000

    Changed output file names

--- hdf/test/thfile1.c
+++ hdf/test/thfile1.c
@@ -5,9 +5,12 @@ static char RcsId[] = "@(#)$Revision$";
 $Header$
 
 $Log$
-Revision 1.1  1992/02/10 20:59:34  chouck
-Initial revision
+Revision 1.2  1992/06/25 19:41:58  chouck
+Changed output file names
 
+ * Revision 1.1  1992/02/10  20:59:34  chouck
+ * Initial revision
+ *
 */
 /*
   test opening files and access elements until limits are reached
@@ -15,7 +18,7 @@ Initial revision
 
 #include "hdf.h"
 #define BIG 600
-#define TESTFILE_NAME "thfile.hdf"
+#define TESTFILE_NAME "thf"
 
 int32 files[BIG];
 int32 accs[BIG];
@@ -26,7 +29,7 @@ main()
 
   puts("Opening many files of same name");
   for (i=0; i< BIG; i++) {
-    files[i] = Hopen(TESTFILE_NAME, DFACC_ALL, 0);
+    files[i] = Hopen("thf.hdf", DFACC_ALL, 0);
     if (files[i] < 0) break;
   }
   printf("Opening stopped at %d/%d files\n", i, BIG);
@@ -42,7 +45,7 @@ main()
   puts("Opening many files of different names");
   for (i=0; i< BIG; i++) {
     char fname[100];
-    sprintf(fname, "%s.%1d", TESTFILE_NAME, i);
+    sprintf(fname, "%s%1d.hdf", TESTFILE_NAME, i);
     files[i] = Hopen(fname, DFACC_ALL, 0);
     if (files[i] < 0) break;
   }
