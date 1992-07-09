c50793b0a6884a62832e1eba5c2600c738e28e8d
chouck <chouck@hdfgroup.org>
1992-07-09 19:39:35 +0000

    rank was declared twice

--- hdf/test/tsdstrf.f
+++ hdf/test/tsdstrf.f
@@ -2,9 +2,12 @@ C
 C $Header$
 C
 C $Log$
-C Revision 1.1  1992/07/08 22:15:59  sxu
-C Initial revision
+C Revision 1.2  1992/07/09 19:39:35  chouck
+C rank was declared twice
 C
+c Revision 1.1  1992/07/08  22:15:59  sxu
+c Initial revision
+c
 C
       program tsdstrf
 C
@@ -13,7 +16,7 @@ C and dimension strings.
 C To avoid the '\0' inserted by HDstrncpy, compare the first 14
 C characters of output and input strings in subroutine compare()
 
-      integer rank, rank, i, j, ret, err, num_failed
+      integer rank, i, j, ret, err, num_failed
       integer dims(2)
       integer dssnt, dssdims, dssdast, dssdist, dsadata
       integer dsgdast, dsgdist, dsgdata, DFNT_NFLOAT32
