f1ebfbc2298f6ba87a9a0fde636db85540745f21
chouck <chouck@hdfgroup.org>
1992-12-01 21:07:48 +0000

    Fixed tab / space problem so Mac Fortran doesn't complain

--- hdf/test/tsdnntf.f
+++ hdf/test/tsdnntf.f
@@ -2,9 +2,12 @@ C
 C $Header$
 C
 C $Log$
-C Revision 1.5  1992/07/09 16:09:49  chouck
-C Changed characters to bytes for VMS
+C Revision 1.6  1992/12/01 21:07:48  chouck
+C Fixed tab / space problem so Mac Fortran doesn't complain
 C
+c Revision 1.5  1992/07/09  16:09:49  chouck
+c Changed characters to bytes for VMS
+c
 c Revision 1.4  1992/06/29  15:41:39  chouck
 c Changed the OR() to an addition.  Removed bitwise assignment
 c to make VMS happy.
@@ -43,12 +46,12 @@ C   VMS you should be able to find all of the necessary changes.
 C
 
       integer dspdata, dsgdata, dsadata, dssdims, dssnt
-
+      
       real*8 f64(10,10), tf64(10,10)
       real*4 f32(10,10), tf32(10,10)
       integer*2 i16(10,10), ti16(10,10)
       integer*4 i32(10,10), ti32(10,10)
-
+      
 C  Change these to be of type 'byte' for VMS
 C      byte      i8(10,10), ti8(10,10)
       character i8(10,10), ti8(10,10)
@@ -61,14 +64,14 @@ C      byte      i8(10,10), ti8(10,10)
       integer DFNT_INT32
       integer DFNT_NFLOAT64, DFNT_NFLOAT32, DFNT_NINT8
       integer DFNT_NINT16, DFNT_NINT32, DFNT_NATIVE
-
+      
       DFNT_FLOAT64 = 6
       DFNT_FLOAT32 = 5
       DFNT_INT8 = 20
       DFNT_INT16 = 22
       DFNT_INT32 = 24
       DFNT_NATIVE = 4096
-
+      
 C These should really use a logical OR to compute these values
 C However, OR() is not really that portable
 
@@ -122,149 +125,149 @@ C  individual files
       print *,'Write: ', err1, '    Read: ', err2
       err = 0
       do 210 i=1,10
-          do 200 j=1,10
-  	    if (f32(i,j).ne.tf32(i,j)) err = 1
-  	    tf32(i,j) = 0.0
-  200     continue
-  210 continue
-
-      call err_check(err, number_failed, 'float32')
-
-      err = dssnt(DFNT_NINT8)
-      err1 = dspdata('fo3.hdf', rank, dims, i8)
-      err2 = dsgdata('fo3.hdf', rank, dims, ti8)
-      print *,'Write: ', err1, '    Read: ', err2
-      err = 0
-      do 310 i=1,10
+         do 200 j=1,10
+             if (f32(i,j).ne.tf32(i,j)) err = 1
+             tf32(i,j) = 0.0
+ 200      continue
+ 210   continue
+       
+       call err_check(err, number_failed, 'float32')
+       
+       err = dssnt(DFNT_NINT8)
+       err1 = dspdata('fo3.hdf', rank, dims, i8)
+       err2 = dsgdata('fo3.hdf', rank, dims, ti8)
+       print *,'Write: ', err1, '    Read: ', err2
+       err = 0
+       do 310 i=1,10
           do 300 j=1,10
-  	    if (i8(i,j).ne.ti8(i,j)) err = 1
-C Use the following line for VMS
-C            ti8(i,j) = 0
-            ti8(i,j) = char(0)
-  300     continue
-  310 continue
-
-      call err_check(err, number_failed, 'int8')
-
-      err = dssnt(DFNT_NINT16)
-      err1 = dspdata('fo4.hdf', rank, dims, i16)
-      err2 = dsgdata('fo4.hdf', rank, dims, ti16)
-      print *,'Write: ', err1, '    Read: ', err2
-      err = 0
-      do 410 i=1,10
+             if (i8(i,j).ne.ti8(i,j)) err = 1
+C     Use the following line for VMS
+C     ti8(i,j) = 0
+             ti8(i,j) = char(0)
+ 300      continue
+ 310   continue
+       
+       call err_check(err, number_failed, 'int8')
+       
+       err = dssnt(DFNT_NINT16)
+       err1 = dspdata('fo4.hdf', rank, dims, i16)
+       err2 = dsgdata('fo4.hdf', rank, dims, ti16)
+       print *,'Write: ', err1, '    Read: ', err2
+       err = 0
+       do 410 i=1,10
           do 400 j=1,10
-  	    if (i16(i,j).ne.ti16(i,j)) err = 1
-  	    ti16(i,j) = 0
-  400     continue
-  410 continue
-
-      call err_check(err, number_failed, 'int16')
-
-      err = dssnt(DFNT_NINT32)
-      err1 = dspdata('fo5.hdf', rank, dims, i32)
-      err2 = dsgdata('fo5.hdf', rank, dims, ti32)
-      print *,'Write: ', err1, '    Read: ', err2
-      err = 0
-      do 510 i=1,10
+             if (i16(i,j).ne.ti16(i,j)) err = 1
+             ti16(i,j) = 0
+ 400      continue
+ 410   continue
+       
+       call err_check(err, number_failed, 'int16')
+       
+       err = dssnt(DFNT_NINT32)
+       err1 = dspdata('fo5.hdf', rank, dims, i32)
+       err2 = dsgdata('fo5.hdf', rank, dims, ti32)
+       print *,'Write: ', err1, '    Read: ', err2
+       err = 0
+       do 510 i=1,10
           do 500 j=1,10
-  	    if (i32(i,j).ne.ti32(i,j)) err = 1
-  	    ti32(i,j) = 0
-  500     continue
-  510 continue
-
-      call err_check(err, number_failed, 'int32')
-
-
-C 
-      print *, 'Writing arrays to single file.'
-      print *, 'Error values: '
-C
-      err = dssnt(DFNT_NFLOAT64)
-      print *, '            ', dsadata('fo.hdf', rank, dims, f64)
-
-      err = dssnt(DFNT_NFLOAT32)
-      print *, '            ', dsadata('fo.hdf', rank, dims, f32)
-
-      err = dssnt(DFNT_NINT8)
-      print *, '            ', dsadata('fo.hdf', rank, dims, i8)
-
-      err = dssnt(DFNT_NINT16)
-      print *, '            ', dsadata('fo.hdf', rank, dims, i16)
-
-      err = dssnt(DFNT_NINT32)
-      print *, '            ', dsadata('fo.hdf', rank, dims, i32)
-
-C 
-      print *, 'Reading arrays from single file... '
-      print *, 'Error values: '
-C
-      print *, '            ', dsgdata('fo.hdf', rank, dims, tf64)
-      print *, '            ', dsgdata('fo.hdf', rank, dims, tf32)
-      print *, '            ', dsgdata('fo.hdf', rank, dims, ti8)
-      print *, '            ', dsgdata('fo.hdf', rank, dims, ti16)
-      print *, '            ', dsgdata('fo.hdf', rank, dims, ti32)
-
-C 
-      print *, 'Checking arrays from single file...\n\n'
-
-      err = 0
-      do 910 i=1,10
-         do 900 j=1,10
-           if (f64(i,j) .ne. tf64(i,j)) err = 1
-  900    continue
-  910 continue
-
-      call err_check(err, number_failed, 'float64')
-C 
-
-      err = 0
-      do 1010 i=1,10
-         do 1000 j=1,10
-           if (f32(i,j) .ne. tf32(i,j)) err = 1
- 1000    continue
- 1010 continue
-
-      call err_check(err, number_failed, 'float32')
-C 
-      err = 0
-      do 1110 i=1,10
-         do 1100 j=1,10
-           if (i8(i,j) .ne. ti8(i,j)) err = 1
- 1100    continue
- 1110 continue
-
-      call err_check(err, number_failed, 'int8')
-C 
-      err = 0
-      do 1210 i=1,10
-         do 1200 j=1,10
-           if (i16(i,j) .ne. ti16(i,j)) err = 1
- 1200    continue
- 1210 continue
-
-      call err_check(err, number_failed, 'int16')
-C 
-      err = 0
-      do 1310 i=1,10
-         do 1300 j=1,10
-           if (i32(i,j) .ne. ti32(i,j)) err = 1
- 1300    continue
- 1310 continue
-
-      call err_check(err, number_failed, 'int32')
-C 
-      print *
-      if (number_failed .gt. 0 ) then
-  	print *,'        >>> ', number_failed, ' TESTS FAILED <<<'
-      else
-  	print *,'        >>> ALL TESTS PASSED <<<'
-      endif
-      print *
-      print *
-
-      stop
-      end  
-  
+             if (i32(i,j).ne.ti32(i,j)) err = 1
+             ti32(i,j) = 0
+ 500      continue
+ 510   continue
+       
+       call err_check(err, number_failed, 'int32')
+       
+       
+C     
+       print *, 'Writing arrays to single file.'
+       print *, 'Error values: '
+C     
+       err = dssnt(DFNT_NFLOAT64)
+       print *, '            ', dsadata('fo.hdf', rank, dims, f64)
+       
+       err = dssnt(DFNT_NFLOAT32)
+       print *, '            ', dsadata('fo.hdf', rank, dims, f32)
+       
+       err = dssnt(DFNT_NINT8)
+       print *, '            ', dsadata('fo.hdf', rank, dims, i8)
+       
+       err = dssnt(DFNT_NINT16)
+       print *, '            ', dsadata('fo.hdf', rank, dims, i16)
+       
+       err = dssnt(DFNT_NINT32)
+       print *, '            ', dsadata('fo.hdf', rank, dims, i32)
+       
+C     
+       print *, 'Reading arrays from single file... '
+       print *, 'Error values: '
+C     
+       print *, '            ', dsgdata('fo.hdf', rank, dims, tf64)
+       print *, '            ', dsgdata('fo.hdf', rank, dims, tf32)
+       print *, '            ', dsgdata('fo.hdf', rank, dims, ti8)
+       print *, '            ', dsgdata('fo.hdf', rank, dims, ti16)
+       print *, '            ', dsgdata('fo.hdf', rank, dims, ti32)
+       
+C     
+       print *, 'Checking arrays from single file...\n\n'
+       
+       err = 0
+       do 910 i=1,10
+          do 900 j=1,10
+             if (f64(i,j) .ne. tf64(i,j)) err = 1
+ 900      continue
+ 910   continue
+       
+       call err_check(err, number_failed, 'float64')
+C     
+       
+       err = 0
+       do 1010 i=1,10
+          do 1000 j=1,10
+             if (f32(i,j) .ne. tf32(i,j)) err = 1
+ 1000     continue
+ 1010  continue
+       
+       call err_check(err, number_failed, 'float32')
+C     
+       err = 0
+       do 1110 i=1,10
+          do 1100 j=1,10
+             if (i8(i,j) .ne. ti8(i,j)) err = 1
+ 1100     continue
+ 1110  continue
+       
+       call err_check(err, number_failed, 'int8')
+C     
+       err = 0
+       do 1210 i=1,10
+          do 1200 j=1,10
+             if (i16(i,j) .ne. ti16(i,j)) err = 1
+ 1200     continue
+ 1210  continue
+       
+       call err_check(err, number_failed, 'int16')
+C     
+       err = 0
+       do 1310 i=1,10
+          do 1300 j=1,10
+             if (i32(i,j) .ne. ti32(i,j)) err = 1
+ 1300     continue
+ 1310  continue
+       
+       call err_check(err, number_failed, 'int32')
+C     
+       print *
+       if (number_failed .gt. 0 ) then
+          print *,'        >>> ', number_failed, ' TESTS FAILED <<<'
+       else
+          print *,'        >>> ALL TESTS PASSED <<<'
+       endif
+       print *
+       print *
+       
+       stop
+       end  
+       
 C
 C
       subroutine err_check(err, num_fail, type)
--- hdf/test/tstubsf.f
+++ hdf/test/tstubsf.f
@@ -2,9 +2,12 @@ C
 C $Header$
 C
 C $Log$
-C Revision 1.6  1992/06/16 15:39:42  chouck
-C HP is really picky about things not extending into column 72
+C Revision 1.7  1992/12/01 21:07:48  chouck
+C Fixed tab / space problem so Mac Fortran doesn't complain
 C
+c Revision 1.6  1992/06/16  15:39:42  chouck
+c HP is really picky about things not extending into column 72
+c
 c Revision 1.5  1992/06/02  16:04:38  dilg
 c Added more thorough test of dffind() and fixed error in expected return code
 c from dfput().
@@ -23,9 +26,9 @@ c
 c Revision 1.1  1992/03/26  21:51:22  dilg
 c Initial revision
 c
-C
+C     
       program tstubsF
-
+      
       character ar0*10
       character ar1*9
       character ar2*30
@@ -59,9 +62,9 @@ C
       data r7   /7/
 
       integer dfaccess, dfopen, dfclose, dfdesc, dfdup, dfdel, dfread,
-     +		dfwrite, dfupdate, dfget, dfput, dfsfind, dffind, 
-     +          dferrno, dfishdf, dfnewref, dfnumber, dfstat
-
+     +     dfwrite, dfupdate, dfget, dfput, dfsfind, dffind, 
+     +     dferrno, dfishdf, dfnewref, dfnumber, dfstat
+      
       a0size = len(ar0)
       a1size = len(ar1)
       a2size = len(ar2)
@@ -80,17 +83,17 @@ C
       read(5,100) in
  100  format(a)
       in(1:20) = '                    '
-
+      
       print *, ' '
       print *, 'Testing dferrno...'
       ret = dferrno()
       if (ret .ne. 0) then
-	print *, '>>>Failure:  Returned ', ret, ' rather than 0.'
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  Returned ', ret, ' rather than 0.'
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-
+      
       print *, ' '
       print *, 'Testing dfishdf... (should fail)'
       ret = dfishdf('tstubsF.hdf')
@@ -98,117 +101,117 @@ C
       if (ret .eq. -1) then
         print *, 'Success:  dfishdf failed with DFerror = ', dfenum
       else
-	print *, '>>>Failure:  Non-existent file looks like HDF file.'
-	print *, '   Maybe was a pre-existing file named "tstubsF.hdf"'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  Non-existent file looks like HDF file.'
+         print *, '   Maybe was a pre-existing file named "tstubsF.hdf"'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       endif
-
+      
       print *, ' '
       print *, 'Testing dfopen... (new file)'
       dfile = dfopen('tstubsF.hdf', 6, 0)
       dfenum = dferrno()
       if (dfile .eq. 0) then
-	print *, '>>>Failure:  Error ', dfenum, ' opening file.'
-	print *, '   Quiting.'
-	stop
+         print *, '>>>Failure:  Error ', dfenum, ' opening file.'
+         print *, '   Quiting.'
+         stop
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-
+      
       print *, ' '
       print *, 'Testing dfclose...'
       ret = dfclose(dfile)
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, '>>>Failure:  could not close file.'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  could not close file.'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-
+      
       print *, ' '
       print *, 'Testing dfclose... (invalid file; should fail)'
       ret = dfclose(dfile)
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, 'Success: dfclose failed with DFerror = ', dfenum
+         print *, 'Success: dfclose failed with DFerror = ', dfenum
       else
-	print *, '>>>Failure:  Close allowed on unopened file.'
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  Close allowed on unopened file.'
+         nerrors = nerrors + 1
       endif
-
+      
       print *, ' '
       print *, 'Testing dfopen... (existing file)'
       dfile = dfopen('tstubsF.hdf', 2, 0)
       dfenum = dferrno()
       if (dfile .eq. 0) then
-	print *, '>>>Failure:  Error ', dfenum, ' opening file.'
-	print *, '   Quiting.'
-	stop
+         print *, '>>>Failure:  Error ', dfenum, ' opening file.'
+         print *, '   Quiting.'
+         stop
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-
+      
       print *, ' '
       print *, 'Testing dfput...'
       ret = dfput(dfile, t255, r1, ar0, a0size)
       dfenum = dferrno()
       if (ret .ne. a0size) then
-	print *, '>>>Failure:  DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-
+      
       print *, ' '
       print *, 'Testing dfget...'
       ret = dfget(dfile, t255, r1, in)
       dfenum = dferrno()
       if (ret .ne. a0size) then
-	print *, '>>>Failure:  read ', ret, ' of ', a0size, ' bytes.'
-	print *, '   String read:  ', in
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  read ', ret, ' of ', a0size, ' bytes.'
+         print *, '   String read:  ', in
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	err = 0
-	do 10 i=1, a0size
+         err = 0
+         do 10 i=1, a0size
 	    if (in(i:i) .ne. ar0(i:i)) err = 1
- 10	continue
-	if (err .eq. 1) then
-	  print *, '>>>Failure:  strings differ.'
-	  print *, '   String written:  ', ar0
-	  print *, '   String read:     ', in
-	  nerrors = nerrors + 1
-	else
-	  print *, 'Success:  string read is the same as written.'
-        endif
+ 10      continue
+         if (err .eq. 1) then
+            print *, '>>>Failure:  strings differ.'
+            print *, '   String written:  ', ar0
+            print *, '   String read:     ', in
+            nerrors = nerrors + 1
+         else
+            print *, 'Success:  string read is the same as written.'
+         endif
       endif
-
+      
       in(1:20) = '                    '
-
+      
       print *, ' '
       print *, 'Testing dfaccess (write)...'
       ret = dfaccess(dfile, t255, r3, 'w')
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, '>>>Failure:'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-
+      
       print *, ' '
       print *, 'Testing dfread... (should fail)'
       ret = dfread(dfile, in, 5)
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, 'Success:  dfread failed with DFerror = ', dfenum
+         print *, 'Success:  dfread failed with DFerror = ', dfenum
       else
-	print *, '>>>Failure:  Read allowed on write element.'
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  Read allowed on write element.'
+         nerrors = nerrors + 1
       endif
       in(1:20) = '                    '
 
@@ -217,11 +220,11 @@ C
       ret = dfwrite(dfile, ar1, a1size)
       dfenum = dferrno()
       if (ret .ne. a1size) then
-	print *, '>>>Failure:  wrote ', ret, ' of ', a1size, ' bytes.'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  wrote ', ret, ' of ', a1size, ' bytes.'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
 
       print *, ' '
@@ -229,11 +232,11 @@ C
       ret = dfaccess(dfile, t255, r3, 'r')
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, '>>>Failure:'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
 
       print *, ' '
@@ -241,10 +244,10 @@ C
       ret = dfwrite(dfile, in, 5)
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, 'Success:  dfwrite failed with DFerror = ', dfenum
+         print *, 'Success:  dfwrite failed with DFerror = ', dfenum
       else
-	print *, '>>>Failure:  write allowed on read element.'
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  write allowed on read element.'
+         nerrors = nerrors + 1
       endif
 
       print *, ' '
@@ -252,64 +255,64 @@ C
       ret = dfread(dfile, in, a1size)
       dfenum = dferrno()
       if (ret .ne. a1size) then
-	print *, '>>>Failure:  read ', ret, ' of ', a1size, ' bytes.'
-	print *, '   String read:  ', in
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  read ', ret, ' of ', a1size, ' bytes.'
+         print *, '   String read:  ', in
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
 	err = 0
 	do 40 i=1, a1size
-	  if (in(i:i) .ne. ar1(i:i)) err = 1
+           if (in(i:i) .ne. ar1(i:i)) err = 1
  40	continue
 	if (err .eq. 1) then
-	  print *, '>>>Failure:  strings differ.'
-	  print *, '   String written:  ', ar1
-	  print *, '   String read:     ', in
-	  nerrors = nerrors + 1
+           print *, '>>>Failure:  strings differ.'
+           print *, '   String written:  ', ar1
+           print *, '   String read:     ', in
+           nerrors = nerrors + 1
 	else
-	  print *, 'Success:  string read is the same as written.'
+           print *, 'Success:  string read is the same as written.'
         endif
       endif
       in(1:20) = '                    '
-
+      
       print *, ' '
       print *, 'Testing dfnumber...'
       nd = dfnumber(dfile, t255)
       dfenum = dferrno()
       if (nd .ne. 2) then
-	print *, '>>>Failure:'
-	print *, '   Saw ', nd, ' occurrances of tag 255 not than 2.'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   Saw ', nd, ' occurrances of tag 255 not than 2.'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-    
+      
       print *, ' '
       print *, 'Testing dfdesc...'
       ret = dfdesc(dfile, dlist, 0, 5)
       dfenum = dferrno()
-C
+C     
 C  add one for version tag
 C
       if (ret .ne. (nd + 1)) then
-	print *, '>>>Failure:  Returned ', ret, ' rather than ', nd+1
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  Returned ', ret, ' rather than ', nd+1
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
-
+      
       print *, ' '
       print *, 'Testing dfupdate'
       ret = dfupdate(dfile)
       dfenum = dferrno()
       if (ret .ne. 0) then
-	print *, '>>>Failure:'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
 
       print *, ' '
@@ -317,11 +320,11 @@ C
       ret = dfstat(dfile, dfinfo)
       dfenum = dferrno()
       if (ret .ne. 0) then
-	print *, '>>>Failure:'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
     
       print *, ' '
@@ -329,11 +332,11 @@ C
       ret = dfnewref(dfile)
       dfenum = dferrno()
       if (ret .ne. 4) then
-	print *, '>>>Failure:  Returned ref. ', ret, ' instead of 4'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  Returned ref. ', ret, ' instead of 4'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
 
       print *, ' '
@@ -341,108 +344,108 @@ C
       ret = dfdup(dfile, 127, r7, t255, r3)
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, '>>>Failure:'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	ret = dfnumber(dfile, t127)
-      dfenum = dferrno()
+         ret = dfnumber(dfile, t127)
+         dfenum = dferrno()
 	if (ret .ne. 1) then
-	  print *, '>>>Failure:  duplicated tag not found.'
-	  print *, '   DFerror = ', dfenum
-	  nerrors = nerrors + 1
+           print *, '>>>Failure:  duplicated tag not found.'
+           print *, '   DFerror = ', dfenum
+           nerrors = nerrors + 1
 	else
-	  print *, 'Success!'
+           print *, 'Success    !'
         endif
       endif
-
+      
       print *, ' '
       print *, 'Testing dfdel...'
       ret = dfdel(dfile, t127, r7)
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, '>>>Failure:'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	ret = dfnumber(dfile, t127)
-      dfenum = dferrno()
-	if (ret .ne. 0) then
-	  print *, '>>>Failure:  found ', ret, ' deleted tags.'
-	  print *, '   DFerror = ', dfenum
-	  nerrors = nerrors + 1
-	else
+         ret = dfnumber(dfile, t127)
+         dfenum = dferrno()
+         if (ret .ne. 0) then
+            print *, '>>>Failure:  found ', ret, ' deleted tags.'
+            print *, '   DFerror = ', dfenum
+            nerrors = nerrors + 1
+         else
 	  print *, 'Success!'
-        endif
+       endif
       endif
-
+      
       print *, ' '
       print *, 'Testing dfsfind...'
       ret = dfsfind(dfile, 254, 0)
       dfenum = dferrno()
       if (ret .eq. -1) then
-	print *, '>>>Failure:'
-	print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       else
-	print *, 'Success!'
+         print *, 'Success      !'
       endif
 
       print *, ' '
       print *, 'Testing dffind...'
       ret = dfdup(dfile, 254, 4, 255, 3)
       if (ret .ne. 0) then
-        print *, '>>>DFdup 1 failed.'
+         print *, '>>>DFdup 1 failed.'
       endif
       ret = dfdup(dfile, 254, 5, 255, 3)
       if (ret .ne. 0) then
-        print *, '>>>DFdup 2 failed.'
+         print *, '>>>DFdup 2 failed.'
       endif
       ret = dfdup(dfile, 254, 6, 255, 3)
       if (ret .ne. 0) then
-        print *, '>>>DFdup 3 failed.'
+         print *, '>>>DFdup 3 failed.'
       endif
       do 200 i=4,6
-      ret = dffind(dfile, tag, ref, length)
-      dfenum = dferrno()
-      if (ret .eq. -1) then
-        print *, '>>>Failure:'
-        print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
-      else
-	if ((tag .ne. 254) .or. (ref .ne. i) .or. 
-     +                                  (length .ne. a1size)) then
-	  print *, '>>>Failure:  tag/ref found is not correct.'
-	  print *, '   Looking for:'
-	  print *, '      tag:      254'
-	  print *, '      ref:    ', i
-	  print *, '      length: ', a1size
-	  print *, '   Found:'
-	  print *, '      tag:    ', tag
-	  print *, '      ref:    ', ref
-	  print *, '      length: ', length
-	  nerrors = nerrors + 1
-	else
-          print *, 'Success!'
-	endif
-      endif
+         ret = dffind(dfile, tag, ref, length)
+         dfenum = dferrno()
+         if (ret .eq. -1) then
+            print *, '>>>Failure:'
+            print *, '   DFerror = ', dfenum
+            nerrors = nerrors + 1
+         else
+            if ((tag .ne. 254) .or. (ref .ne. i) .or. 
+     +           (length .ne. a1size)) then
+               print *, '>>>Failure:  tag/ref found is not correct.'
+               print *, '   Looking for:'
+               print *, '      tag:      254'
+               print *, '      ref:    ', i
+               print *, '      length: ', a1size
+               print *, '   Found:'
+               print *, '      tag:    ', tag
+               print *, '      ref:    ', ref
+               print *, '      length: ', length
+               nerrors = nerrors + 1
+            else
+               print *, 'Success !'
+            endif
+         endif
  200  continue
-
+      
       ret = dfclose(dfile)
       dfenum = dferrno()
       if (ret .ne. 0) then
-	print *, '>>>Failure:  dfclose failed (probably from open aid)'
-        print *, '   DFerror = ', dfenum
-	nerrors = nerrors + 1
+         print *, '>>>Failure:  dfclose failed (probably from open aid)'
+         print *, '   DFerror = ', dfenum
+         nerrors = nerrors + 1
       endif
-
-
+      
+      
       print *, ' '
       print *, ' '
       print *, 'Test Summary:'
       print *, '   ', nerrors, ' errors were encountered.'
       if (nerrors .ne. 0) then
-	print *, '   Please check program output carefully.'
+         print *, '   Please check program output carefully.'
       endif
 
       stop
