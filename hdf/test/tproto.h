7bb09022c8e0db9b68346da50fc96b466540c705
briand <briand@hdfgroup.org>
1993-01-27 22:05:43 +0000

    Initial version of testhdf.

---
+++ hdf/test/tproto.h
@@ -0,0 +1,22 @@
+#ifndef TPROTO_H
+#define TPROTO_H
+
+#include "hdf.h"
+#include "tutils.h"
+
+test_an();
+test_anfile();
+test_hblocks();
+test_hextelt();
+test_hfile();
+test_hfile1();
+test_r24();
+test_r8();
+test_pal();
+test_sdmms();
+test_sdnmms();
+test_tsdstr();
+test_vers();
+
+
+#endif
