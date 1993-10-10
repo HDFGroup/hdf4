bd7ce866ad16d03f279a9c9319dcacc50a8f9979
Quincey Koziol <koziol@lbl.gov>
1993-10-10 17:11:18 -0500

    Updated header files and added compression test.

--- hdf/test/tutils.h
+++ hdf/test/tutils.h
@@ -4,18 +4,18 @@
 /* Used to make certain a return value _is_ a value */
 #define CHECK(ret, val, where) \
 do {if (Verbocity>9) printf("   Call to HDF routine: %15s at line %4d in %s returned %d \n",where,__LINE__,__FILE__,(int)ret);\
-if(ret == val) {printf("*** UNEXPECTED RETURN from %s is %d\n", where, (int)ret); num_errs++;} \
+if(ret == val) {printf("*** UNEXPECTED RETURN from %s is %d at line %4d in %s\n", where, (int)ret, __LINE__,__FILE__); num_errs++;} \
 } while(0)
 
 /* Used to make certain a return value _is_not_ a value */
 #define VERIFY(x, val, where) \
 do {if (Verbocity>9) printf("   Call to HDF routine: %15s at line %4d in %s had value %d \n",where,__LINE__,__FILE__,(int)x);\
-if(x != val) {printf("*** UNEXPECTED VALUE from %s is %d\n", where, (int)x); num_errs++;} \
+if(x != val) {printf("*** UNEXPECTED VALUE from %s is %d at line %4d in %s\n", where, (int)x,__LINE__,__FILE__); num_errs++;} \
 } while(0)
 
 #define RESULT(a) \
 do {if (Verbocity>8) printf("   Call to HDF routine: %15s at line %4d in %s returned %d \n",a,__LINE__,__FILE__,(int)ret); \
-if(ret == FAIL) {printf("*** UNEXPECTED RETURN from %s is %d\n", a, (int)ret); num_errs++;} \
+if(ret == FAIL) {printf("*** UNEXPECTED RETURN from %s is %d at line %4d in %s\n", a, (int)ret,__LINE__,__FILE__); num_errs++;} \
 } while(0)
 
 #define MESSAGE(v,a) {if (Verbocity>v) {a}}
