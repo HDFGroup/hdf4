e4ea6224c4e9d63bc0c50ae53dc956a7f63d1fff
Quincey Koziol <koziol@lbl.gov>
1993-10-10 17:10:36 -0500

    Moved Tag descriptions into a header file.  Updated compression routines.

--- hdf/src/hcompi.h
+++ hdf/src/hcompi.h
@@ -14,9 +14,12 @@ static char RcsId[] = "@(#)$Revision$";
 $Header$
 
 $Log$
-Revision 1.3  1993/10/06 20:27:32  koziol
-More compression fixed, and folded Doug's suggested change into VSappendable.
+Revision 1.3.2.1  1993/10/10 22:10:15  koziol
+Moved Tag descriptions into a header file.  Updated compression routines.
 
+ * Revision 1.3  1993/10/06  20:27:32  koziol
+ * More compression fixed, and folded Doug's suggested change into VSappendable.
+ *
  * Revision 1.2  1993/10/01  20:00:57  koziol
  * Put "extern C" block around function prototypes for C++ compatibility.
  *
@@ -63,7 +66,8 @@ typedef struct comp_coder_info_tag {
 
 /* structure for storing a state */
 typedef struct comp_stateinfo_tag {
-    uint32 offset;              /* the offset in bytes of the state */
+    uint32 d_offset;            /* the offset of the state in the dataset */
+    uint32 c_offset;            /* offset of the state in the compressed data */
     comp_model_info_t minfo;    /* modeling information */
     comp_coder_info_t cinfo;    /* coding information */
  } comp_stateinfo_t;
