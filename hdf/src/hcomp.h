adb10dc3280097734dd742d1f4ec3259b8b01713
Quincey Koziol <koziol@lbl.gov>
1993-10-19 12:38:14 -0500

    Tons of changes and updates.  Fixed the 0 length problem in Hstartwrite(), made all the 'void' returning routines in the Vsets return a succeed or fail.  Added N-Bit files (which aren't working yet...)

--- hdf/src/hcomp.h
+++ hdf/src/hcomp.h
@@ -22,7 +22,8 @@ typedef enum {
 /* For determining which type of encoding is being done */
 typedef enum {
     COMP_CODE_NONE=0,       /* don't encode at all, just store */
-    COMP_CODE_RLE           /* for simple RLE encoding */
+    COMP_CODE_RLE,          /* for simple RLE encoding */
+    COMP_CODE_NBIT          /* for N-bit encoding */
 } comp_coder_t;
 
 /* Compression types available */
@@ -54,6 +55,14 @@ uint16 compress_map[COMP_MAX_COMP+1]={  /* Mapping from compression types to tag
   };
 #endif
 
+typedef union tag_model_info {  /* Union to contain modeling information */
+    struct {
+        int32 nt;       /* number type */
+        intn ndim;      /* number of dimensions */
+        int32 *dims;    /* array of dimensions */
+      } dim;
+  } model_info;
+
 typedef union tag_comp_info {  /* Union to contain compression information */
     struct {            /* Struct to contain information about how to compress */
                         /* or decompress a JPEG encoded 24-bit image */
@@ -65,6 +74,15 @@ typedef union tag_comp_info {  /* Union to contain compression information */
                                 /* This is only an issue for quality */
                                 /* settings below 24 */
       } jpeg;
+    struct {            /* struct to contain information about how to compress */
+                        /* or decompress a N-bit encoded dataset */
+        int32 nt;       /* number type of the data to encode */
+        bool sign_ext;  /* whether to sign extend or not */
+        bool fill_one;  /* whether to fill with 1's or 0's */
+        intn start_bit; /* offset of the start bit in the data */
+        intn bit_len;   /* number of bits to store */
+      } nbit;
   } comp_info;
 
 #endif /* __HCOMP_H */
+
