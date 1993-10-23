d0308ac65b5c6bab252eea45acc6973976c62b40
georgev <georgev@hdfgroup.org>
1993-10-23 19:39:37 +0000

    Long entry: 1. Added new Copyright header to all HDF files in "src". The header    now points to "hdf/COPYING" for license/distribution info. 2. Delete $Log variable from all source files. 3. Reorganized most data structures to allow for better storage alignment    during allocation. 4. Reducing global memory allocation by using dynamic allocation where    possible. Still needs work. 5. General code clean up. Added coments where they were lacking, noticable     by the trailing "?" at the end of each comment.

--- hdf/src/dfgr.h
+++ hdf/src/dfgr.h
@@ -1,3 +1,15 @@
+/****************************************************************************
+ * NCSA HDF                                                                 *
+ * Software Development Group                                               *
+ * National Center for Supercomputing Applications                          *
+ * University of Illinois at Urbana-Champaign                               *
+ * 605 E. Springfield, Champaign IL 61820                                   *
+ *                                                                          *
+ * For conditions of distribution and use, see the accompanying             *
+ * hdf/COPYING file.                                                      *
+ *                                                                          *
+ ****************************************************************************/
+
 /* $Id$ */
 
 /*-----------------------------------------------------------------------------
@@ -17,25 +29,31 @@
 
 /* description record: used to describe image data, palette data etc. */
 typedef struct {
-    int32 xdim, ydim;               /* dimensions of data */
-    DFdi nt;                        /* number type of data */
-    intn ncomponents, interlace;    /* data ordering: chunky / planar etc */
-    DFdi compr;                     /* compression */
-        /* ### Note: compression is currently uniquely described with a tag.
-            No data is attached to this tag/ref.  But this capability is
-            provided for future expansion, when this tag/ref might point to
-            some data needed for decompression, such as the actual encodings */
+    intn  ncomponents;  /* number of components */
+    intn  interlace;    /* data ordering: chunky / planar etc */
+    int32 xdim;         /* X- dimension of data */
+    int32 ydim;         /* Y- dimensionsof data */
+    DFdi  nt;           /* number type of data */
+    DFdi  compr;        /* compression */
+    /* ### Note: compression is currently uniquely described with a tag.
+           No data is attached to this tag/ref.  But this capability is
+           provided for future expansion, when this tag/ref might point to
+           some data needed for decompression, such as the actual encodings */
 } DFGRdr;
 
 /* structure to hold RIG info */
 typedef struct {
-    DFdi data[3];                   /* image/lut/mattechannel */
-    DFGRdr datadesc[3];             /* description of image/lut/mattechannel */
-    int32 xpos, ypos;               /* X-Y position of image on screen */
-    float32 aspectratio;            /* ratio of pixel height to width */
-    float32 ccngamma,               /* color correction parameters */
-        ccnred[3], ccngrren[3], ccnblue[3], ccnwhite[3];
-    char *cf;                       /* color format */
+    char    *cf;              /* color format */
+    int32   xpos;             /* X position of image on screen */
+    int32   ypos;             /* Y position of image on screen */
+    float32 aspectratio;      /* ratio of pixel height to width */
+    float32 ccngamma;         /* gamma color correction parameter */ 
+    float32 ccnred[3];        /* red color correction parameter */ 
+    float32 ccngrren[3];      /* green color correction parameter */ 
+    float32 ccnblue[3];       /* blue color correction parameter */  
+    float32 ccnwhite[3];      /* white color correction parameter */ 
+    DFdi    data[3];          /* image/lut/mattechannel */
+    DFGRdr  datadesc[3];      /* description of image/lut/mattechannel */
 } DFGRrig;
 
 #if defined c_plusplus || defined __cplusplus
@@ -44,7 +62,7 @@ extern "C" {
 
 /* Library-developer functions */
 extern int32 DFGRIopen
-    PROTO((char *filename, int access));
+       PROTO((char *filename, int access));
 
 #if defined c_plusplus || defined __cplusplus
 }
