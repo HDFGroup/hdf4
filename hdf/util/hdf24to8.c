ce5354e72c99fee75dc9deb00133e0cef9b317e6
georgev <georgev@hdfgroup.org>
1993-10-25 23:57:13 +0000

    Forgot to remove outdated COPYRIGHT.

--- hdf/util/hdf24to8.c
+++ hdf/util/hdf24to8.c
@@ -16,27 +16,6 @@ static char RcsId[] = "@(#)$Revision$";
 
 /* $Id$ */
 
-/*****************************************************************************
-*
-*                         NCSA HDF version 3.2beta
-*                            February 29, 1992
-*
-* NCSA HDF Version 3.2 source code and documentation are in the public
-* domain.  Specifically, we give to the public domain all rights for future
-* licensing of the source code, all resale rights, and all publishing rights.
-*
-* We ask, but do not require, that the following message be included in all
-* derived works:
-*
-* Portions developed at the National Center for Supercomputing Applications at
-* the University of Illinois at Urbana-Champaign.
-*
-* THE UNIVERSITY OF ILLINOIS GIVES NO WARRANTY, EXPRESSED OR IMPLIED, FOR THE
-* SOFTWARE AND/OR DOCUMENTATION PROVIDED, INCLUDING, WITHOUT LIMITATION,
-* WARRANTY OF MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE
-*
-*****************************************************************************/
-
 /**************************************************************************
 * hdf24hdf8	Quantizes a HDF RGB 24 bit "pixel" image into a 8 bit image
 *               with RGB palette and stores it as a HDF 8-bit raster image
