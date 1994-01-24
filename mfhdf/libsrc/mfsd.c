949d0d7f9c17e3c25fd4845fd7666011753b4678
chouck <chouck@hdfgroup.org>
1994-01-24 20:16:14 +0000

    Added attribute test calls and fixed SDfindattr() to accept all types of IDs.

--- mfhdf/libsrc/mfsd.c
+++ mfhdf/libsrc/mfsd.c
@@ -63,6 +63,8 @@ PRIVATE intn SDIputattr PROTO((NC_array **ap, char *name, int32 nt,
 PRIVATE int32 SDIgetcoordvar PROTO((NC *handle, NC_dim *dim, int32 id,
     int32 nt));
 PRIVATE int32 SDIfreevarAID PROTO((NC * handle, int32 index));
+PRIVATE intn SDIapfromid
+    PROTO((int32 id, NC ** handlep, NC_array *** app));
 
 /* ---------------------------- SDIhandle_from_id ---------------------------- */
 /*
@@ -275,7 +277,7 @@ int32 id;
 } /* SDend */
 
 
-/* ------------------------------- SDnumber ------------------------------- */
+/* ------------------------------- SDfileinfo ------------------------------- */
 /*
   Return the number of SDS-type objects and global attributes in the file
 
@@ -1184,6 +1186,91 @@ VOIDP pmax, pmin;
 
 } /* SDsetrange */
 
+/* ----------------------------- SDIapfromid ------------------------------ */
+/*
+
+ NAME
+	SDIapfromid -- get the attribute list
+ USAGE
+	int32 SDapfromid(id, handle, app)
+        int32        id;               IN:  object ID
+        NC       **  handlep;          IN:  handle for this file
+        NC_array *** app;  / yuk! /    OUT: attribute list
+ RETURNS
+        On error FAIL else SUCCEED.
+ DESCRIPTION
+        Given a ID figure out what the handle and attribute 
+        list pointer are for that object.
+
+--------------------------------------------------------------------------- */
+PRIVATE
+intn
+#ifdef PROTOTYPE
+SDIapfromid(int32 id, NC ** handlep, NC_array *** app)
+#else
+SDIapfromid(id, handlep, app)
+int32        id;
+NC       **  handlep;
+NC_array *** app;
+#endif
+{
+    NC     * handle;
+    NC_var * var;
+    NC_dim * dim;
+    int32    varid;
+
+    handle = NULL;
+
+    /* see if its a variable ID */
+    handle = SDIhandle_from_id(id, SDSTYPE);
+    if(handle != NULL) { 
+        
+        /* find the variable */
+        var = SDIget_var(handle, id);
+        if(var == NULL)
+            return FAIL;
+
+        (*app) = &(var->attrs);
+        (*handlep) = handle;
+        return SUCCEED;
+    } 
+
+    /* see if its a file ID */
+    handle = SDIhandle_from_id(id, CDFTYPE);
+    if(handle != NULL) {
+        (*app) = &(handle->attrs);
+        (*handlep) = handle;
+        return SUCCEED;
+    }
+
+    /* see if its a dimension ID */
+    handle = SDIhandle_from_id(id, DIMTYPE);
+    if(handle != NULL) {
+
+        /* find the dimension */
+        dim = SDIget_dim(handle, id);
+        if(dim == NULL)
+            return FAIL;
+
+        /* get index of coordinate variable */
+        varid = SDIgetcoordvar(handle, dim, (int32)(id & 0xffff), (int32) 0);
+
+        /* get the variable object */
+        var = NC_hlookupvar(handle, varid);
+        if(var == NULL)
+            return FAIL;
+
+        (*app) = &(var->attrs);
+        (*handlep) = handle;
+        return SUCCEED;
+
+    }
+
+    return FAIL;
+
+} /* SDIapfromid */
+
+
 /* ------------------------------ SDsetattr ------------------------------- */
 /*
 
@@ -1208,7 +1295,6 @@ VOIDP data;
 {
 
     NC_array **ap;
-    NC_var   * var;
     NC       * handle;
 
 #ifdef SDDEBUG
@@ -1220,20 +1306,8 @@ VOIDP data;
         return FAIL;
 
     /* determine what type of ID we've been given */
-    handle = NULL;
-    handle = SDIhandle_from_id(id, SDSTYPE);
-    if(handle != NULL) { /* was a variable ID */
-        var = SDIget_var(handle, id);
-        if(var == NULL)
-            return FAIL;
-        ap = &(var->attrs);
-    } else {
-        /* see if its a fidle ID */
-        handle = SDIhandle_from_id(id, CDFTYPE);
-        if(handle == NULL)
-            return FAIL;
-        ap = &(handle->attrs);
-    }
+    if(SDIapfromid(id, &handle, &ap) == FAIL)
+        return FAIL;
 
     /* still no handle ? */
     if(handle == NULL)
@@ -1276,9 +1350,9 @@ intn  *count;
 {
 
     NC_array *  ap;
-    NC_attr  **atp;
-    NC_var   * var;
-    NC       * handle;
+    NC_array ** app;
+    NC_attr  ** atp;
+    NC       *  handle;
 
 #ifdef SDDEBUG
     fprintf(stderr, "SDattrinfo: I've been called\n");
@@ -1289,20 +1363,10 @@ intn  *count;
         return FAIL;
 
     /* determine what type of ID we've been given */
-    handle = NULL;
-    handle = SDIhandle_from_id(id, SDSTYPE);
-    if(handle != NULL) { /* was a variable ID */
-        var = SDIget_var(handle, id);
-        if(var == NULL)
-            return FAIL;
-        ap = var->attrs;
-    } else {
-        /* see if its a file ID */
-        handle = SDIhandle_from_id(id, CDFTYPE);
-        if(handle == NULL)
-            return FAIL;
-        ap = handle->attrs;
-    }
+    if(SDIapfromid(id, &handle, &app) == FAIL)
+        return FAIL;
+
+    ap = (*app);
 
     if((ap == NULL) || (index >= ap->count))
         return FAIL;
@@ -1351,33 +1415,24 @@ VOIDP buf;
 {
 
     NC_array *  ap;
-    NC_attr  **atp;
-    NC_var   * var;
-    NC       * handle;
+    NC_array ** app;
+    NC_attr  ** atp;
+    NC_var   *  var;
+    NC       *  handle;
 
 #ifdef SDDEBUG
     fprintf(stderr, "SDreadattr: I've been called\n");
 #endif
 
     /* sanity check args */
-    if(buf == NULL)
+    if((buf == NULL) || (index < 0))
         return FAIL;
     
     /* determine what type of ID we've been given */
-    handle = NULL;
-    handle = SDIhandle_from_id(id, SDSTYPE);
-    if(handle != NULL) { /* was a variable ID */
-        var = SDIget_var(handle, id);
-        if(var == NULL)
-            return FAIL;
-        ap = var->attrs;
-    } else {
-        /* see if its a fidle ID */
-        handle = SDIhandle_from_id(id, CDFTYPE);
-        if(handle == NULL)
-            return FAIL;
-        ap = handle->attrs;
-    }
+    if(SDIapfromid(id, &handle, &app) == FAIL)
+        return FAIL;
+
+    ap = (*app);
 
     if((ap == NULL) || (index >= ap->count))
         return FAIL;
@@ -2461,26 +2516,17 @@ char  * attrname;
 {
 
     NC_array *  ap;
+    NC_array ** app;
     NC_attr  ** attr;
     NC_var   *  var;
     NC       *  handle;
     int32       attrid, len;
 
     /* determine what type of ID we've been given */
-    handle = NULL;
-    handle = SDIhandle_from_id(id, SDSTYPE);
-    if(handle != NULL) { /* was a variable ID */
-        var = SDIget_var(handle, id);
-        if(var == NULL)
-            return FAIL;
-        ap = var->attrs;
-    } else {
-        /* see if its a fidle ID */
-        handle = SDIhandle_from_id(id, CDFTYPE);
-        if(handle == NULL)
-            return FAIL;
-        ap = handle->attrs;
-    }
+    if(SDIapfromid(id, &handle, &app) == FAIL)
+        return FAIL;
+
+    ap = (*app);
 
     if(ap == NULL)
         return FAIL;
