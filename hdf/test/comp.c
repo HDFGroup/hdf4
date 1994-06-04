f11c9b30c1b22c069f9f4f8b034f3bac5134a496
Quincey Koziol <koziol@lbl.gov>
1994-06-03 19:40:06 -0500

    Re-wrote compression tests to be much more easily expandable and easier to figure out.

--- hdf/test/comp.c
+++ hdf/test/comp.c
@@ -46,17 +46,351 @@ static char RcsId[] = "@(#)$Revision$";
 
 #define BUFSIZE 4096
 
-#ifdef QAK
+/* last ditch attempt to define this value... */
+#ifndef UINT_MAX
+#define UINT_MAX (unsigned)(-1)
+#endif /* UINT_MAX */
+
+/* make a guess about RAND_MAX if it isn't defined... */
+#ifndef RAND_MAX
+#define RAND_MAX (UINT_MAX)
+#endif /* RAND_MAX */
+
+/* define aliases for random number generation */
+#define RAND rand
+#define SEED(a) srand((unsigned)(a))
+
+#define COMP_TAG 1000
+
 /* different modeling layers to test */
 comp_model_t test_models[]={COMP_MODEL_STDIO};
 
 /* different compression layers to test */
-comp_coder_t test_coders[]={COMP_CODE_NONE,COMP_CODE_RLE,COMP_CODE_NBIT);
+comp_coder_t test_coders[]={
+COMP_CODE_NONE,
+COMP_CODE_RLE
+/*,COMP_CODE_NBIT*/	/* n-bit testing is done in it's own module, nbit.c */
+};
 
-#define NUM_OUTBUFS 1
-static uint8 FAR *outbuf[NUM_OUTBUFS];
-#endif
+int32 test_ntypes[]={
+DFNT_INT8 ,DFNT_UINT8 ,DFNT_INT16 ,DFNT_UINT16 ,DFNT_INT32 ,DFNT_UINT32 };
 
+#define NUM_OUTBUFS 3	/* the number of different output buffers to test */
+/* outbuf #1 is all zeros (very easy to compress) */
+/* outbuf #2 is a fibonacci sequence (very hard to compress) */
+/* outbuf #3 is random data (also hard to compress) */
+static int8 FAR *outbuf_int8[NUM_OUTBUFS];
+static uint8 FAR *outbuf_uint8[NUM_OUTBUFS];
+static int16 FAR *outbuf_int16[NUM_OUTBUFS];
+static uint16 FAR *outbuf_uint16[NUM_OUTBUFS];
+static int32 FAR *outbuf_int32[NUM_OUTBUFS];
+static uint32 FAR *outbuf_uint32[NUM_OUTBUFS];
+
+/* only need one input buffer per type of data */
+static int8 FAR *inbuf_int8;
+static uint8 FAR *inbuf_uint8;
+static int16 FAR *inbuf_int16;
+static uint16 FAR *inbuf_uint16;
+static int32 FAR *inbuf_int32;
+static uint32 FAR *inbuf_uint32;
+
+/* local function prototypes */
+static void init_model_info(comp_model_t m_type,model_info *m_info,
+		int32 test_ntype);
+static void init_coder_info(comp_coder_t c_type,comp_info *c_info,
+		int32 test_ntype);
+static void allocate_buffers(void);
+static void init_buffers(void);
+static void free_buffers(void);
+static uint16 write_data(int32 fid,comp_model_t m_type,model_info *m_info,
+		comp_coder_t c_type,comp_info *c_info,intn test_num,
+		int32 ntype);
+static void read_data(int32 fid,uint16 ref_num,intn test_num,int32 ntype);
+
+static void init_model_info(comp_model_t m_type,model_info *m_info,int32 test_ntype)
+{
+	/* shut the compiler up */
+	m_type=m_type; m_info=m_info; test_ntype=test_ntype;
+
+	switch(m_type) {
+		case COMP_MODEL_STDIO:
+		default:
+			/* don't do anything for this case */
+			break;
+	  }	/* end switch */
+}	/* end init_model_info() */
+
+static void init_coder_info(comp_coder_t c_type,comp_info *c_info,int32 test_ntype)
+{
+	/* shut the compiler up */
+	c_type=c_type; c_info=c_info; test_ntype=test_ntype;
+
+	switch(c_type) {
+		case COMP_CODE_RLE:
+		case COMP_CODE_NONE:
+		default:
+			/* don't do anything for this case */
+			break;
+	  }	/* end switch */
+}	/* end init_coder_info() */
+
+static void allocate_buffers(void)
+{
+	intn i;
+
+	for(i=0; i<NUM_OUTBUFS; i++) {
+		outbuf_int8[i]=(int8 *)HDgetspace(BUFSIZE*sizeof(int8));
+		outbuf_uint8[i]=(uint8 *)HDgetspace(BUFSIZE*sizeof(uint8));
+		outbuf_int16[i]=(int16 *)HDgetspace(BUFSIZE*sizeof(int16));
+		outbuf_uint16[i]=(uint16 *)HDgetspace(BUFSIZE*sizeof(uint16));
+		outbuf_int32[i]=(int32 *)HDgetspace(BUFSIZE*sizeof(int32));
+		outbuf_uint32[i]=(uint32 *)HDgetspace(BUFSIZE*sizeof(uint32));
+	  }	/* end for */
+	inbuf_int8=(int8 *)HDclearspace(BUFSIZE,sizeof(int8));
+	inbuf_uint8=(uint8 *)HDclearspace(BUFSIZE,sizeof(uint8));
+	inbuf_int16=(int16 *)HDclearspace(BUFSIZE,sizeof(int16));
+	inbuf_uint16=(uint16 *)HDclearspace(BUFSIZE,sizeof(uint16));
+	inbuf_int32=(int32 *)HDclearspace(BUFSIZE,sizeof(int32));
+	inbuf_uint32=(uint32 *)HDclearspace(BUFSIZE,sizeof(uint32));
+}	/* allocate_buffers() */
+
+static void init_buffers(void)
+{
+	uint32 last_fib,curr_fib,next_fib;
+	intn r;
+	intn i,j;
+
+	for(i=0; i<NUM_OUTBUFS; i++) {
+		switch(i) {
+			case 0:	/* all zero filled */
+				HDmemset(outbuf_int8[i],0,BUFSIZE*sizeof(int8));
+				HDmemset(outbuf_uint8[i],0,BUFSIZE*sizeof(uint8));
+				HDmemset(outbuf_int16[i],0,BUFSIZE*sizeof(int16));
+				HDmemset(outbuf_uint16[i],0,BUFSIZE*sizeof(uint16));
+				HDmemset(outbuf_int32[i],0,BUFSIZE*sizeof(int32));
+				HDmemset(outbuf_uint32[i],0,BUFSIZE*sizeof(uint32));
+				break;
+
+			case 1: /* fibonacci sequence */
+				for(j=0,last_fib=0,curr_fib=1; j<BUFSIZE; j++) {
+					outbuf_int8[i][j]=(int8)curr_fib;
+					outbuf_uint8[i][j]=(uint8)curr_fib;
+					outbuf_int16[i][j]=(int16)curr_fib;
+					outbuf_uint16[i][j]=(uint16)curr_fib;
+					outbuf_int32[i][j]=(int32)curr_fib;
+					outbuf_uint32[i][j]=(uint32)curr_fib;
+					next_fib=curr_fib+last_fib;
+					last_fib=curr_fib;
+					curr_fib=next_fib;
+				  }	/* end for */
+				break;
+
+			case 2: /* random #'s */
+				SEED(time(NULL));
+				for(j=0; j<BUFSIZE; j++) {
+					r=RAND();
+					outbuf_int8[i][j]=(int8)(r-RAND_MAX/2);
+					outbuf_uint8[i][j]=(uint8)r;
+					outbuf_int16[i][j]=(int16)(r-RAND_MAX/2);
+					outbuf_uint16[i][j]=(uint16)r;
+					outbuf_int32[i][j]=(int32)(r-RAND_MAX/2);
+					outbuf_uint32[i][j]=(uint32)r;
+				  }	/* end for */
+				break;
+		  }	/* end switch */
+	  }	/* end for */
+}	/* init_buffers() */
+
+static void free_buffers(void)
+{
+	intn i;
+
+	for(i=0; i<NUM_OUTBUFS; i++) {
+		HDfreespace(outbuf_int8[i]);
+		HDfreespace(outbuf_uint8[i]);
+		HDfreespace(outbuf_int16[i]);
+		HDfreespace(outbuf_uint16[i]);
+		HDfreespace(outbuf_int32[i]);
+		HDfreespace(outbuf_uint32[i]);
+	  }	/* end for */
+	HDfreespace(inbuf_int8);
+	HDfreespace(inbuf_uint8);
+	HDfreespace(inbuf_int16);
+	HDfreespace(inbuf_uint16);
+	HDfreespace(inbuf_int32);
+	HDfreespace(inbuf_uint32);
+}	/* free_buffers() */
+
+static uint16 write_data(int32 fid,comp_model_t m_type,model_info *m_info,
+	comp_coder_t c_type,comp_info *c_info,intn test_num,int32 ntype)
+{
+	int32 aid;
+	uint16 ret_ref;
+	int32 err_ret;
+	int32 write_size;
+	VOIDP data_ptr;
+
+	MESSAGE(8, printf("Writing data for test %d, ntype=%d, model_type=%d, coder_type=%d\n",(int)test_num,(int)ntype,(int)m_type,(int)c_type);)
+	ret_ref=Hnewref(fid);
+	aid=HCcreate(fid,COMP_TAG,ret_ref,m_type,m_info,c_type,c_info);
+    CHECK(aid, FAIL, "HCcreate");
+	if(aid==FAIL)
+		return(0);
+
+	switch(ntype) {
+		case DFNT_INT8:
+			data_ptr=(VOIDP)outbuf_int8[test_num];
+			break;
+		case DFNT_UINT8:
+			data_ptr=(VOIDP)outbuf_uint8[test_num];
+			break;
+		case DFNT_INT16:
+			data_ptr=(VOIDP)outbuf_int16[test_num];
+			break;
+		case DFNT_UINT16:
+			data_ptr=(VOIDP)outbuf_uint16[test_num];
+			break;
+		case DFNT_INT32:
+			data_ptr=(VOIDP)outbuf_int32[test_num];
+			break;
+		case DFNT_UINT32:
+			data_ptr=(VOIDP)outbuf_uint32[test_num];
+			break;
+		default:
+			return(0);
+	  }	/* end switch */
+
+	write_size=BUFSIZE*DFKNTsize(ntype);
+    err_ret = Hwrite(aid, write_size, data_ptr);
+    if(err_ret != write_size) {
+      fprintf(stderr, "ERROR(%d): Hwrite returned the wrong length: %d\n", __LINE__,(int)err_ret);
+      HEprint(stdout,0);
+      num_errs++;
+    }
+
+    err_ret = Hendaccess(aid);
+    CHECK(err_ret, FAIL, "Hendaccess");
+
+	return(ret_ref);
+}	/* end write_data() */
+
+static void read_data(int32 fid,uint16 ref_num,intn test_num,int32 ntype)
+{
+	int32 aid;
+	int32 err_ret;
+	int32 read_size;
+	VOIDP out_ptr;
+	VOIDP in_ptr;
+
+	MESSAGE(8, printf("Reading data for test %d, ntype=%d\n",(int)test_num,(int)ntype);)
+
+	aid=Hstartread(fid,COMP_TAG,ref_num);
+    CHECK(aid, FAIL, "Hstartread");
+	if(aid==FAIL)
+		return;
+
+	switch(ntype) {
+		case DFNT_INT8:
+			out_ptr=(VOIDP)outbuf_int8[test_num];
+			in_ptr=(VOIDP)inbuf_int8;
+			break;
+		case DFNT_UINT8:
+			out_ptr=(VOIDP)outbuf_uint8[test_num];
+			in_ptr=(VOIDP)inbuf_uint8;
+			break;
+		case DFNT_INT16:
+			out_ptr=(VOIDP)outbuf_int16[test_num];
+			in_ptr=(VOIDP)inbuf_int16;
+			break;
+		case DFNT_UINT16:
+			out_ptr=(VOIDP)outbuf_uint16[test_num];
+			in_ptr=(VOIDP)inbuf_uint16;
+			break;
+		case DFNT_INT32:
+			out_ptr=(VOIDP)outbuf_int32[test_num];
+			in_ptr=(VOIDP)inbuf_int32;
+			break;
+		case DFNT_UINT32:
+			out_ptr=(VOIDP)outbuf_uint32[test_num];
+			in_ptr=(VOIDP)inbuf_uint32;
+			break;
+		default:
+			return;
+	  }	/* end switch */
+
+	read_size=BUFSIZE*DFKNTsize(ntype);
+    err_ret = Hread(aid, read_size, in_ptr);
+    if(err_ret != read_size) {
+		fprintf(stderr, "ERROR(%d): Hwrite returned the wrong length: %d\n", __LINE__,(int)err_ret);
+		HEprint(stdout,0);
+		num_errs++;
+      } /* end if */
+
+	if(HDmemcmp(in_ptr,out_ptr,read_size)!=0) {
+		char *s=HDgetNTdesc(ntype);
+
+		fprintf(stderr, "ERROR: Data from test %d, number type %s differs\n",test_num,s);
+		HDfreespace(s);
+		num_errs++;
+	  }	/* end if */
+
+    err_ret = Hendaccess(aid);
+    CHECK(err_ret, FAIL, "Hendaccess");
+}	/* end read_data() */
+
+void test_comp(void)
+{
+    model_info m_info;
+    comp_info c_info;
+    uint16 ref_num;		/* reference number of the data written out */
+	int32 fid;			/* file ID of HDF file for testing */
+    intn test_num,ntype_num,model_num,coder_num;
+	int32 ret;
+
+	MESSAGE(6, printf("Starting compression test\n");)
+
+    /* allocate room for the input and output buffers */
+    allocate_buffers();
+
+    /* fill the buffers with interesting data to compress */
+    init_buffers();
+
+	/* open the HDF file */
+	fid=Hopen(TESTFILE_NAME,DFACC_ALL,0);
+
+    /* Cycle through the different testing data, the number types, */
+    /* the different modeling layers and the different coding layers, */
+    /* in that order */
+    for(test_num=0; test_num<NUM_OUTBUFS; test_num++) {
+		for(ntype_num=0; ntype_num<(sizeof(test_ntypes)/sizeof(test_ntypes[0]));
+				ntype_num++) {
+            for(model_num=0;
+                    model_num<(sizeof(test_models)/sizeof(test_models[0]));
+                    model_num++) {
+				init_model_info(test_models[model_num],&m_info,test_ntypes[ntype_num]);
+                for(coder_num=0;
+						coder_num<(sizeof(test_coders)/sizeof(test_coders[0]));
+						coder_num++) {
+					init_coder_info(test_coders[coder_num],&c_info,test_ntypes[ntype_num]);
+
+					ref_num=write_data(fid,test_models[model_num],&m_info,test_coders[coder_num],&c_info,test_num,test_ntypes[ntype_num]);
+					read_data(fid,ref_num,test_num,test_ntypes[ntype_num]);
+                  } /* end for */
+              } /* end for */
+          } /* end for */
+      } /* end for */
+
+	/* close the HDF file */
+	ret=Hclose(fid);
+    CHECK(ret, FAIL, "Hclose");
+
+    /* free the input and output buffers */
+    free_buffers();
+
+	MESSAGE(6, printf("Finished compression test\n");)
+}   /* end test_comp() */
+
+#ifdef OLD_WAY
 static uint8 FAR outbuf[BUFSIZE],
     FAR inbuf[BUFSIZE];
 
@@ -269,3 +603,5 @@ void test_comp()
     ret = Hclose(fid);
     CHECK(ret, FAIL, "Hclose");
 }
+#endif /* OLD_WAY */
+
