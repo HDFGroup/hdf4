c9365744592107c52c06151c2cbca31543fc2cad
sxu <sxu@hdfgroup.org>
1994-06-10 20:02:18 +0000

    patched changes between 1.2 and 1.8 into 1.2 to clean up the mess in revision tags.

--- hdf/util/hdf8to24.c
+++ hdf/util/hdf8to24.c
@@ -59,17 +59,14 @@ comp_info cinfo;        /* compression structure */
 *  Called by    :
 **********************************************************************/
 #ifdef PROTOTYPE
-bool magnify(uint8 *from_buffer,uint8 *to_buffer,int32 from_x0,
-    int32 from_y0,int32 from_x1,int32 from_y1,int32 from_width,
-    int32 from_height,int32 to_width,int32 to_height)
+intn magnify(uint8 *from_buffer,uint8 *to_buffer,int32 from_x0,
+	int32 from_y0, int32 from_x1, int32 from_y1, int32 from_width,
+	int32 from_height, int32 to_width, int32 to_height)
 #else
-bool magnify(from_buffer,to_buffer,from_x0,from_y0,from_x1,from_y1,
-    from_width,from_height,to_width,to_height)
-uint8 *from_buffer,
-    *to_buffer;
-int32 from_x0,from_y0,from_x1,from_y1,
-    from_width,from_height,
-    to_width,to_height;
+intn magnify(from_buffer,to_buffer,from_x0,from_y0,from_x1,from_y1,from_width,
+	from_height, to_width, to_height)
+uint8 *from_buffer,*to_buffer;
+int32 from_x0, from_y0, from_x1, from_y1, from_width, from_height, to_width, to_height;
 #endif
 {
     uint8 *buf_off,     /* the current offset into the magnified data */
@@ -152,12 +149,12 @@ XCoorFailed:    /* Failed to allocate memory for the X coor. lookup table */
 *  Called by    :
 **********************************************************************/
 #ifdef PROTOTYPE
-bool convert8to24(uint8 *img8_buf,uint8 *img24_buf,int32 img_xdim,
-    int32 img_ydim)
+intn convert8to24(uint8 *img8_buf,uint8 *img24_buf,int32 img_xdim,
+	int32 img_ydim)
 #else
-bool convert8to24(img8_buf,img24_buf,img_xdim,img_ydim)
+intn convert8to24(img8_buf,img24_buf,img_xdim,img_ydim)
 uint8 *img8_buf,*img24_buf;
-int32 img_xdim,img_ydim;
+int32 img_xdim, img_ydim;
 #endif
 {
     uint32 pixels;      /* local counting variable */
@@ -178,11 +175,7 @@ int32 img_xdim,img_ydim;
     return(TRUE);
 }   /* end convert8to24() */
 
-#if defined ( PROTOTYPE ) && ! defined ( CONVEX )
-VOID usage(VOID)
-#else
 VOID usage()
-#endif
 {
     printf("USAGE: make24 [-s<scale>] [-j] <input HDF file> <output HDF file>\n");
     printf("    -s<scale> : set scale for magnifying the 8-bit input file.\n");
@@ -204,15 +197,15 @@ int argc;
 char *argv[];
 #endif
 {
-    bool do_jpeg=FALSE;     /* flag to indicate JPEG compression */
+    intn do_jpeg=FALSE;     /* flag to indicate JPEG compression */
     intn jpeg_qual=75;      /* JPEG quality factor */
-    bool do_scale=FALSE;    /* flag to indicate whether to scale images */
-    float32 img_scale;      /* scaling factor */
+    intn do_scale=FALSE;    /* flag to indicate whether to scale images */
+    float32 img_scale=(float32)1.0;  /* scaling factor */
     int32 xdim,ydim;        /* dimensions of the image to convert */
     intn ispal;             /* whether there's a palette with the image */
     uint8 *img_buf;         /* buffer to store the image in */
     uint8 *img24_buf;       /* buffer to store the 24-bit image in */
-    uint8 *pal_buf;         /* buffer to store the palette in */
+    uint8 *pal_buf=NULL;    /* buffer to store the palette in */
     intn file=1;            /* the arguement the files start at */
     intn i;                 /* local counting variable */
 
