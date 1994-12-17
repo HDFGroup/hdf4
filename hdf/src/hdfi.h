25d2bea44b2c673d7ddeb039ffb463f07f2699d6
Quincey Koziol <koziol@lbl.gov>
1994-12-17 16:00:37 -0600

    Removed "long" from int32 and uin32 definitions for SGI machines, which should make porting to the Power challenge much easier.

--- hdf/src/hdfi.h
+++ hdf/src/hdfi.h
@@ -278,8 +278,8 @@ typedef signed char        int8;
 typedef unsigned char      uint8;
 typedef short int          int16;
 typedef unsigned short int uint16;
-typedef long int           int32;
-typedef unsigned long int  uint32;
+typedef int                int32;
+typedef unsigned int       uint32;
 typedef int                intn;
 typedef unsigned int       uintn;
 typedef float              float32;
@@ -1070,21 +1070,21 @@ extern uint8 FAR *DFtbuf;
 **
 **---------------------------------------------------------------*/
 #if defined(FNAME_PRE_UNDERSCORE) && defined(FNAME_POST_UNDERSCORE)
-#ifdef __STDC__
+#if defined __STDC__ | defined CONVEX | defined DECCXX
 #   define FNAME(x)     _##x##_
 #else
 #   define FNAME(x)     _/**/x/**/_
 #endif
 #endif
 #if defined(FNAME_PRE_UNDERSCORE) && !defined(FNAME_POST_UNDERSCORE)
-#ifdef __STDC__
+#if defined __STDC__ | defined CONVEX | defined DECCXX
 #   define FNAME(x)     _##x
 #else
 #   define FNAME(x)     _/**/x
 #endif
 #endif
 #if !defined(FNAME_PRE_UNDERSCORE) && defined(FNAME_POST_UNDERSCORE)
-#ifdef __STDC__
+#if defined __STDC__ | defined CONVEX | defined DECCXX
 #   define FNAME(x)     x##_
 #else
 #   define FNAME(x)     x/**/_
