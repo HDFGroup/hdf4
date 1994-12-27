dbf548f5ae47be18e00365932f331f1de16d0c56
sxu <sxu@hdfgroup.org>
1994-12-27 21:35:27 +0000

    Cleanup debug message.

--- mfhdf/libsrc/cdf.c
+++ mfhdf/libsrc/cdf.c
@@ -846,10 +846,6 @@ NC_var **var;
       /* need to fake the size of the record dimension */
       if(val == NC_UNLIMITED) {
           if(handle->is_hdf)
-/*              val = (*var)->numrecs; Multi_dim SDS UNLIMITED dim  
-                                        is 1 less than the # of recs */
-/* 12/27              val = (rank > 1)? (*var)->numrecs + 1: (*var)->numrecs;
-*/
               val = (*var)->numrecs;
           else
               val = handle->numrecs;
