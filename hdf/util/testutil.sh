1a3c183161241ceb56f10ba1ca32ac96b1be258d
Albert Cheng <acheng@hdfgroup.org>
1996-01-24 16:15:00 -0600

    Added errors variable detection and report any errors encounterd when finished.   Currently, missing utilities are not considered as an error.

--- hdf/util/testutil.sh
+++ hdf/util/testutil.sh
@@ -2,6 +2,14 @@
 #
 # Testing HDF utilities
 
+# initialize errors variable
+errors=0
+
+echo ""
+echo "======================="
+echo "Utilities tests started"
+echo "======================="
+
 # hdf24to8
 if [ -f hdf24to8 -a -f hdftor8 ]; then
 echo "** Testing hdf24to8  ***"
@@ -10,7 +18,7 @@ cp testfiles/head.r24.Z testfiles/head.r8.Z .
 uncompress head.r24.Z head.r8.Z
 ./hdf24to8 head.r24 head8.hdf
 ./hdftor8 head8.hdf
-cmp img001-263.328 head.r8
+cmp img001-263.328 head.r8 || errors=1
 /bin/rm -f head.r24 head.r8 head8.hdf img001-263.328 pal001
 else
 echo "** hdf24to8 or hdftor8 is not available ***"
@@ -22,7 +30,7 @@ echo "** Testing hdfed  ***"
 /bin/rm -f storm110.hdf ntcheck.hdf hdfed.tmp
 cp testfiles/storm110.hdf testfiles/ntcheck.hdf .
 ./hdfed < hdfed.input1 > hdfed.tmp 2>&1
-diff hdfed.tmp hdfed.out1
+diff hdfed.tmp hdfed.out1 || errors=1
 /bin/rm -f storm110.hdf ntcheck.hdf hdfed.tmp
 else
 echo "** hdfed not available ***"
@@ -35,7 +43,7 @@ echo "** Testing ristosds  ***"
 cp testfiles/storm110.hdf testfiles/storm120.hdf testfiles/storm130.hdf .
 ./ristosds storm*.hdf -o storm.hdf
 ./hdfed < ristosds.input1 > hdfed.tmp1 2>&1
-diff hdfed.tmp1 ristosds.out1
+diff hdfed.tmp1 ristosds.out1 || errors=1
 /bin/rm -f storm*.hdf hdfed.tmp1
 else
 echo "** ristosds or hdfed not available ***"
@@ -50,7 +58,7 @@ cp testfiles/test.hdf .
 ./hdfpack -b test.hdf test.blk
 ./hdfls test.hdf > hdfls.tmp1 2>&1
 ./hdfls test.pck >> hdfls.tmp1 2>&1
-diff hdfls.tmp1 hdfpack.out1
+diff hdfls.tmp1 hdfpack.out1 || errors=1
 /bin/rm -f test.hdf test.blk test.pck hdfls.tmp1
 else
 echo "** hdfpack or hdfls not available ***"
@@ -63,7 +71,7 @@ echo "** Testing hdftopal/paltohdf  ***"
 cp testfiles/palette.raw .
 ./paltohdf palette.raw palette.hdf
 ./hdftopal palette.hdf palette.raw.new
-cmp palette.raw palette.raw.new
+cmp palette.raw palette.raw.new || errors=1
 /bin/rm -f palette.* 
 else
 echo "** hdftopal or paltohdf not available ***"
@@ -78,11 +86,11 @@ cp testfiles/storm*.raw testfiles/palette.raw .
 ./r8tohdf 57 57 storm.hdf -p palette.raw -i storm110.raw
 ./hdftor8 storm.hdf
 ./hdfls -l storm.hdf > hdfls.tmp2 2>&1
-diff hdfls.tmp2 hdftor8.out1
-cmp img001-057.057  storm110.raw
-cmp img002-057.057  storm120.raw
-cmp img003-057.057  storm130.raw
-cmp img004-057.057  storm140.raw
+diff hdfls.tmp2 hdftor8.out1 || errors=1
+cmp img001-057.057  storm110.raw || errors=1
+cmp img002-057.057  storm120.raw || errors=1
+cmp img003-057.057  storm130.raw || errors=1
+cmp img004-057.057  storm140.raw || errors=1
 /bin/rm -f storm* img* palette.raw hdfls.tmp2 pal005
 else
 echo "** r8tohdf, hdftor8 or hdfls not available ***"
@@ -97,7 +105,7 @@ cp testfiles/storm*.hdf .
 ./hdfcomp allcomp.hdf -c storm*.hdf
 ./hdfls -l allstorms.hdf > hdfls.tmp3 2>&1
 ./hdfls -l allcomp.hdf >> hdfls.tmp3 2>&1
-diff hdfls.tmp3 hdfcomp.out1
+diff hdfls.tmp3 hdfcomp.out1 || errors=1
 /bin/rm -f storm*.hdf all*.hdf hdfls.tmp3
 else
 echo "** hdfcomp or hdfls not available ***"
@@ -111,8 +119,8 @@ cp testfiles/jpeg_img.jpg .
 ./jpeg2hdf jpeg_img.jpg jpeg.hdf
 ./hdf2jpeg jpeg.hdf jpeg2.jpg
 ./hdfls -l jpeg.hdf > hdfls.tmp4 2>&1
-diff hdfls.tmp4 jpeg2hdf.out1
-cmp jpeg_img.jpg jpeg2.jpg
+diff hdfls.tmp4 jpeg2hdf.out1 || errors=1
+cmp jpeg_img.jpg jpeg2.jpg || errors=1
 /bin/rm -f jpeg.hdf jpeg_img.jpg jpeg2.jpg hdfls.tmp4
 else
 echo "** jpeg2hdf, hdf2jpeg or hdfls  not available ***"
@@ -139,10 +147,24 @@ echo "** Testing fp2hdf  ***"
 ./hdfls -l cb64r3.hdf >> hdfls.tmp5 2>&1
 ./hdfls -l ctxtr2_ris.hdf >> hdfls.tmp5 2>&1
 ./hdfls -l cb64r2_ris.hdf >> hdfls.tmp5 2>&1
-diff hdfls.tmp5 fp2hdf.out1
+diff hdfls.tmp5 fp2hdf.out1 || errors=1
 ./hdfed < fp2hdf.input1 > hdfed.tmp6 2>&1
 diff hdfed.tmp6 fp2hdf.out2
-/bin/rm -f ctxtr* cb* *.hdf hdfls.tmp5 hdfed.tmp6
+/bin/rm -f ctxtr* cb* *.hdf hdfls.tmp5 hdfed.tmp5
 else
 echo "** fp2hdf, hdfed or hdfls not available ***"
 fi
+
+#
+# Check errors result
+if [ $errors -eq 0 ]; then
+    echo "======================="
+    echo "Utilities tests passed."
+    echo "======================="
+else
+    echo "**********************************"
+    echo "Utilities tests encountered errors"
+    echo "**********************************"
+fi
+echo ""
+exit $errors
