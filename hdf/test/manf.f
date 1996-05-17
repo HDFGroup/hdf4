e53a477fa2f79f067885a90fb4407da0e677def7
georgev <georgev@hdfgroup.org>
1996-05-17 18:16:18 +0000

    Updated 'manf.f' to use VERIFY() instead of RESULT()

--- hdf/test/manf.f
+++ hdf/test/manf.f
@@ -24,9 +24,10 @@ C
 C  Input file:   none
 C  Output files: manf.hdf
 C
-      integer number_failed
       integer afstart, afend, afcreate, affcreate
       integer afwriteann, afendaccess, hopen, hclose
+      integer getverb
+      integer Verbosity, number_failed
 
       integer dssdims, dsadata, dslref, dsgdims
       integer d8aimg, DFR8lastref, d8gimg
@@ -65,6 +66,9 @@ C
       character*1 CR
       character image(ROWS, COLS), newimage(ROWS, COLS)
       real      data(ROWS, COLS)
+
+C     Set verbosity level
+      Verbosity=getverb()     
 
       number_failed = 0
       numberfailed =  0
@@ -110,48 +114,48 @@ C  *** generate float array and image ***
       call genimage(ROWS, COLS, data, image)
 
       ret = dssdims(rank,dimsizes)
-      call RESULT(ret, 'dssdims', number_failed)
+      call VERIFY(ret,'dssdims',number_failed,Verbosity)
 
 C  *** start annotation on file ***
       fhandle = hopen(TESTFILE,DFACC_CREATE, 0)
       ret = fhandle
-      call RESULT(ret, 'hopen', number_failed)
+      call VERIFY(ret,'fhanlde',number_failed,Verbosity)
       ahandle = afstart(fhandle)
       ret = ahandle
-      call RESULT(ret, 'afstart', number_failed)
+      call VERIFY(ret,'afstart',number_failed,Verbosity)
 
 C  *** write file 2 labels/ 2 descriptions ***
       anhandle = affcreate(ahandle, AN_FILE_LABEL)
       ret = anhandle
-      call RESULT(ret, 'affcreate', number_failed)
+      call VERIFY(ret, 'affcreate', number_failed, Verbosity)
       ret = afwriteann(anhandle,lab2,len(lab2))
-      call RESULT(ret, 'afwriteann', number_failed)
+      call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
       ret = afendaccess(anhandle)
-      call RESULT(ret, 'afendaccess', number_failed)
+      call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
       anhandle = affcreate(ahandle, AN_FILE_LABEL)
       ret = anhandle
-      call RESULT(ret, 'affcreate', number_failed)
+      call VERIFY(ret, 'affcreate', number_failed, Verbosity)
       ret = afwriteann(anhandle,lab1,len(lab1))
-      call RESULT(ret, 'afwriteann', number_failed)
+      call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
       ret = afendaccess(anhandle)
-      call RESULT(ret, 'afendaccess', number_failed)
+      call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
       anhandle = affcreate(ahandle, AN_FILE_DESC)
       ret = anhandle
-      call RESULT(ret, 'affcreate', number_failed)
+      call VERIFY(ret, 'affcreate', number_failed, Verbosity)
       ret = afwriteann(anhandle,desc2,len(desc2))
-      call RESULT(ret, 'afwriteann', number_failed)
+      call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
       ret = afendaccess(anhandle)
-      call RESULT(ret, 'afendaccess', number_failed)
+      call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
       anhandle = affcreate(ahandle, AN_FILE_DESC)
       ret = anhandle
-      call RESULT(ret, 'affcreate', number_failed)
+      call VERIFY(ret, 'affcreate', number_failed, Verbosity)
       ret = afwriteann(anhandle,desc1,len(desc1))
-      call RESULT(ret, 'afwriteann', number_failed)
+      call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
       ret = afendaccess(anhandle)
-      call RESULT(ret, 'afendaccess', number_failed)
+      call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
 C  ***  Write data labels and descriptions ***
 
@@ -160,7 +164,7 @@ C  ***  Write data labels and descriptions ***
       do 100 j=1,REPS
 C  ***  write out scientific data set 
          ret = dsadata(TESTFILE, rank,dimsizes, data)
-         call RESULT(ret, 'dsadata', number_failed)
+         call VERIFY(ret, 'dsadata', number_failed, Verbosity)
 
 C ****    write out annotations for 2 out of every 3 
          if (mod(j,3) .ne. 0) then 
@@ -169,93 +173,92 @@ C ****    write out annotations for 2 out of every 3
 C ********** Write out 2 labels for each SDS *****************
             anhandle = afcreate(ahandle,DFTAG_SDG,refnum,AN_DATA_LABEL)
             ret = anhandle
-            call RESULT(ret, 'afcreate', number_failed)
+            call VERIFY(ret, 'afcreate', number_failed, Verbosity)
             ret = afwriteann(anhandle,labsds2,len(labsds2))
-            call RESULT(ret, 'afwriteann', number_failed)
+            call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
             ret = afendaccess(anhandle)
-            call RESULT(ret, 'afendaccess', number_failed)
+            call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
             anhandle = afcreate(ahandle,DFTAG_SDG,refnum,AN_DATA_LABEL)
             ret = anhandle
-            call RESULT(ret, 'afcreate', number_failed)
+            call VERIFY(ret, 'afcreate', number_failed, Verbosity)
             ret = afwriteann(anhandle,labsds,len(labsds))
-            call RESULT(ret, 'afwriteann', number_failed)
+            call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
             ret = afendaccess(anhandle)
-            call RESULT(ret, 'afendaccess', number_failed)
+            call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
 C *********** Write out 2 descritptions for each SDS ***********
             anhandle = afcreate(ahandle,DFTAG_SDG,refnum,AN_DATA_DESC)
             ret = anhandle
-            call RESULT(ret, 'afcreate', number_failed)
+            call VERIFY(ret, 'afcreate', number_failed, Verbosity)
             ret = afwriteann(anhandle,descsds2,len(descsds2))
-            call RESULT(ret, 'afwriteann', number_failed)
+            call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
             ret = afendaccess(anhandle)
-            call RESULT(ret, 'afendaccess', number_failed)
+            call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
             anhandle = afcreate(ahandle,DFTAG_SDG,refnum,AN_DATA_DESC)
             ret = anhandle
-            call RESULT(ret, 'afcreate', number_failed)
+            call VERIFY(ret, 'afcreate', number_failed, Verbosity)
             ret = afwriteann(anhandle,descsds,len(descsds))
-            call RESULT(ret, 'afwriteann', number_failed)
+            call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
             ret = afendaccess(anhandle)
-            call RESULT(ret, 'afendaccess', number_failed)
+            call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
          endif
 
          ret = d8aimg(TESTFILE, image, COLS, ROWS, 0)
-         call RESULT(ret, 'd8aimg', number_failed)
+         call VERIFY(ret, 'd8aimg', number_failed, Verbosity)
          refnum = DFR8lastref()
 
 C ********** Write out 2 labels for each Image *****************
           anhandle = afcreate(ahandle, DFTAG_RIG, refnum, AN_DATA_LABEL)
           ret = anhandle
-          call RESULT(ret, 'afcreate', number_failed)
+          call VERIFY(ret, 'afcreate', number_failed, Verbosity)
           ret = afwriteann(anhandle,labris2,len(labris2))
-          call RESULT(ret, 'afwriteann', number_failed)
+          call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
           ret = afendaccess(anhandle)
-          call RESULT(ret, 'afendaccess', number_failed)
+          call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
           anhandle = afcreate(ahandle, DFTAG_RIG, refnum, AN_DATA_LABEL)
           ret = anhandle
-          call RESULT(ret, 'afcreate', number_failed)
+          call VERIFY(ret, 'afcreate', number_failed, Verbosity)
           ret = afwriteann(anhandle,labris,len(labris))
-          call RESULT(ret, 'afwriteann', number_failed)
+          call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
           ret = afendaccess(anhandle)
-          call RESULT(ret, 'afendaccess', number_failed)
+          call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
 C *********** Write out 2 descritptions for each Image ***********
           anhandle = afcreate(ahandle, DFTAG_RIG, refnum, AN_DATA_DESC)
           ret = anhandle
-          call RESULT(ret, 'afcreate', number_failed)
+          call VERIFY(ret, 'afcreate', number_failed, Verbosity)
           ret = afwriteann(anhandle,descris2,len(descris2))
-          call RESULT(ret, 'afwriteann', number_failed)
+          call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
           ret = afendaccess(anhandle)
-          call RESULT(ret, 'afendaccess', number_failed)
+          call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
           anhandle = afcreate(ahandle, DFTAG_RIG, refnum, AN_DATA_DESC)
           ret = anhandle
-          call RESULT(ret, 'afcreate', number_failed)
+          call VERIFY(ret, 'afcreate', number_failed, Verbosity)
           ret = afwriteann(anhandle,descris,len(descris))
-          call RESULT(ret, 'afwriteann', number_failed)
+          call VERIFY(ret, 'afwriteann', number_failed, Verbosity)
           ret = afendaccess(anhandle)
-          call RESULT(ret, 'afendaccess', number_failed)
+          call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
   100 continue
 
 C ******* End writing annotatons **********
       ret = afend(ahandle)
-      call RESULT(ret, 'afend', number_failed)
+      call VERIFY(ret, 'afend', number_failed, Verbosity)
       ret = hclose(fhandle)
-      call RESULT(ret, 'hclose', number_failed)
+      call VERIFY(ret, 'hclose', number_failed, Verbosity)
 
 
 C********  Read data labels and descriptions *********
-      print *, CR, CR
       print *,'*** Reading labels and descriptions for SDS and RIS ***'
 
       do 200 j=1,REPS
           ret = dsgdims(TESTFILE, rank,dimsizes,3)
-          call RESULT(ret, 'dsgdims', number_failed)
+          call VERIFY(ret, 'dsgdims', number_failed, Verbosity)
           refnum = dslref()
 
 C ******  read in annotations for 2 out of every 3 
@@ -269,7 +272,7 @@ C ******  read in annotations for 2 out of every 3
 
 C ****    read annotations for images
           ret = d8gimg(TESTFILE, newimage, COLS, ROWS, pal)
-          call RESULT(ret, 'd8gimg', number_failed)
+          call VERIFY(ret, 'd8gimg', number_failed, Verbosity)
           refnum = DFR8lastref()
           call man_check_lab_desc(TESTFILE, DFTAG_RIG, refnum, 
      *                        labris, descris, numberfailed)
@@ -280,17 +283,14 @@ C ****    read annotations for images
   200 continue
 
 C ****** Check file labels/descriptions *******
-      print *, CR, CR
       print *,'*** Reading file labels and descriptions ***'
 
       call check_fan(TESTFILE, 0, lab1, desc1, numberfailed)
       call check_fan(TESTFILE, 1, lab2, desc2, numberfailed)
 
       if ( numberfailed .eq. 0 ) then
-          print *, CR, CR
-          print *,'***** ALL TESTS SUCCESSFUL ***** '
+          print *,'***** ALL ANxxx TESTS SUCCESSFUL ***** '
       else
-          print *, CR, CR
           print *,'***** ',numberfailed,' TESTS FAILED ***** '
       endif
 
@@ -317,6 +317,9 @@ C**************************************************************
       integer affileinfo, afnumann, afannlist, afannlen
       integer afreadann, afstart, afend, afendaccess, hopen, hclose
 
+      integer getverb
+      integer Verbosity
+
       integer fileh, anh
       integer nflabs, nfdescs, nolabs, nodescs
       integer numdlabels, numddescs
@@ -332,28 +335,31 @@ C**************************************************************
       AN_FILE_LABEL = 2
       AN_FILE_DESC  = 3
 
+C     Set verbosity level
+      Verbosity=getverb()     
+
 C *****start annotation access on file *****
       fileh = hopen(fname, DFACC_READ,0)
       ret = fileh
-      call RESULT(ret, 'hopen', number_failed)
+      call VERIFY(ret, 'hopen', number_failed, Verbosity)
       anh = afstart(fileh)
       ret = anh
-      call RESULT(ret, 'afstart', number_failed)
+      call VERIFY(ret, 'afstart', number_failed, Verbosity)
 
       ret = affileinfo(anh,nflabs,nfdescs,nolabs,nodescs)
-      call RESULT(ret, 'affileinfo', number_failed)
+      call VERIFY(ret, 'affileinfo', number_failed, Verbosity)
 
       numdlabels = afnumann(anh, AN_DATA_LABEL, tag, ref)
-      call RESULT(numdlabels, 'afnumann', number_failed)
+      call VERIFY(numdlabels, 'afnumann', number_failed, Verbosity)
 
       numddescs = afnumann(anh, AN_DATA_DESC, tag, ref)
-      call RESULT(numddescs, 'afnumann', number_failed)
+      call VERIFY(numddescs, 'afnumann', number_failed, Verbosity)
 
       ret = afannlist(anh, AN_DATA_LABEL, tag, ref, dlabels)
-      call RESULT(ret, 'afannlist', number_failed)
+      call VERIFY(ret, 'afannlist', number_failed, Verbosity)
 
       ret = afannlist(anh, AN_DATA_DESC, tag, ref, ddescs)
-      call RESULT(ret, 'afannlist', number_failed)
+      call VERIFY(ret, 'afannlist', number_failed, Verbosity)
 
 C ***** Look for label in list ******
       found = 0
@@ -361,12 +367,12 @@ C ***** Look for label in list ******
       fannlabel = ' '
       do 300 j=1, numdlabels
          annlen = afannlen(dlabels(j))
-         call RESULT(annlen, 'afannlen', number_failed)
+         call VERIFY(annlen, 'afannlen', number_failed, Verbosity)
 
          ret = afreadann(dlabels(j), inlabel, MAXLENLAB)
-         call RESULT(ret, 'afreadann', number_failed)
+         call VERIFY(ret, 'afreadann', number_failed, Verbosity)
          ret = afendaccess(dlabels(j))
-         call RESULT(ret, 'afendaccess', number_failed)
+         call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
          if (inlabel .eq. label) then
             found = 1
@@ -397,12 +403,12 @@ C ***** look for description in list
       fanndesc = ' '
       do 400 j=1, numddescs
          annlen = afannlen(ddescs(j))
-         call RESULT(annlen, 'afannlen', number_failed)
+         call VERIFY(annlen, 'afannlen', number_failed, Verbosity)
 
          ret = afreadann(ddescs(j), indesc, MAXLEN_DESC)
-         call RESULT(ret, 'afreadann', number_failed)
+         call VERIFY(ret, 'afreadann', number_failed, Verbosity)
          ret = afendaccess(ddescs(j))
-         call RESULT(ret, 'afendaccess', number_failed)
+         call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
          if (indesc .eq. desc) then
             found = 1
@@ -428,9 +434,9 @@ C ***** look for description in list
 
 C ****** close file *******
       ret = afend(anh)
-      call RESULT(ret, 'afend', number_failed)
+      call VERIFY(ret, 'afend', number_failed, Verbosity)
       ret = hclose(fileh)
-      call RESULT(ret, 'hclose', number_failed)
+      call VERIFY(ret, 'hclose', number_failed, Verbosity)
 
 
       return
@@ -452,6 +458,9 @@ C************************************************************
       integer affileinfo, afselect, afannlen, afreadann
       integer afstart, afend, afendaccess, hopen, hclose
 
+      integer getverb
+      integer Verbosity
+
       integer ret
       integer fileh, annh, anh
       integer nflabs, nfdescs, nolabs, nodescs
@@ -466,28 +475,31 @@ C************************************************************
       AN_FILE_LABEL = 2
       AN_FILE_DESC  = 3
 
+C     Set verbosity level
+      Verbosity=getverb()     
+
 C **** We check both file label/description
       fileh = hopen(fname, DFACC_READ,0)
       ret = fileh
-      call RESULT(ret, 'hopen', number_failed)
+      call VERIFY(ret, 'hopen', number_failed, Verbosity)
       anh = afstart(fileh)
       ret = anh
-      call RESULT(ret, 'afstart', number_failed)
+      call VERIFY(ret, 'afstart', number_failed, Verbosity)
 
       ret = affileinfo(anh,nflabs,nfdescs,nolabs,nodescs)
-      call RESULT(ret, 'affileinfo', number_failed)
+      call VERIFY(ret, 'affileinfo', number_failed, Verbosity)
 
 C ***** Read file label **********
       annh = afselect(anh, index, AN_FILE_LABEL)
-      call RESULT(ret, 'afselect', number_failed)
+      call VERIFY(ret, 'afselect', number_failed, Verbosity)
 
       fannlen = afannlen(annh)
-      call RESULT(fannlen, 'afannlen', number_failed)
+      call VERIFY(fannlen, 'afannlen', number_failed, Verbosity)
 
       ret = afreadann(annh, flabel, fannlen)
-      call RESULT(ret, 'afreadann', number_failed)
+      call VERIFY(ret, 'afreadann', number_failed, Verbosity)
       ret = afendaccess(annh)
-      call RESULT(ret, 'afendaccess', number_failed)
+      call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
       if (fannlen .ne. len(label)) then
          print *,'   >>>BAD LABEL LENGTH.'
@@ -505,15 +517,15 @@ C ***** Read file label **********
 
 C **** Read file description *****
       annh = afselect(anh, index, AN_FILE_DESC)
-      call RESULT(ret, 'afselect', number_failed)
+      call VERIFY(ret, 'afselect', number_failed, Verbosity)
 
       fannlen = afannlen(annh)
-      call RESULT(fannlen, 'afannlen', number_failed)
+      call VERIFY(fannlen, 'afannlen', number_failed, Verbosity)
 
       ret = afreadann(annh, fdesc, fannlen)
-      call RESULT(ret, 'afreadann', number_failed)
+      call VERIFY(ret, 'afreadann', number_failed, Verbosity)
       ret = afendaccess(annh)
-      call RESULT(ret, 'afendaccess', number_failed)
+      call VERIFY(ret, 'afendaccess', number_failed, Verbosity)
 
       if (fannlen .ne. len(desc)) then
           print *,'   >>>BAD DESCRIPTION LENGTH.' 
@@ -531,9 +543,9 @@ C **** Read file description *****
 
 C ****** close file *******
       ret = afend(anh)
-      call RESULT(ret, 'afend', number_failed)
+      call VERIFY(ret, 'afend', number_failed, Verbosity)
       ret = hclose(fileh)
-      call RESULT(ret, 'hclose', number_failed)
+      call VERIFY(ret, 'hclose', number_failed, Verbosity)
 
       return
       end
