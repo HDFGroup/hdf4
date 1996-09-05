0a1c17762fc5a57318b60e85c0a44a3bfef0b364
sxu <sxu@hdfgroup.org>
1996-09-05 15:03:10 +0000

    Initail version. Vset attributes Fortran routines.

---
+++ hdf/src/vattrf.c
@@ -0,0 +1,336 @@
+/***********************************************************
+ * NCSA HDF
+    *
+ * Software Development Group
+    *
+ * National Center for Supercomputing Applications
+    *
+ * University of Illinois at Urbana-Champaign
+    *
+ * 605 E. Springfield, Champaign IL 61820
+    *
+ *
+    *
+ * For conditions of distribution and use, see the accompanying
+    *
+ * hdf/COPYING file.
+    *
+ *
+    *
+ ********************************************************/
+
+#ifdef RCSID
+static char RcsId[] = "@(#)$";
+#endif
+
+/* $Id$ */
+
+/*
+   *
+   * vattrf.c
+   * Part of the HDF Vset attribute interface.
+   *
+   * C routines (short names) to be called from fortran
+   *
+   *
+   ******************************************************/
+
+#include "vg.h"
+
+/* ----------------- vsfcfdx ---------------------- 
+ *  get field index
+ *  VSfindex -- vsfcfdx -- vsffidx
+ */
+
+FRETVAL(intf)
+nvsfcfdx(intf *vsid, _fcd fldnm, intf *findex,
+         intf *fldnmlen)
+{
+    intf  ret;
+    char *fld;
+
+    fld = HDf2cstring(fldnm, (intn) *fldnmlen);
+    if (!fld) return(FAIL);
+    ret = (intf) VSfindex((int32) *vsid, fld, (int32 *) findex);
+    HDfree(fld);
+    return(ret);
+}
+
+/* -------------------------------------------------
+ * vsfcsat -- set a numeric attr for a vdata or a field
+ *            of a vdata
+ * VSsetattr -- vsfcsat -- vsfsat
+ */
+
+FRETVAL(intf)
+nvsfcsat(intf *vsid, intf *findex, _fcd attrnm, intf *dtype,
+         intf *count, intf *values, intf *attrnmlen)
+{
+    intf  ret;
+    char *attrname;
+
+    attrname = HDf2cstring(attrnm, (intn) *attrnmlen);
+    if (!attrname) return(FAIL);
+    ret = (intf )VSsetattr((int32) *vsid, (int32) *findex, attrname,
+              (int32) *dtype, (int32) *count, (VOIDP) values);
+    HDfree(attrname);
+    return(ret);
+} 
+
+/* ----------------------------------------------------
+ * vsfcsca -- set char attr for vdata or its field
+ * 
+ * VSsetattr -- vsfcsca -- vsfscat
+ */
+
+FRETVAL(intf)
+nvsfcsca(intf *vsid, intf *findex, _fcd attrnm, intf *dtype,
+         intf *count, _fcd values, intf *attrnmlen)
+{
+    intf ret;
+    char *attrname;
+ 
+    attrname = HDf2cstring(attrnm, (intn) *attrnmlen);
+    if (!attrname) return(FAIL);
+    ret = (intf) VSsetattr((int32) *vsid, (int32) *findex, attrname,
+          (int32) *dtype, (int32) *count, (VOIDP) _fcdtocp(values));
+    HDfree(attrname);
+    return(ret);
+}
+
+/* -------------------------------------------------------
+ * vsfnat -- get total number of attributes of a vdata and
+ *             its fields
+ * VSnattr -- vsfnat
+ */
+
+FRETVAL(intf)
+nvsfnat(intf *vsid)
+{
+   intf ret;
+
+   ret = (intf) VSnattrs((int32) *vsid); 
+   return(ret);
+}
+
+/* -------------------------------------------------------
+ * vsffnat -- get number of attributes of a vdata or of a field
+ *
+ * VSfnattrs -- vsffnat
+ */
+
+FRETVAL(intf)
+nvsffnat(intf *vsid, intf *findex)
+{ 
+    intf ret;
+    
+    ret = (intf) VSfnattrs((int32) *vsid, (int32) *findex);
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vsfcfda -- find attribute of a vdata of a field
+ *               of the vdata
+ *    VSfindattr -- vsfcfda -- vsffdat
+ */
+
+FRETVAL(intf)
+nvsfcfda(intf *vsid, intf *findex, _fcd attrnm, intf *attrnmlen)
+{
+    intf ret;
+    char *attrname;
+
+    attrname = HDf2cstring(attrnm, (intn) *attrnmlen);
+    if (!attrname) return(FAIL);
+    ret = (intf) VSfindattr((int32) *vsid, (int32) *findex, attrname);
+    HDfree(attrname);
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vsfainf -- get attribute info
+ * VSattrinfo -- vsfainf
+ */
+
+FRETVAL(intf)
+nvsfainf(intf *vsid, intf *findex, intf *aindex, _fcd attrname,
+         intf *dtype, intf *count, intf *size)
+{
+    intf ret;
+    ret = (intf) VSattrinfo((int32) *vsid, (int32) *findex, (int32) *aindex,
+          _fcdtocp(attrname), (int32 *) dtype, (int32 *) count, 
+          (int32 *) size);
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vsfgat -- get values of a numeric attribute 
+ * VSgetattr -- vsfgat
+ */
+
+FRETVAL(intf)
+nvsfgat(intf *vsid, intf *findex, intf *aindex, intf *values)
+{
+    intf ret;
+    ret = (intf) VSgetattr((int32) *vsid, (int32) *findex, (int32) *aindex,
+                    (VOIDP) values);
+    return(ret);
+}
+
+/* --------------------------------------------------------
+ * vsfgcat -- get values of a char type attribute 
+ * VSgetattr -- vsfgcat
+ */
+
+FRETVAL(intf)
+nvsfgcat(intf *vsid, intf *findex, intf *aindex, _fcd *values)
+{
+    intf ret;
+    ret = (intf )VSgetattr((int32) *vsid, (int32) *findex, (int32) *aindex,
+                    (VOIDP) _fcdtocp(values));
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vsfisat -- test if a vdata is an attribute of other object
+ * VSisattr -- vsfisat
+ */
+
+FRETVAL(intf)
+nvsfisat(intf *vsid)
+{
+    intf ret;
+    ret = (intf) VSisattr((int32) *vsid);
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vfcsatt -- set a numeric attr for a vgroup
+ * Vsetattr -- vfcsatt -- vfsatt
+ */
+
+FRETVAL(intf)
+nvfcsatt(intf *vgid, _fcd attrnm, intf *dtype,
+         intf *count, intf *values, intf *attrnmlen)
+{
+    intf  ret;
+    char *attrname;
+
+    attrname = HDf2cstring(attrnm, (intn) *attrnmlen);
+    if (!attrname) return(FAIL);
+    ret = (intf) Vsetattr((int32) *vgid, attrname, (int32) *dtype, 
+                   (int32) *count, (VOIDP) values);
+    HDfree(attrname);
+    return(ret);
+} 
+
+/* ----------------------------------------------------
+ * vfcscat -- set char attr for vgroup
+ * 
+ * Vsetattr -- vfcscat -- vfscatt
+ */
+
+FRETVAL(intf)
+nvfcscat(intf *vgid, _fcd attrnm, intf *dtype, intf *count, 
+         _fcd values, intf *attrnmlen)
+{
+    intf ret;
+    char *attrname;
+ 
+    attrname = HDf2cstring(attrnm, (intn) *attrnmlen);
+    if (!attrname) return(FAIL);
+    ret = (intf) Vsetattr((int32) *vgid, attrname, (int32) *dtype, 
+           (int32) *count, (VOIDP) _fcdtocp(values));
+    HDfree(attrname);
+    return(ret);
+}
+
+/* -------------------------------------------------------
+ * vfnatts -- get number of attributes of a vgroup 
+ * Vnattrs -- vfnatts
+ */
+
+FRETVAL(intf)
+nvfnatts(intf *vgid)
+{
+   intf ret;
+
+   ret = (intf) Vnattrs((int32) *vgid); 
+   return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vfcfdat -- find attribute of a vgroup
+ * Vfindattr -- vfcfdat -- vffdatt
+ */
+
+FRETVAL(intf)
+nvfcfdat(intf *vgid, _fcd attrnm, intf *attrnmlen)
+{
+    intf ret;
+    char *attrname;
+
+    attrname = HDf2cstring(attrnm, (intn) *attrnmlen);
+    if (!attrname) return(FAIL);
+    ret = (intf) Vfindattr((int32) *vgid, attrname);
+    HDfree(attrname);
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vfainf -- get attribute info
+ * Vattrinfo -- vfainfo
+ */
+
+FRETVAL(intf)
+nvfainfo(intf *vgid, intf *aindex, _fcd attrname,
+         intf *dtype, intf *count, intf *size)
+{
+    intf ret;
+    ret = (intf) Vattrinfo((int32) *vgid, (int32) *aindex,
+          _fcdtocp(attrname), (int32 *) dtype, (int32 *) count, 
+          (int32 *) size);
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vfgatt -- get values of a numeric attribute 
+ * Vgetattr -- vfgatt
+ */
+
+FRETVAL(intf)
+nvfgatt(intf *vgid, intf *aindex, intf *values)
+{
+    intf ret;
+    ret = (intf) Vgetattr((int32) *vgid, (int32) *aindex,
+                    (VOIDP) values);
+    return(ret);
+}
+
+/* --------------------------------------------------------
+ * vfgcatt -- get values of a char type attribute 
+ * Vgetattr -- vfgcatt
+ */
+
+FRETVAL(intf)
+nvfgcatt(intf *vgid, intf *aindex, _fcd *values)
+{
+    intf ret;
+    ret = (intf) Vgetattr((int32) *vgid, (int32) *aindex,
+                    (VOIDP) _fcdtocp(values));
+    return(ret);
+}
+
+/* ---------------------------------------------------------
+ * vfgver -- get version number of a vgroup
+ * Vgetversion -- vfgver
+ */
+
+FRETVAL(intf)
+nvfgver(intf *vgid)
+{
+    intf ret;
+    ret = (intf) Vgetversion((int32) *vgid);
+    return(ret);
+}
+
---
+++ hdf/src/vattrff.f
@@ -0,0 +1,128 @@
+C***********************************************************
+C * NCSA HDF
+C   *
+C * Software Development Group
+C   *
+C * National Center for Supercomputing Applications
+C   *
+C * University of Illinois at Urbana-Champaign
+C   *
+C * 605 E. Springfield, Champaign IL 61820
+C   *
+C *
+C   *
+C * For conditions of distribution and use, see the accompanying
+C   *
+C * hdf/COPYING file.
+C   *
+C *
+C   *
+C***************************************************
+C
+C $Id$ 
+C
+C **************************************************
+C *
+C * vattrff.f
+C * Vset attribute Fortran routines
+C *
+C *************************************************
+
+C --------------- vsffidx ------------------------
+C    vsffidx -- find index of a named field in a vdata
+C    VSfindex -- vsfcfdx -- vsffidx
+C
+       integer function vsffidx(vsid, fldnm, fldidx) 
+       integer vsid
+       character*(*) fldnm
+       integer fldidx
+       integer vsfcfdx
+
+       vsffidx = vsfcfdx(vsid, fldnm, fldidx, 
+     +                  len(fldnm))
+       end
+C --------------- vsfsat -------------------------
+C    vsfsat -- set a numeric attribute for a vdata 
+C              or a field of a vdata
+C    VSsetattr -- vsfcsat -- vsfsat
+C
+       integer function vsfsat(vsid, findex,attrnm,dtype,
+     +                        count, values)
+       integer vsid, findex, dtype, count
+       character*(*) attrnm
+       integer  values
+       integer vsfcsat
+     
+       vsfsat = vsfcsat(vsid, findex, attrnm, dtype,
+     +                count, values, len(attrnm))
+       end
+C --------------- vsfscat -------------------------
+C    vsfscat -- set a char type attribute for a vdata 
+C              or a field of a vdata
+C    VSsetattr -- vsfcsca -- vsfscat
+C
+       integer function vsfscat(vsid, findex,attrnm,dtype,
+     +                      count, values)
+       integer vsid, findex, dtype, count
+       character*(*) attrnm
+       character*(*) values
+       integer vsfcsca
+     
+       vsfscat = vsfcsca(vsid, findex, attrnm, dtype,
+     +         count, values, len(attrnm), len(values))
+       end
+C --------------- vsffdat -------------------------
+C    vsffdat -- find attribute of a vdata of a field
+C               of the vdata
+C    VSfindattr -- vsfcfda -- vsffdat
+C
+       integer function vsffdat(vsid, findex, attrnm)
+       integer vsid, findex
+       character*(*) attrnm
+       integer vsfcfda
+
+       vsffdat = vsfcfda(vsid, findex, attrnm, 
+     +                   len(attrnm))
+       end 
+C --------------- vfsatt -------------------------
+C    vfsatt -- set a numeric attr for a vgroup
+C    Vsetattr -- vfcsatt -- vfsatt 
+C
+       integer function vfsatt(vgid, attrnm, dtype, 
+     +                        count, values)
+       integer vgid, dtype, count, values
+       character*(*) attrnm
+       integer vfcsatt
+
+       vfsatt = vfcsatt(vgid, attrnm, dtype,count,
+     +                  values, len(attrnm))
+       end
+C -------------- vfscatt -------------------------
+C    vfscatt -- set a char attr for a vgroup
+C    Vsetattr -- vfcscat -- vfscatt
+C
+       integer function vfscatt(vgid, attrnm, dtype,
+     +                        count, values)
+       integer vgid, dtype, count
+       character*(*) attrnm, values
+       integer vfcscat
+
+       vfscatt = vfcscat(vgid, attrnm, dtype,count,
+     +                  values, len(attrnm))
+       end
+C --------------- vffdatt ---------------------
+C    vffdatt -- find an attribute of a vgroup
+C    Vfindattr -- vfcfdat -- vffdatt
+C
+       integer function vffdatt(vg, attrnm)
+       integer vg
+       character*(*)   attrnm
+       integer vfcfdat
+
+       vffdatt = vfcfdat(vg, attrnm, len(attrnm))
+       end
+C --------------------------------------------
+
+ 
+
+     
