16cbac204ca24b6ee694afd6bd2b4cbd6fb11368
Barbara Jones <bljones@hdfgroup.org>
1996-11-19 10:26:46 -0600

    changed LIBVER_MINOR, LIBVER_RELEASE, and LIBVER_STRING for 4.1 Beta 0

--- hdf/src/hfile.h
+++ hdf/src/hfile.h
@@ -47,9 +47,9 @@
 /* Library version numbers */
 
 #define LIBVER_MAJOR    4
-#define LIBVER_MINOR    0
-#define LIBVER_RELEASE  2
-#define LIBVER_STRING   "NCSA HDF Version 4.1 Beta 0, Sept 20, 1996"
+#define LIBVER_MINOR    1 
+#define LIBVER_RELEASE  0
+#define LIBVER_STRING   "NCSA HDF Version 4.1 Beta 0, Dec 1, 1996"
 #define LIBVSTR_LEN    80   /* length of version string  */
 #define LIBVER_LEN  92      /* 4+4+4+80 = 92 */
 /* end of version tags */
