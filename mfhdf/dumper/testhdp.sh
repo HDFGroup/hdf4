0fad7995a4c1f2ea191a2889d4b9d7730af3e272
Albert Cheng <acheng@hdfgroup.org>
1997-02-06 13:46:54 -0600

    Added options for specific hdp command test which could be useful for debugging time.

--- mfhdf/dumper/testhdp.sh
+++ mfhdf/dumper/testhdp.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 # $Id$
-# Test scripts for hdp (dumper)
+# Test scripts for hdp (dumper).
+# See the USAGE function for command usage.
 
 
 # Definitions of commands and variables
@@ -10,32 +11,22 @@ DIFF=diff
 nerrors=0		# number of errors (0)
 quitonerr=0		# quit on error (not)
 noclean=0		# no cleaning temp. files (yes)
+command=""		# dumper sub-command to test
 
-
-# parse arguments
-while [ $# -gt 0 ]
-do
-    case "$1" in
-	"-quit")
-	    quitonerr=1
-	    ;;
-	"-noclean")
-	    noclean=1
-	    ;;
-	"-help" | * )
-	    echo "Usage: $0 [-help] [-quit] [-noclean]"
-	    echo "    -help: display help information"
-	    echo "    -noclean: do not clean away temporary files"
-	    echo "    -quit: quit immediately if any test fails"
-	    exit 0
-	    ;;
-    esac
-    shift
-done
-    
 
 # Definitions of functions/shorthands
 #
+
+# Print Usage of the command
+USAGE()
+{
+    echo "Usage: $0 [-help] [-noclean] [-quit] [-command <command>]"
+    echo "    -help: display help information"
+    echo "    -noclean: do not clean away temporary files"
+    echo "    -quit: quit immediately if any test fails"
+    echo "    -command: test one specific command where <command> can be"
+    echo "        {list, dumpsds, dumprig, dumpvd, dumpvg, dumpgr}"
+}
 
 # Print message with formats according to message level ($1)
 MESG()
@@ -115,8 +106,51 @@ FINISH()
 }
 
 
+#===============
+# Main Body
+#===============
+
+# parse arguments
+while [ $# -gt 0 ]
+do
+    case "$1" in
+	"-quit")
+	    quitonerr=1
+	    ;;
+	"-noclean")
+	    noclean=1
+	    ;;
+	"-help")
+	    USAGE
+	    exit 0
+	    ;;
+	"-command")
+	    shift
+	    case "$1" in
+    		"list"|"dumpsds"|"dumprig"|"dumpvd"|"dumpvg"|"dumpgr")
+		    command="$1"
+		    ;;
+		*)
+		    echo "Unknown command: $1"
+		    USAGE
+		    exit 1
+		    ;;
+	    esac
+	    ;;
+	* )
+	    echo "Unknow option: $1"
+	    USAGE
+	    exit 1
+	    ;;
+    esac
+    shift
+done
+    
+
 # Test command list
 MESG 3 Test command list
+if [ ! $command -o $command = list ]
+then
 TEST list-1.out list tdata.hdf
 TEST list-2.out list -l tdata.hdf
 TEST list-3.out list -d tdata.hdf
@@ -127,9 +161,14 @@ TEST list-7.out list -g tdata.hdf
 TEST list-8.out list -a tdata.hdf
 TEST list-9.out list -a Example6.hdf
 TEST list-10.out list -n Example6.hdf
+else
+    echo "   <<< SKIPPED >>>"
+fi
 
 # Test command dumpsds
 MESG 3 Test command dumpsds
+if [ ! $command -o $command = dumpsds ]
+then
 TEST dumpsds-1.out dumpsds swf32.hdf
 TEST dumpsds-2.out dumpsds -i 2 swf32.hdf
 TEST dumpsds-3.out dumpsds -i 1,3 swf32.hdf
@@ -138,18 +177,28 @@ TEST dumpsds-4.out dumpsds -n Time swf32.hdf
 TEST dumpsds-5.out dumpsds -n fakeDim0,Data-Set-2 swf32.hdf
 TEST dumpsds-6.out dumpsds -r 3,2 swf32.hdf
 TEST dumpsds-7.out dumpsds -r 3,2 -d swf32.hdf
+else
+    echo "   <<< SKIPPED >>>"
+fi
 
 # Test command dumprig
 MESG 3 Test command dumprig
+if [ ! $command -o $command = dumprig ]
+then
 TEST dumprig-1.out dumprig tdf24.hdf
 TEST dumprig-2.out dumprig -i 1,2 tdf24.hdf
 TEST dumprig-3.out dumprig -i 1,3 tdf24.hdf		# '-i 3' is invalid
 TEST dumprig-4.out dumprig -m 24 tdf24.hdf
 TEST dumprig-5.out dumprig -r 3,4 tdf24.hdf
 TEST dumprig-6.out dumprig -r 3,4 -d tdf24.hdf
+else
+    echo "   <<< SKIPPED >>>"
+fi
 
 # Test command dumpvd
 MESG 3 Test command dumpvd
+if [ ! $command -o $command = dumpvd ]
+then
 TEST dumpvd-1.out dumpvd tvset.hdf
 TEST dumpvd-2.out dumpvd -i 1,3,5 tvset.hdf
 TEST dumpvd-3.out dumpvd -r 1238,1239,1251,1252 tvset.hdf
@@ -160,9 +209,14 @@ TEST dumpvd-7.out dumpvd -f B tvset.hdf
 TEST dumpvd-8.out dumpvd -f "STATION_NAME","FLOATS" tvset.hdf
 TEST dumpvd-9.out dumpvd -f "STATION_NAME","FLOATS" -d tvset.hdf
 TEST dumpvd-10.out dumpvd tvattr.hdf
+else
+    echo "   <<< SKIPPED >>>"
+fi
 
 # Test command dumpvg
 MESG 3 Test command dumpvg
+if [ ! $command -o $command = dumpvg ]
+then
 TEST dumpvg-1.out dumpvg tvset.hdf
 TEST dumpvg-2.out dumpvg -i 0,1 tvset.hdf
 TEST dumpvg-3.out dumpvg -r 3 tvset.hdf
@@ -175,9 +229,14 @@ TEST dumpvg-9.out dumpvg -c CDF0.0 tdata.hdf
 TEST dumpvg-10.out dumpvg -c Dim0.0,Var0.0 tdata.hdf
 TEST dumpvg-11.out dumpvg -c Dim0.0,Var0.0 -d tdata.hdf
 TEST dumpvg-12.out dumpvg tvattr.hdf
+else
+    echo "   <<< SKIPPED >>>"
+fi
 
 # Test command dumpgr
 MESG 3 Test command dumpgr
+if [ ! $command -o $command = dumpgr ]
+then
 TEST dumpgr-1.out dumpgr grtdfui82.hdf
 TEST dumpgr-2.out dumpgr -i 0,1,3 grtdfui82.hdf
 TEST dumpgr-3.out dumpgr -i 0 grtdfui82.hdf
@@ -192,6 +251,9 @@ TEST dumpgr-9.out dumpgr grtdfui83.hdf
 TEST dumpgr-10.out dumpgr grtdfui84.hdf
 TEST dumpgr-11.out dumpgr grtdfui162.hdf
 TEST dumpgr-12.out dumpgr grtdfi322.hdf
+else
+    echo "   <<< SKIPPED >>>"
+fi
 
 # End of test
 FINISH
