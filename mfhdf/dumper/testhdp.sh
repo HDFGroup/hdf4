8696a8db13788eb1aca7707506e5bd055ab26eb5
Albert Cheng <acheng@hdfgroup.org>
1997-12-05 13:18:01 -0600

    Purpose     Features Description     Added -except option to skip one sub-command.     Changed -command to -only as it is a more appropriate name.     Changed some output appearance to be more consistent with other tests. Platform     Tested on baldric.

--- mfhdf/dumper/testhdp.sh
+++ mfhdf/dumper/testhdp.sh
@@ -8,10 +8,12 @@
 CMD='./hdp'
 RM='rm -f'
 DIFF=diff
+CMP='cmp -s'
 nerrors=0		# number of errors (0)
 quitonerr=0		# quit on error (not)
 noclean=0		# no cleaning temp. files (yes)
-command=""		# dumper sub-command to test
+only=""			# dumper sub-command to test only
+except=""		# dumper sub-command to test not
 
 
 # Definitions of functions/shorthands
@@ -20,12 +22,13 @@ command=""		# dumper sub-command to test
 # Print Usage of the command
 USAGE()
 {
-    echo "Usage: $0 [-help] [-noclean] [-quit] [-command <command>]"
+    echo "Usage: $0 [-help] [-noclean] [-quit] [-except <command>] [-only <command>]"
     echo "    -help: display help information"
     echo "    -noclean: do not clean away temporary files"
     echo "    -quit: quit immediately if any test fails"
-    echo "    -command: test one specific command where <command> can be"
-    echo "        {list, dumpsds, dumprig, dumpvd, dumpvg, dumpgr}"
+    echo "    -except: skip one specific command"
+    echo "    -only: test one specific command"
+    echo "<command> can be one of {list, dumpsds, dumprig, dumpvd, dumpvg, dumpgr}"
 }
 
 # Print message with formats according to message level ($1)
@@ -35,14 +38,14 @@ MESG()
     shift
     case $level in
 	0)
-	    echo '#############################'
+	    echo '============================='
 	    echo $*
-	    echo '#############################'
+	    echo '============================='
 	    ;;
 	3)
-	    echo '============================='
+	    echo '-----------------------------'
 	    echo $*
-	    echo '============================='
+	    echo '-----------------------------'
 	    ;;
 	6)
 	    echo "*** $* ***"
@@ -76,9 +79,11 @@ TEST()
 	echo "#############################"
         $CMD "$@"
     ) > $output
-    $DIFF $expected $output
+    $CMP $expected $output
     if [ $? -ne 0 ]
     then
+	echo $DIFF $expected $output
+	$DIFF $expected $output
 	echo "   <<< FAILED >>>"
 	nerrors=`expr $nerrors + 1`
 	if [ $quitonerr -gt 0 ]; 
@@ -124,11 +129,24 @@ do
 	    USAGE
 	    exit 0
 	    ;;
-	"-command")
+	"-only")
 	    shift
 	    case "$1" in
     		"list"|"dumpsds"|"dumprig"|"dumpvd"|"dumpvg"|"dumpgr")
-		    command="$1"
+		    only="$1"
+		    ;;
+		*)
+		    echo "Unknown command: $1"
+		    USAGE
+		    exit 1
+		    ;;
+	    esac
+	    ;;
+	"-except")
+	    shift
+	    case "$1" in
+    		"list"|"dumpsds"|"dumprig"|"dumpvd"|"dumpvg"|"dumpgr")
+		    except="$1"
 		    ;;
 		*)
 		    echo "Unknown command: $1"
@@ -145,14 +163,16 @@ do
     esac
     shift
 done
-    
+
 # Print a beginning banner
 MESG 0 "Running hdp tests"
 
 # Test command list
-MESG 3 Test command list
-if [ ! $command -o $command = list ]
+TestCmd=list
+TestName="Test command $TestCmd"
+if [ "$except" != $TestCmd -a \( -z "$only" -o "$only" = $TestCmd \) ]
 then
+MESG 3 "$TestName"
 TEST list-1.out list tdata.hdf
 TEST list-2.out list -l tdata.hdf
 TEST list-3.out list -d tdata.hdf
@@ -164,13 +184,15 @@ TEST list-8.out list -a tdata.hdf
 TEST list-9.out list -a Example6.hdf
 TEST list-10.out list -n Example6.hdf
 else
-    echo "   <<< SKIPPED >>>"
+MESG 3 "$TestName <<<SKIPPED>>>"
 fi
 
 # Test command dumpsds
-MESG 3 Test command dumpsds
-if [ ! $command -o $command = dumpsds ]
+TestCmd=dumpsds
+TestName="Test command $TestCmd"
+if [ "$except" != $TestCmd -a \( -z "$only" -o "$only" = $TestCmd \) ]
 then
+MESG 3 "$TestName"
 TEST dumpsds-1.out dumpsds swf32.hdf
 TEST dumpsds-2.out dumpsds -i 2 swf32.hdf
 TEST dumpsds-3.out dumpsds -i 1,3 swf32.hdf
@@ -180,13 +202,15 @@ TEST dumpsds-5.out dumpsds -n fakeDim0,Data-Set-2 swf32.hdf
 TEST dumpsds-6.out dumpsds -r 3,2 swf32.hdf
 TEST dumpsds-7.out dumpsds -r 3,2 -d swf32.hdf
 else
-    echo "   <<< SKIPPED >>>"
+MESG 3 "$TestName <<<SKIPPED>>>"
 fi
 
 # Test command dumprig
-MESG 3 Test command dumprig
-if [ ! $command -o $command = dumprig ]
+TestCmd=dumprig
+TestName="Test command $TestCmd"
+if [ "$except" != $TestCmd -a \( -z "$only" -o "$only" = $TestCmd \) ]
 then
+MESG 3 "$TestName"
 TEST dumprig-1.out dumprig tdf24.hdf
 TEST dumprig-2.out dumprig -i 1,2 tdf24.hdf
 TEST dumprig-3.out dumprig -i 1,3 tdf24.hdf		# '-i 3' is invalid
@@ -194,13 +218,15 @@ TEST dumprig-4.out dumprig -m 24 tdf24.hdf
 TEST dumprig-5.out dumprig -r 3,4 tdf24.hdf
 TEST dumprig-6.out dumprig -r 3,4 -d tdf24.hdf
 else
-    echo "   <<< SKIPPED >>>"
+MESG 3 "$TestName <<<SKIPPED>>>"
 fi
 
 # Test command dumpvd
-MESG 3 Test command dumpvd
-if [ ! $command -o $command = dumpvd ]
+TestCmd=dumpvd
+TestName="Test command $TestCmd"
+if [ "$except" != $TestCmd -a \( -z "$only" -o "$only" = $TestCmd \) ]
 then
+MESG 3 "$TestName"
 TEST dumpvd-1.out dumpvd tvset.hdf
 TEST dumpvd-2.out dumpvd -i 1,3,5 tvset.hdf
 TEST dumpvd-3.out dumpvd -r 1238,1239,1251,1252 tvset.hdf
@@ -212,13 +238,15 @@ TEST dumpvd-8.out dumpvd -f "STATION_NAME","FLOATS" tvset.hdf
 TEST dumpvd-9.out dumpvd -f "STATION_NAME","FLOATS" -d tvset.hdf
 TEST dumpvd-10.out dumpvd tvattr.hdf
 else
-    echo "   <<< SKIPPED >>>"
+MESG 3 "$TestName <<<SKIPPED>>>"
 fi
 
 # Test command dumpvg
-MESG 3 Test command dumpvg
-if [ ! $command -o $command = dumpvg ]
+TestCmd=dumpvg
+TestName="Test command $TestCmd"
+if [ "$except" != $TestCmd -a \( -z "$only" -o "$only" = $TestCmd \) ]
 then
+MESG 3 "$TestName"
 TEST dumpvg-1.out dumpvg tvset.hdf
 TEST dumpvg-2.out dumpvg -i 0,1 tvset.hdf
 TEST dumpvg-3.out dumpvg -r 3 tvset.hdf
@@ -232,13 +260,15 @@ TEST dumpvg-10.out dumpvg -c Dim0.0,Var0.0 tdata.hdf
 TEST dumpvg-11.out dumpvg -c Dim0.0,Var0.0 -d tdata.hdf
 TEST dumpvg-12.out dumpvg tvattr.hdf
 else
-    echo "   <<< SKIPPED >>>"
+MESG 3 "$TestName <<<SKIPPED>>>"
 fi
 
 # Test command dumpgr
-MESG 3 Test command dumpgr
-if [ ! $command -o $command = dumpgr ]
+TestCmd=dumpgr
+TestName="Test command $TestCmd"
+if [ "$except" != $TestCmd -a \( -z "$only" -o "$only" = $TestCmd \) ]
 then
+MESG 3 "$TestName"
 TEST dumpgr-1.out dumpgr grtdfui82.hdf
 TEST dumpgr-2.out dumpgr -i 0,1,3 grtdfui82.hdf
 TEST dumpgr-3.out dumpgr -i 0 grtdfui82.hdf
@@ -254,7 +284,7 @@ TEST dumpgr-10.out dumpgr grtdfui84.hdf
 TEST dumpgr-11.out dumpgr grtdfui162.hdf
 TEST dumpgr-12.out dumpgr grtdfi322.hdf
 else
-    echo "   <<< SKIPPED >>>"
+MESG 3 "$TestName <<<SKIPPED>>>"
 fi
 
 # End of test
