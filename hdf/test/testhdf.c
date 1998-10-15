fd46f76e69c1850abab4a723e05b661cd504c7bb
Elena Pourmal <epourmal@hdfgroup.org>
1998-10-15 12:01:25 -0500

    NT DLL port:
    Bit-Vector test and Threaded Balanced Binary Trees test are bypassed on
    the NT platform.
    
    Both programs test low level library routines and those routines are not set
    up to be DLL imported/exported.
    
    Tests run successfully on both Win32 NT and Alpha NT for the single-threaded library. We should probably let tests run for the static libraries
    and bypass them only when testing DLLs.

--- hdf/test/testhdf.c
+++ hdf/test/testhdf.c
@@ -135,10 +135,11 @@ main(int argc, char *argv[])
     setbuf(stderr, NULL);
     setbuf(stdout, NULL);
 #endif
-
     /* Tests are generally arranged from least to most complexity... */
+#if !(defined WIN32)
     InitTest("bitvect", test_bitvect, "Bit-Vector routines");
     InitTest("tbbt", test_tbbt, "Threaded Balanced Binary Trees");
+#endif
     InitTest("vers", test_vers, "VERSION OF LIBRARY");
     InitTest("hfile", test_hfile, "HFILE");
     InitTest("hfile1", test_hfile1, "HFILE LIMITS");
