8231d2b215d6c455c3d7e4bebc348cfbb468bb86
Elena Pourmal <epourmal@hdfgroup.org>
1999-07-16 09:56:11 -0500

    hdfi.h had a very long line created by copy/paste in the telnet window when I was checking fix for AIX 4.3 port. Would not compile.
    *.f90 were missing from CVS. Those files "were last seen" during HDF4r1.3
    release. If anybody knows what could happen to them, please reply.

--- hdf/src/hdfi.h
+++ hdf/src/hdfi.h
@@ -303,10 +303,11 @@ Please check your Makefile.
 typedef void              VOID;
 typedef void              *VOIDP;
 typedef char              *_fcd;
-/*  AIX 4.3 defines int8, int16, int32 in /usr/include/intypes.h file.
-    Compilation fails with complains about redefinition.
-    Use of predefined macro helped.  EIP 7/15/99 */
-#ifndef _ALL_SOURCE                                                              typedef char              int8;                                                  typedef short int         int16;                                                 typedef long int          int32;                                                 #endif  
+#ifndef _ALL_SOURCE       
+typedef char              int8;
+typedef short int         int16; 
+typedef long int          int32; 
+#endif  
 typedef char              char8;
 typedef unsigned char     uchar8;
 typedef unsigned char     uint8;
