7011cdc8f745a0e140e06123982aac6cf78f79f9
wendling <wendling@hdfgroup.org>
2003-09-11 07:29:26 +0000

    Purpose:     New Script Description:     Added a script to regenerate Makefile.ins and other auto* things. Platforms tested:     Linux

---
+++ bin/regenerate.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+#
+# Run this script in the top source directory of HDF4 to regenerate
+# all of the necessary files.
+#
+# This is a helper script to regenerate the aclocal.m4, h4config.h.in,
+# Makefile.in, and configure files all in one go. The versions for the
+# tools need to be according to this table or better:
+#
+#       Name            Version
+#       ----            -------
+#       aclocal           1.6
+#       autoheader        2.53
+#       automake          1.6
+#       autoconf          2.53
+#
+echo /usr/bin/autoreconf -v
+env AUTOMAKE="/usr/bin/automake --foreign" /usr/bin/autoreconf -v
+#echo /usr/bin/autoconf
+#/usr/bin/autoconf
+#echo /usr/bin/autoheader
+#/usr/bin/autoheader
+#echo /usr/bin/aclocal
+#/usr/bin/aclocal
+#echo /usr/bin/automake --foreign
+#/usr/bin/automake --foreign
