66abcbac46db345365cec19e13c33d0593adbd11
Elena Pourmal <epourmal@hdfgroup.org>
2005-11-01 10:41:11 -0600

    Purpose: Maintenance
    Description: Changed version number to 4.2r1-post1 before createing a
                 tar ball for NASA user.
    
    Solution:
    
    Platforms tested: heping, lemiuex
    
    Misc. update:

--- hdf/src/hfile.h
+++ hdf/src/hfile.h
@@ -49,7 +49,7 @@
 #define LIBVER_MAJOR    4
 #define LIBVER_MINOR    2 
 #define LIBVER_RELEASE  1 
-#define LIBVER_STRING   "NCSA HDF Version 4.2 Release 1-post0, February 18, 2005"
+#define LIBVER_STRING   "NCSA HDF Version 4.2 Release 1-post1, November 1, 2005"
 #define LIBVSTR_LEN    80   /* length of version string  */
 #define LIBVER_LEN  92      /* 4+4+4+80 = 92 */
 /* end of version tags */
