05ea5f3536c004ec6a46d2493f28ca895ca22cb7
swegner <swegner@hdfgroup.org>
2007-08-02 18:10:02 +0000

    Purpose: Major Windows documentation, and misc. enhancements/cleanup
    Description:
    This brings the Windows documentation fully up-to-date, including new instructions for 6.0 and .NET, revised instructions for 2005, and new Fortran support.  I've also been able to streamline some of the testing in .NET projects (ctest0).  And the project output for C and Fortran examples have be moved, with batch script updates accordingly.
    
    Tested:
    VS.NET
    VS6.0

--- windows/InstallExamples.bat
+++ windows/InstallExamples.bat
@@ -1,63 +1,18 @@
 @ECHO OFF
-REM This batch file is used to install HDF4 C examples'
+REM This batch file is used to install HDF4 C examples
 REM executable files.
 REM By Xuan Bai
 REM Created on: 12/23/2004
-REM Last Modified: 12/23/2004
+REM Last Modified: 8/1/2004
 
-cd examples
+pushd windows\examples\cexamples
 
 mkdir examplesREL
 mkdir examplesDBG
 
-cd chgnamtest
-copy debug\chgnamtest.exe ..\examplesDBG\
-copy release\chgnamtest.exe ..\examplesREL\
-cd ..
+for %%a in (chgnamtest createtest dimtest gitest rattrtest readtest sattrtest sds2grouptest sh_dimtest unltest) do (
+    copy debug\%%a.exe examplesDBG\
+    copy release\%%a.exe examplesREL\
+)
 
-cd createtest
-copy debug\createtest.exe ..\examplesDBG\
-copy release\createtest.exe ..\examplesREL\
-cd ..
-
-cd dimtest
-copy debug\dimtest.exe ..\examplesDBG\
-copy release\dimtest.exe ..\examplesREL\
-cd ..
-
-cd gitest
-copy debug\gitest.exe ..\examplesDBG\
-copy release\gitest.exe ..\examplesREL\
-cd ..
-
-cd rattrtest
-copy debug\rattrtest.exe ..\examplesDBG\
-copy release\rattrtest.exe ..\examplesREL\
-cd ..
-
-cd readtest
-copy debug\readtest.exe ..\examplesDBG\
-copy release\readtest.exe ..\examplesREL\
-cd ..
-
-cd sattrtest
-copy debug\sattrtest.exe ..\examplesDBG\
-copy release\sattrtest.exe ..\examplesREL\
-cd ..
-
-cd sds2grouptest
-copy debug\sds2grouptest.exe ..\examplesDBG\
-copy release\sds2grouptest.exe ..\examplesREL\
-cd ..
-
-cd sh_dimtest
-copy debug\sh_dimtest.exe ..\examplesDBG\
-copy release\sh_dimtest.exe ..\examplesREL\
-cd ..
-
-cd unltest
-copy debug\unltest.exe ..\examplesDBG\
-copy release\unltest.exe ..\examplesREL\
-cd ..
-
-cd ..
+popd
