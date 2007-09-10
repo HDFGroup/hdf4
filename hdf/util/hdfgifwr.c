c3e77052c0376200c3eee676730f580f4ae49e5f
pvn <pvn@hdfgroup.org>
2007-09-10 16:36:16 +0000

    updated (c) to
    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     * Copyright by The HDF Group.                                               *
     * Copyright by the Board of Trustees of the University of Illinois.         *
     * All rights reserved.                                                      *
     *                                                                           *
     * This file is part of HDF.  The full HDF copyright notice, including       *
     * terms governing use, modification, and redistribution, is contained in    *
     * the files COPYING and Copyright.html.  COPYING can be found at the root   *
     * of the source code distribution tree; Copyright.html can be found at      *
     * http://hdfgroup.org/products/hdf4/doc/Copyright.html.  If you do not have *
     * access to either file, you may request a copy from help@hdfgroup.org.     *
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
    
    tested: linux

--- hdf/util/hdfgifwr.c
+++ hdf/util/hdfgifwr.c
@@ -1,3 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF.  The full HDF copyright notice, including       *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at      *
+ * http://hdfgroup.org/products/hdf4/doc/Copyright.html.  If you do not have *
+ * access to either file, you may request a copy from help@hdfgroup.org.     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+ 
 /*
  * hdfgifwr.c  - handles writing of GIF files.  
  * 
