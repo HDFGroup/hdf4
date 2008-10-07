cea303f540eeb903ab6f5e43959eb7b1f4f89877
Binh-Minh Ribler <bmribler@hdfgroup.org>
2008-10-07 13:05:13 -0500

    Description:     Fixed a typo in making the path for input test file test1.nc, from "./ncgen"     to "./".  This typo caused "file not found" error when the tests were ran     by ./hdftest, and not by "make check," because test1.nc resides in the src     directory hdf4/mfhdf/libsrc and "srcdir" is NULL when ./hdftest is used.
    Platforms tested:
        SunOS 5.10 (linew)
        Linux 2.6 (kagiso)

--- mfhdf/libsrc/tnetcdf.c
+++ mfhdf/libsrc/tnetcdf.c
@@ -61,15 +61,16 @@ test_netcdf_reading()
         strcat(testfile, "/");
     }
 
-	/* Windows doesn't set srcdir, and generates files in a different relative
-	path, so we need to special case here.  It is best to look for the testfile
-	in the same path, and the Windows test script will make sure to put it there
-	first.  - SJW 2007/09/19 */
+    /* Windows doesn't set srcdir, and generates files in a different relative
+       path, so we need to special case here.  It is best to look for the
+       testfile in the same path, and the Windows test script will make sure
+       to put it there first.  - SJW 2007/09/19 */
 #ifndef _WIN32
-    /* This is to get to the file if the test is ran by ./hdftest, and not by 
-	make check - BMR - 2007/08/09 */
+    /* This is to get to the file 'test1.nc' when the library was built without
+       srcdir option and the test is ran by ./hdftest in the src directory
+       hdf4/mfhdf/libsrc instead of by make check.  - BMR 2007/08/09 */
     if (srcdir == NULL)
-	strcpy(testfile, "../ncgen/");
+	strcpy(testfile, "./");
 #endif /* _WIN32 */
 
     strcat(testfile, basename);
