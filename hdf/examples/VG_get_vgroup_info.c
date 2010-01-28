f7d3795db56d8154104b0152c3efc0a8f1e9d702
Binh-Minh Ribler <bmribler@hdfgroup.org>
2010-01-28 14:07:21 -0600

    Descritpion:     - Put back VGNAMELENMAX in hlimits.h because Fortran is using it.     - Fixed a C examples to use dynamically allocated vgroup's name and class.
    Platforms tested:
        Linux/32 2.6 (jam)
        FreeBSD/64 6.3 (liberty)

--- hdf/examples/VG_get_vgroup_info.c
+++ hdf/examples/VG_get_vgroup_info.c
@@ -12,7 +12,8 @@ int main( )
    int32  lone_vg_number,      /* current lone vgroup number */
           num_of_lones = 0;    /* number of lone vgroups */
    int32 *ref_array;    /* buffer to hold the ref numbers of lone vgroups   */
-   char   vgroup_name[VGNAMELENMAX], vgroup_class[VGNAMELENMAX];
+   char  *vgroup_name, *vgroup_class;
+   uint16 name_len;
 
    /********************** End of variable declaration **********************/
  
@@ -63,11 +64,28 @@ int main( )
          * moving to the next.
          */
          vgroup_id = Vattach (file_id, ref_array[lone_vg_number], "r");
+	 status_32 = Vgetnamelen(vgroup_id, &name_len);
+	 vgroup_name = (char *) HDmalloc(sizeof(char *) * (name_len+1));
+	 if (vgroup_name == NULL)
+	 {
+	     fprintf(stderr, "Not enough memory for vgroup_name!\n");
+	     exit(1);
+	 }
          status_32 = Vgetname (vgroup_id, vgroup_name);
+
+	 status_32 = Vgetclassnamelen(vgroup_id, &name_len);
+	 vgroup_class = (char *) HDmalloc(sizeof(char *) * (name_len+1));
+	 if (vgroup_class == NULL)
+	 {
+	     fprintf(stderr, "Not enough memory for vgroup_class!\n");
+	     exit(1);
+	 }
          status_32 = Vgetclass (vgroup_id, vgroup_class);
          fprintf(stderr, "   Vgroup name %s and class %s\n", vgroup_name,  
                      vgroup_class); 
          status_32 = Vdetach (vgroup_id);
+	 if (vgroup_name != NULL) HDfree(vgroup_name);
+	 if (vgroup_class != NULL) HDfree(vgroup_class);
       } /* for */
    } /* if */
 
