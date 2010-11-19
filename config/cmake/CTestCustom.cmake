8138843c23fc405e9339198e10b23a25d110c5d4
Allen Byrne <byrn@hdfgroup.org>
2010-11-19 09:52:14 -0600

    Correct SZIP 'warning ignore' regex, add jpeg ignore warning

--- config/cmake/CTestCustom.cmake
+++ config/cmake/CTestCustom.cmake
@@ -2,7 +2,8 @@ SET (CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1500)
 
 SET (CTEST_CUSTOM_WARNING_EXCEPTION
     ${CTEST_CUSTOM_WARNING_EXCEPTION}
-    "SZIP.src.[0-9a-zA-Z]*warning"
+    "SZIP.src.*:[ \t]*warning"
+    "jpeg.src.*:[ \t]*warning"
     "POSIX name for this item is deprecated"
     "disabling jobserver mode"
 )
