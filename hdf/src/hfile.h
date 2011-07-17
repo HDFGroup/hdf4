b413a2f1fc6488f7cc5267427574fc9669e8788e
hdftest <hdftest@hdfgroup.org>
2011-07-17 18:42:21 +0000

    Snapshot HDF Version 4.2 Release 6-post1, July 3, 2011 .

--- hdf/src/hfile.h
+++ hdf/src/hfile.h
@@ -52,9 +52,9 @@
 #define LIBVER_MAJOR    4
 #define LIBVER_MINOR    2 
 #define LIBVER_RELEASE  6 
-#define LIBVER_SUBRELEASE "post1"   /* For pre-releases like snap0       */
+#define LIBVER_SUBRELEASE "post2"   /* For pre-releases like snap0       */
                                 /* Empty string for real releases.           */
-#define LIBVER_STRING   "HDF Version 4.2 Release 6-post1, July 3, 2011"
+#define LIBVER_STRING   "HDF Version 4.2 Release 6-post2, July 17, 2011"
 #define LIBVSTR_LEN    80   /* length of version string  */
 #define LIBVER_LEN  92      /* 4+4+4+80 = 92 */
 /* end of version tags */
