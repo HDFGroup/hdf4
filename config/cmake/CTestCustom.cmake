b385c015882b7cfc6b7e0c8d8ffef07ffb7fca83
Allen Byrne <byrn@hdfgroup.org>
2012-05-25 12:34:53 -0500

    reduce compiler warnings reported by CDash for non-critical issues

--- config/cmake/CTestCustom.cmake
+++ config/cmake/CTestCustom.cmake
@@ -2,11 +2,13 @@ SET (CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1500)
 
 SET (CTEST_CUSTOM_WARNING_EXCEPTION
     ${CTEST_CUSTOM_WARNING_EXCEPTION}
+    "note.*expected.*void.*but argument is of type.*volatile"
     "SZIP.src.*:[ \t]*warning"
     "jpeg.src.*:[ \t]*warning"
     "POSIX name for this item is deprecated"
     "disabling jobserver mode"
     "config.cmake.xlatefile.c"
+    "warning.*implicit declaration of function"
 )
  
  
