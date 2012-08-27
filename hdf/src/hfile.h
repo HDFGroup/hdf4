0f266285fed78353b0427a0d8b975e1ba04bdf78
Allen Byrne <byrn@hdfgroup.org>
2012-08-27 16:12:56 -0500

    Convert tests to *.sh.in
    Tested: local linux

--- hdf/src/hfile.h
+++ hdf/src/hfile.h
@@ -52,9 +52,9 @@
 #define LIBVER_MAJOR    4
 #define LIBVER_MINOR    2 
 #define LIBVER_RELEASE  8 
-#define LIBVER_SUBRELEASE ""   /* For pre-releases like snap0       */
+#define LIBVER_SUBRELEASE "post0"   /* For pre-releases like snap0       */
                                 /* Empty string for real releases.           */
-#define LIBVER_STRING   "HDF Version 4.2 Release 8, August 3, 2012"
+#define LIBVER_STRING   "HDF Version 4.2 Release 8-post0, August 19, 2012"
 #define LIBVSTR_LEN    80   /* length of version string  */
 #define LIBVER_LEN  92      /* 4+4+4+80 = 92 */
 /* end of version tags */
