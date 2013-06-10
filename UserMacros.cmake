8de3db46cc2b0b1303d1696a6c4671979742b205
Allen Byrne <byrn@hdfgroup.org>
2013-06-10 12:18:38 -0500

    HDFFV-8461: Add Windows /MT support This adds the UserMacros.cmake support needed. Correct order of option and macro. Tested: windows

--- UserMacros.cmake
+++ UserMacros.cmake
@@ -5,13 +5,13 @@
 #-----------------------------------------------------------------------------
 # Option to Build with User Defined Values
 #-----------------------------------------------------------------------------
+MACRO (MACRO_USER_DEFINED_LIBS)
+  SET (USER_DEFINED_VALUE "FALSE")
+ENDMACRO (MACRO_USER_DEFINED_LIBS)
+
+#-------------------------------------------------------------------------------
 OPTION (BUILD_USER_DEFINED_LIBS "Build With User Defined Values" OFF)
 IF (BUILD_USER_DEFINED_LIBS)
   MACRO_USER_DEFINED_LIBS ()
 ENDIF (BUILD_USER_DEFINED_LIBS)
-
-#-------------------------------------------------------------------------------
-MACRO (MACRO_USER_DEFINED_LIBS)
-  SET (USER_DEFINED_VALUE "FALSE")
-ENDMACRO (MACRO_USER_DEFINED_LIBS)
  
