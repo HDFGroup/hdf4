1be2929b49cb5a8550d08b7e76726cfff58ff4c4
Binh-Minh Ribler <bmribler@hdfgroup.org>
2017-07-06 01:19:16 -0500

    Updated for 4.2.13-post0

--- hdf/src/hfile.h
+++ hdf/src/hfile.h
@@ -52,9 +52,9 @@
 #define LIBVER_MAJOR    4
 #define LIBVER_MINOR    2 
 #define LIBVER_RELEASE  13 
-#define LIBVER_SUBRELEASE ""   /* For pre-releases like snap0       */
+#define LIBVER_SUBRELEASE "post0"   /* For pre-releases like snap0       */
                                 /* Empty string for real releases.           */
-#define LIBVER_STRING   "HDF Version 4.2 Release 13, June 24, 2017"
+#define LIBVER_STRING   "HDF Version 4.2 Release 13-post0, July 6, 2017"
 #define LIBVSTR_LEN    80   /* length of version string  */
 #define LIBVER_LEN  92      /* 4+4+4+80 = 92 */
 /* end of version tags */
