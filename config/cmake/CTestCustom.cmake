ad1cedfd67cf831d49b587682b633f65601ee92e
Allen Byrne <byrn@hdfgroup.org>
2020-02-18 14:59:20 -0600

    CMake updates and fixes

--- config/cmake/CTestCustom.cmake
+++ config/cmake/CTestCustom.cmake
@@ -3,14 +3,15 @@ set (CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1500)
 
 set (CTEST_CUSTOM_WARNING_EXCEPTION
     ${CTEST_CUSTOM_WARNING_EXCEPTION}
-    "note.*expected.*void.*but argument is of type.*volatile"
-    "SZIP.src.*:[ \t]*warning"
-    "jpeg.src.*:[ \t]*warning"
-    "POSIX name for this item is deprecated"
-    "disabling jobserver mode"
-    "config.cmake.xlatefile.c"
+    ".*note.*expected.*void.*but argument is of type.*volatile.*"
+    ".*src.SZIP.*:[ \t]*warning.*"
+    ".*src.ZLIB.*:[ \t]*warning.*"
+    ".*jpeg.src.*:[ \t]*warning.*"
+    ".*POSIX name for this item is deprecated.*"
+    ".*disabling jobserver mode.*"
+    ".*warning.*implicit declaration of function.*"
+    ".*note: expanded from macro.*"
 )
-
 
 set (CTEST_CUSTOM_MEMCHECK_IGNORE
     ${CTEST_CUSTOM_MEMCHECK_IGNORE}
