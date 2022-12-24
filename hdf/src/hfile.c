c52d0c2b71df20a84376e85f303aa517f6536939
Kurt Schwehr <schwehr@google.com>
2022-12-24 10:32:22 -0800

    hfile.c: Initialize fversion local variable in HIread_version()
    A fuzzer was able to produce input that caused fversion to be used
    uninitialized.  The fuzzer:
    
    https://gist.github.com/schwehr/0af040cb15ab4117f9737697722f9d42

--- hdf/src/hfile.c
+++ hdf/src/hfile.c
@@ -113,6 +113,8 @@
    HIread_version       -- reads a version tag from a file
    + */
 
+#include <string.h>
+
 #define HMASTER
 #include "hdf.h"
 #undef HMASTER
@@ -3421,9 +3423,10 @@ PRIVATE int
 HIread_version(int32 file_id)
 {
   filerec_t  *file_rec;
-  uint8       fversion[LIBVER_LEN];
   CONSTR(FUNC, "Hread_version");
   int         ret_value = SUCCEED;
+  uint8       fversion[LIBVER_LEN];
+  memset(fversion, 0, sizeof(fversion));
 
   HEclear();
 
