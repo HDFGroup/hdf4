7c1759d26f4c8c36f6631dafde295d5c5efd3f38
Dana Robinson <43805+derobins@users.noreply.github.com>
2023-01-10 13:34:13 -0800

    Removes Next-specific code (#87)

--- hdf/src/hdfi.h
+++ hdf/src/hdfi.h
@@ -65,7 +65,6 @@
 #define     DFMT_APPLE_INTEL    0x4441
 #define     DFMT_MAC            0x1111
 #define     DFMT_SUN386         0x4441
-#define     DFMT_NEXT           0x1111
 #define     DFMT_MOTOROLA       0x1111
 #define     DFMT_ALPHA          0x4441
 #define     DFMT_VP             0x6611
@@ -592,59 +591,6 @@ typedef int               hdf_pint_t;   /* 4-byte pointer */
 
 #endif /* INTEL86 */
 #endif /* !(defined(__APPLE__)) */
-
-/*-----------------------------------------------------*/
-#if defined(NEXT) || defined(NeXT)
-
-#ifndef NEXT
-#define NEXT
-#endif
-
-#ifdef GOT_MACHINE
-If you get an error on this line more than one machine type has been defined.
-Please check your Makefile.
-#endif
-#define GOT_MACHINE
-
-#define isascii(c)  (isprint(c) || iscntrl(c))
-#ifndef __GNUC__
-#include <memory.h>
-#endif /* __GNUC__ */
-#include <sys/file.h>               /* for unbuffered i/o stuff */
-#include <sys/stat.h>
-#define DF_MT             DFMT_NEXT
-typedef void              VOID;
-typedef void              *VOIDP;
-typedef char              *_fcd;
-typedef char              char8;
-typedef unsigned char     uchar8;
-typedef char              int8;
-typedef unsigned char     uint8;
-typedef short int         int16;
-typedef unsigned short int uint16;
-typedef long int          int32;
-typedef unsigned long int uint32;
-typedef int               intn;
-typedef unsigned int      uintn;
-typedef int               intf;     /* size of INTEGERs in Fortran compiler */
-typedef float             float32;
-typedef double            float64;
-typedef int               hdf_pint_t;   /* an integer the same size as a pointer */
-#define FNAME_POST_UNDERSCORE
-#define _fcdtocp(desc) (desc)
-#define FILELIB UNIXBUFIO
-
-/* JPEG #define's - Look in the JPEG docs before changing - (Q) */
-
-/* Determine the memory manager we are going to use. Valid values are: */
-/*  MEM_DOS, MEM_ANSI, MEM_NAME, MEM_NOBS.  See the JPEG docs for details on */
-/*  what each does */
-#define JMEMSYS         MEM_ANSI
-
-#define HAVE_STDC
-#define INCLUDES_ARE_ANSI
-
-#endif /* NEXT */
 
 /*-----------------------------------------------------*/
 #if defined(MOTOROLA) || defined(m88k)
