3d065b6b9f731296c4ea8ec6a66e2b213a80d866
Allen Byrne <byrn@hdfgroup.org>
2023-01-17 16:19:19 -0600

    Executables do not import/export symbols within itself.

--- hdf/src/H4api_adpt.h
+++ hdf/src/H4api_adpt.h
@@ -97,11 +97,9 @@
 #if defined(_MSC_VER) || defined(__MINGW32__) /* MSVC Compiler Case */
 #define HDFPUBLIC   __declspec(dllimport)
 #define HDFLIBAPI   extern __declspec(dllimport)
-#define HDFTOOLSAPI extern __declspec(dllexport)
 #elif (__GNUC__ >= 4) /* GCC 4.x has support for visibility options */
 #define HDFPUBLIC   __attribute__((visibility("default")))
 #define HDFLIBAPI   extern __attribute__((visibility("default")))
-#define HDFTOOLSAPI extern __attribute__((visibility("default")))
 #endif
 #endif /* mfhdf_hdiff_shared_EXPORTS */
 
@@ -109,11 +107,9 @@
 #if defined(_MSC_VER) || defined(__MINGW32__) /* MSVC Compiler Case */
 #define HDFPUBLIC   __declspec(dllimport)
 #define HDFLIBAPI   extern __declspec(dllimport)
-#define HDFTOOLSAPI extern __declspec(dllexport)
 #elif (__GNUC__ >= 4) /* GCC 4.x has support for visibility options */
 #define HDFPUBLIC   __attribute__((visibility("default")))
 #define HDFLIBAPI   extern __attribute__((visibility("default")))
-#define HDFTOOLSAPI extern __attribute__((visibility("default")))
 #endif
 #endif /* mfhdf_hrepack_shared_EXPORTS */
 
@@ -152,13 +148,6 @@
 #define HDFFCLIBAPI extern __attribute__((visibility("default")))
 #endif
 #endif
-#if !defined(HDFTOOLSAPI)
-#if defined(_MSC_VER) || defined(__MINGW32__) /* MSVC Compiler Case */
-#define HDFTOOLSAPI extern __declspec(dllimport)
-#elif (__GNUC__ >= 4) /* GCC 4.x has support for visibility options */
-#define HDFTOOLSAPI extern __attribute__((visibility("default")))
-#endif
-#endif
 
 #else
 #define XDRLIBAPI    extern
@@ -166,7 +155,6 @@
 #define HDFPUBLIC
 #define HDFLIBAPI   extern
 #define HDFFCLIBAPI extern
-#define HDFTOOLSAPI extern
 #endif /*H4_BUILT_AS_DYNAMIC_LIB  */
 
 #endif /* H4API_ADPT_H */
