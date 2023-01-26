4a4b49c4ed5c30a44afc5de0e1b85be1e4dd19e2
Allen Byrne <byrn@hdfgroup.org>
2023-01-26 09:34:12 -0600

    Remove C++ warnings - cleanup autotools config dir

--- config/sanitizer/tools.cmake
+++ config/sanitizer/tools.cmake
@@ -13,17 +13,8 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-option(CLANG_TIDY "Turns on clang-tidy processing if it is found." OFF)
 option(IWYU "Turns on include-what-you-use processing if it is found." OFF)
 option(CPPCHECK "Turns on cppcheck processing if it is found." OFF)
-
-# Adds clang-tidy checks to the compilation, with the given arguments being used
-# as the options set.
-macro(clang_tidy)
-  if(CLANG_TIDY AND CLANG_TIDY_EXE)
-    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_EXE} ${ARGN})
-  endif()
-endmacro()
 
 # Adds include_what_you_use to the compilation, with the given arguments being
 # used as the options set.
@@ -40,28 +31,6 @@ macro(cppcheck)
     set(CMAKE_CXX_CPPCHECK ${CPPCHECK_EXE} ${ARGN})
   endif()
 endmacro()
-
-find_program(CLANG_TIDY_EXE NAMES "clang-tidy")
-mark_as_advanced(FORCE CLANG_TIDY_EXE)
-if(CLANG_TIDY_EXE)
-  message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
-  if(NOT CLANG_TIDY)
-    message(STATUS "clang-tidy NOT ENABLED via 'CLANG_TIDY' variable!")
-    set(CMAKE_CXX_CLANG_TIDY
-        ""
-        CACHE STRING "" FORCE) # delete it
-  endif()
-elseif(CLANG_TIDY)
-  message(SEND_ERROR "Cannot enable clang-tidy, as executable not found!")
-  set(CMAKE_CXX_CLANG_TIDY
-      ""
-      CACHE STRING "" FORCE) # delete it
-else()
-  message(STATUS "clang-tidy not found!")
-  set(CMAKE_CXX_CLANG_TIDY
-      ""
-      CACHE STRING "" FORCE) # delete it
-endif()
 
 find_program(IWYU_EXE NAMES "include-what-you-use")
 mark_as_advanced(FORCE IWYU_EXE)
