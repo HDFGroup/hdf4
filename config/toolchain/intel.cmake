b270b32e89e64aa65f91d9ab6bec8c86297db89b
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2023-02-03 16:00:23 -0600

    Account for Intels name for compiler based on clang (#250)
    * Account for Intels name for compiler based on clang
    
    * Fix intel LLVM checks
    
    * Fix icx warning error for xdrtest
    
    * Restrict change to Intel icx

--- config/toolchain/intel.cmake
+++ config/toolchain/intel.cmake
@@ -1,18 +1,11 @@
 # Uncomment the following to use cross-compiling
 #set(CMAKE_SYSTEM_NAME Linux)
 
-set (CMAKE_COMPILER_VENDOR "intel")
+set(CMAKE_COMPILER_VENDOR "intel")
 
-if (USE_LLVM)
-  set(CMAKE_C_COMPILER icx)
-  set(CMAKE_CXX_COMPILER icpx)
-  set(CMAKE_Fortran_COMPILER ifx)
-  set(INTEL_CLANG ON)
-else ()
-  set(CMAKE_C_COMPILER icc)
-  set(CMAKE_CXX_COMPILER icpc)
-  set(CMAKE_Fortran_COMPILER ifort)
-endif ()
+set(CMAKE_C_COMPILER icx)
+set(CMAKE_CXX_COMPILER icpx)
+set(CMAKE_Fortran_COMPILER ifx)
 
 # the following is used if cross-compiling
 set(CMAKE_CROSSCOMPILING_EMULATOR "")
