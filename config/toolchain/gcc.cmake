fe215457701a03ffbc63077cc3fd1c703c64b29d
Allen Byrne <byrn@hdfgroup.org>
2023-02-03 10:30:13 -0600

    Fix intel LLVM checks

--- config/toolchain/gcc.cmake
+++ config/toolchain/gcc.cmake
@@ -1,7 +1,7 @@
 # Uncomment the following line and the correct system name to use cross-compiling
 #set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_COMPILER_VENDOR "GCC")
+set(CMAKE_COMPILER_VENDOR "gcc")
 
 set(CMAKE_C_COMPILER cc)
 set(CMAKE_CXX_COMPILER c++)
