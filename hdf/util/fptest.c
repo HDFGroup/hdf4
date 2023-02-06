4dfa9e8632b6fa0a9f705348040525316aeba62a
Dana Robinson <43805+derobins@users.noreply.github.com>
2023-02-06 12:03:41 -0800

    Use int main(void) when argc/argv not used (#270)
    * Use alternative main() signature when args not used
    * Return EXIT_SUCCESS/EXIT_FAILURE
    * Delete noise casts

--- hdf/util/fptest.c
+++ hdf/util/fptest.c
@@ -48,7 +48,7 @@
  *      data element value = row value + column value [+ plane value, if rank=3]
  */
 int
-main(int argc, char *argv[])
+main(void)
 {
     int   nrow = 3, ncol = 4, npln = 5, ione = 1;
     int   i, j, k;
@@ -69,9 +69,6 @@ main(int argc, char *argv[])
     const char *text = "TEXT";
     const char *fp32 = "FP32";
     const char *fp64 = "FP64";
-
-    (void)argv;
-    (void)argc;
 
     /*
      * initialize the row, column, and plane vectors
@@ -256,5 +253,6 @@ main(int argc, char *argv[])
             for (j = 0; j < ncol; j++)
                 (void)fwrite((char *)&b64r3[k][i][j], sizeof(float64), 1, sp);
     (void)fclose(sp);
-    return (0);
+
+    return EXIT_SUCCESS;
 }
