6f1dcac8391232f0c572affa8dca73918c3a7288
Larry Knox <lrknox@hdfgroup.org>
2023-03-03 22:57:14 -0600

    * Initial updates for HDF 4.2.16 release March 2023
    Set version to 4.2.16
    
    Switch AM_MAINTAINER_MODE to disabled with bin/switch_maint_mode
    Commit Makefile.ins and other files generated by autogen.sh for release
    except 2 ...~ files.

--- hdf/src/hfile.h
+++ hdf/src/hfile.h
@@ -49,9 +49,9 @@
 #define LIBVER_MAJOR      4
 #define LIBVER_MINOR      2
 #define LIBVER_RELEASE    16
-#define LIBVER_SUBRELEASE "1" /* For pre-releases like snap0       */
+#define LIBVER_SUBRELEASE "" /* For pre-releases like snap0       */
                               /* Empty string for real releases.           */
-#define LIBVER_STRING "HDF Version 4.2 Release 16-1, March 3, 2023"
+#define LIBVER_STRING "HDF Version 4.2 Release 16, March 3, 2023"
 #define LIBVSTR_LEN   80 /* length of version string  */
 #define LIBVER_LEN    92 /* 4+4+4+80 = 92 */
 /* end of version tags */
