2311d4fde4c37b8c9ad76ecb717a6f9ffdd6f357
Dana Robinson <43805+derobins@users.noreply.github.com>
2023-06-19 10:13:25 -0700

    Fix all gcc warnings in hrepack (#383)

--- mfhdf/hrepack/hrepack_vs.c
+++ mfhdf/hrepack/hrepack_vs.c
@@ -40,18 +40,32 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
         char      *path_name,                         /* absolute path for input group name */
         options_t *options, list_table_t *list_tbl, int is_lone)
 {
-    int32 vdata_id, /* vdata identifier */
-        vdata_out,  /* vdata identifier */
-        vdata_ref,  /* reference number of the vdata */
-        n_records,  /* number of records */
+    int32 vdata_id  = FAIL; /* vdata identifier */
+    int32 vdata_out = FAIL; /* vdata identifier */
+    int32 vdata_ref,        /* reference number of the vdata */
+        n_records,          /* number of records */
         vdata_size, interlace_mode, field_type, field_order;
     int    n_fields, n_attrs;
-    char   vdata_name[VSNAMELENMAX], vdata_class[VSNAMELENMAX];
-    char   fieldname_list[VSFIELDMAX * FIELDNAMELENMAX];
-    char  *path = NULL;
+    char  *vdata_name     = NULL;
+    char  *vdata_class    = NULL;
+    char  *fieldname_list = NULL;
+    char  *path           = NULL;
     char  *field_name;
     uint8 *buf = NULL;
-    int    i, j, ret = 1;
+    int    ret = 0;
+
+    if (NULL == (vdata_name = calloc(1, VSNAMELENMAX))) {
+        ret = -1;
+        goto out;
+    }
+    if (NULL == (vdata_class = calloc(1, VSNAMELENMAX))) {
+        ret = -1;
+        goto out;
+    }
+    if (NULL == (fieldname_list = calloc(1, VSFIELDMAX * FIELDNAMELENMAX))) {
+        ret = -1;
+        goto out;
+    }
 
     /*-------------------------------------------------------------------------
      * attach the vdata, gets its name and class
@@ -60,15 +74,18 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
 
     if ((vdata_id = VSattach(infile_id, ref, "r")) == FAIL) {
         printf("Failed to attach vdata ref %d\n", ref);
-        return -1;
+        ret = -1;
+        goto out;
     }
     if (VSgetname(vdata_id, vdata_name) == FAIL) {
         printf("Failed to name for vdata ref %d\n", ref);
-        return -1;
+        ret = -1;
+        goto out;
     }
     if (VSgetclass(vdata_id, vdata_class) == FAIL) {
         printf("Failed to name for vdata ref %d\n", ref);
-        return -1;
+        ret = -1;
+        goto out;
     }
 
     /* ignore reserved HDF groups/vdatas; they are lone ones */
@@ -76,7 +93,8 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
         if (is_reserved(vdata_class)) {
             if (VSdetach(vdata_id) == FAIL)
                 printf("Failed to detach vdata <%s>\n", path_name);
-            return 0;
+            ret = 0;
+            goto out;
         }
     }
 
@@ -92,10 +110,14 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
 
     /* check inspection mode */
     if (options->trip == 0) {
-        if (VSdetach(vdata_id) == FAIL)
+        if (VSdetach(vdata_id) == FAIL) {
             printf("Failed to detach vdata <%s>\n", path_name);
-        free(path);
-        return 0;
+            ret = -1;
+            goto out;
+        }
+        vdata_id = FAIL;
+        ret      = 0;
+        goto out;
     }
 
     /*-------------------------------------------------------------------------
@@ -105,8 +127,8 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
 
     if (VSinquire(vdata_id, &n_records, &interlace_mode, fieldname_list, &vdata_size, vdata_name) == FAIL) {
         printf("Failed to get info for vdata ref %d\n", ref);
-        free(path);
-        return -1;
+        ret = -1;
+        goto out;
     }
 
     /*-------------------------------------------------------------------------
@@ -118,8 +140,8 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
     if ((vdata_out = VSattach(outfile_id, -1, "w")) == FAIL) {
         printf("Failed to create new VS <%s>\n", path);
         VSdetach(vdata_id);
-        free(path);
-        return -1;
+        ret = -1;
+        goto out;
     }
     if (VSsetname(vdata_out, vdata_name) == FAIL) {
         printf("Failed to set name for new VS <%s>\n", path);
@@ -148,7 +170,7 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
         goto out;
     }
 
-    for (i = 0; i < n_fields; i++) {
+    for (int i = 0; i < n_fields; i++) {
         field_name  = VFfieldname(vdata_id, i);
         field_type  = VFfieldtype(vdata_id, i);
         field_order = VFfieldorder(vdata_id, i);
@@ -205,7 +227,7 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
         ret = -1;
         goto out;
     }
-    for (i = 0; i < n_attrs; i++) {
+    for (int i = 0; i < n_attrs; i++) {
         copy_vdata_attribute(vdata_id, vdata_out, -1, i);
     }
 
@@ -214,13 +236,13 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
      *-------------------------------------------------------------------------
      */
 
-    for (i = 0; i < n_fields; i++) {
+    for (int i = 0; i < n_fields; i++) {
         if ((n_attrs = VSfnattrs(vdata_id, i)) == FAIL) {
             printf("Failed getting fields for VS <%s>\n", path);
             ret = -1;
             goto out;
         }
-        for (j = 0; j < n_attrs; j++) {
+        for (int j = 0; j < n_attrs; j++) {
             copy_vdata_attribute(vdata_id, vdata_out, i, j);
         }
     }
@@ -253,10 +275,18 @@ copy_vs(int32 infile_id, int32 outfile_id, int32 tag, /* tag of input VS */
 
 out:
     /* terminate access to the VSs */
-    if (VSdetach(vdata_id) == FAIL || VSdetach(vdata_out) == FAIL) {
+    if (FAIL != vdata_id && VSdetach(vdata_id)) {
         printf("Could not detach VG in <%s>\n", path);
+        ret = -1;
+    }
+    if (FAIL != vdata_out && VSdetach(vdata_out) == FAIL) {
+        printf("Could not detach VG in <%s>\n", path);
+        ret = -1;
     }
 
+    free(vdata_name);
+    free(vdata_class);
+    free(fieldname_list);
     free(path);
     free(buf);
 
