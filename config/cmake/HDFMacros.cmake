c57fd0612f1434460d234c61f5e7980a87020e01
Allen Byrne <byrn@hdfgroup.org>
2024-01-31 08:28:14 -0600

    Correct HDF5 reference

--- config/cmake/HDFMacros.cmake
+++ config/cmake/HDFMacros.cmake
@@ -36,12 +36,12 @@ macro (SET_HDF_OUTPUT_DIRS package_prefix)
 
   if (NOT ${package_prefix}_EXTERNALLY_CONFIGURED AND CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     if (CMAKE_HOST_UNIX)
-      set (CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}/HDF_Group/${HDF5_PACKAGE_NAME}/${HDF5_PACKAGE_VERSION}"
+      set (CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}/HDF_Group/${HDF4_PACKAGE_NAME}/${HDF4_PACKAGE_VERSION}"
         CACHE PATH "Install path prefix, prepended onto install directories." FORCE)
     else ()
       GetDefaultWindowsPrefixBase(CMAKE_GENERIC_PROGRAM_FILES)
       set (CMAKE_INSTALL_PREFIX
-        "${CMAKE_GENERIC_PROGRAM_FILES}/HDF_Group/${HDF5_PACKAGE_NAME}/${HDF5_PACKAGE_VERSION}"
+        "${CMAKE_GENERIC_PROGRAM_FILES}/HDF_Group/${HDF4_PACKAGE_NAME}/${HDF4_PACKAGE_VERSION}"
         CACHE PATH "Install path prefix, prepended onto install directories." FORCE)
       set (CMAKE_GENERIC_PROGRAM_FILES)
     endif ()
