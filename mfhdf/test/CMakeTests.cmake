94b4b4de3d81f542a567fcf0a0e28ef40f2d555b
Dana Robinson <derobins@hdfgroup.org>
2024-02-02 22:12:20 -0800

    Remove xdrstdio and xdrtest
    xdrtest.c relies on xdrstdio and I want to unify xdr.c and xdrposix.c,
    so out they go...

--- mfhdf/test/CMakeTests.cmake
+++ mfhdf/test/CMakeTests.cmake
@@ -130,28 +130,3 @@ set_tests_properties (MFHDF_TEST-hdfnctest PROPERTIES
     DEPENDS MFHDF_TEST-cdftest
     LABELS ${PROJECT_NAME}
 )
-
-if (MSVC_VERSION LESS 1900)
-  HDFTEST_COPY_FILE("${HDF4_MFHDF_TEST_DIR}/xdrtest.out" "${PROJECT_BINARY_DIR}/TEST/xdrtest.out" "xdrtest_files")
-else ()
-  HDFTEST_COPY_FILE("${HDF4_MFHDF_TEST_DIR}/xdrtest.cyg" "${PROJECT_BINARY_DIR}/TEST/xdrtest.out" "xdrtest_files")
-endif ()
-add_custom_target(xdrtest_files ALL COMMENT "Copying files needed by xdrtest tests" DEPENDS ${xdrtest_files_list})
-
-if (HDF4_ENABLE_USING_MEMCHECKER)
-  add_test (NAME MFHDF_TEST-xdrtest COMMAND ${CMAKE_CROSSCOMPILING_EMULATOR} $<TARGET_FILE:xdrtest>)
-else ()
-  add_test (
-      NAME MFHDF_TEST-xdrtest
-      COMMAND "${CMAKE_COMMAND}"
-          -D "TEST_EMULATOR=${CMAKE_CROSSCOMPILING_EMULATOR}"
-          -D "TEST_PROGRAM=$<TARGET_FILE:xdrtest>"
-          -D "TEST_ARGS:STRING="
-          -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/TEST"
-          -D "TEST_OUTPUT=xdrtest.tst"
-          -D "TEST_EXPECT=0"
-          -D "TEST_REFERENCE=xdrtest.out"
-          -P "${HDF_RESOURCES_DIR}/runTest.cmake"
-    )
-endif ()
-set_tests_properties (MFHDF_TEST-xdrtest PROPERTIES DEPENDS hdfnctest LABELS ${PROJECT_NAME})
