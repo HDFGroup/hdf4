6e7c049866fb742c35c742b81cfe22cd14a472a8
Dana Robinson <43805+derobins@users.noreply.github.com>
2024-02-22 05:49:18 -0800

    Move version symbols to hfile.h (#583)
    - Most of hfile.h moved to hfile_priv.h
    
      Still in hfile.h:
    
      LIBVER_MAJOR
      LIBVER_MINOR
      LIBVER_RELEASE
      LIBVER_SUBRELEASE
      LIBVER_STRING
      LIBVSTR_LEN
      LIBVER_LEN
    
      Everything else has been moved to hfile_priv.h
    
      NOTE that hfile.h is NOT included via hdf.h and must be included
      separately, which is the same behavior as HDF 4.2.x.

---
+++ hdf/src/hfile.h
@@ -0,0 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF.  The full HDF copyright notice, including       *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the root of the source code       *
+ * distribution tree, or in https://support.hdfgroup.org/ftp/HDF/releases/.  *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/*  hfile.h
+ *  Header for hfile.c, routines for low level data element I/O
+ */
+
+#ifndef H4_HFILE_H
+#define H4_HFILE_H
+
+#include "hdf.h"
+
+/* ----------------------------- Version Tags ----------------------------- */
+/* Library version numbers */
+
+#define LIBVER_MAJOR      4
+#define LIBVER_MINOR      3
+#define LIBVER_RELEASE    0
+#define LIBVER_SUBRELEASE "1" /* For pre-releases like snap0 */
+                              /* Empty string for real releases */
+#define LIBVER_STRING "HDF Version 4.3 Release 0-1, February 5, 2024"
+#define LIBVSTR_LEN   80 /* Length of version string */
+#define LIBVER_LEN    92 /* 4+4+4+80 = 92 */
+/* end of version tags */
+
+#endif /* H4_HFILE_H */
--- hdf/src/hfile_priv.h
+++ hdf/src/hfile_priv.h
@@ -20,6 +20,8 @@
 
 #include "hdf_priv.h"
 
+#include "hfile.h"
+
 #include "tbbt_priv.h"
 #include "bitvect_priv.h"
 #include "atom_priv.h"
@@ -50,19 +52,6 @@
 #define DISKBLOCK_TSIZE sizeof(diskblock_tail)
 
 #endif
-
-/* ----------------------------- Version Tags ----------------------------- */
-/* Library version numbers */
-
-#define LIBVER_MAJOR      4
-#define LIBVER_MINOR      3
-#define LIBVER_RELEASE    0
-#define LIBVER_SUBRELEASE "1" /* For pre-releases like snap0 */
-                              /* Empty string for real releases */
-#define LIBVER_STRING "HDF Version 4.3 Release 0-1, February 5, 2024"
-#define LIBVSTR_LEN   80 /* Length of version string */
-#define LIBVER_LEN    92 /* 4+4+4+80 = 92 */
-/* end of version tags */
 
 /* -------------------------- File I/O Functions -------------------------- */
 /* FILELIB -- file library to use for file access: 1 stdio, 2 fcntl
