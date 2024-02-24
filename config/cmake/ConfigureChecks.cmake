f5bc8f140aa4547e0bd84207e8acc67060e9e4d3
Dana Robinson <derobins@hdfgroup.org>
2024-02-24 08:08:11 -0800

    Restore math library

--- config/cmake/ConfigureChecks.cmake
+++ config/cmake/ConfigureChecks.cmake
@@ -65,6 +65,7 @@ endif ()
 
 if (WINDOWS)
   set (CMAKE_REQUIRED_LIBRARIES "ws2_32.lib;wsock32.lib")
+  set (${HDF_PREFIX}_HAVE_LIBM 1)
   set (${HDF_PREFIX}_HAVE_WIN32_API 1)
   set (${HDF_PREFIX}_HAVE_SYSTEM 1)
   if (NOT UNIX AND NOT CYGWIN)
@@ -114,6 +115,8 @@ endif ()
 #  Library checks
 #-----------------------------------------------------------------------------
 if (MINGW OR NOT WINDOWS)
+  # MSVC does not need to link to the math library
+  CHECK_LIBRARY_EXISTS_CONCAT ("m" ceil     ${HDF_PREFIX}_HAVE_LIBM)
   CHECK_LIBRARY_EXISTS_CONCAT ("ws2_32" WSAStartup  ${HDF_PREFIX}_HAVE_LIBWS2_32)
   CHECK_LIBRARY_EXISTS_CONCAT ("wsock32" gethostbyname ${HDF_PREFIX}_HAVE_LIBWSOCK32)
 endif ()
