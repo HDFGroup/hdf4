4f0a9e6f4fee5d2dbdefa6a165452fb31533b262
Binh-Minh Ribler <bmribler@hdfgroup.org>
2024-02-26 15:50:00 -0600

    Fixed invalid URLs
    Mostly licenses, download, documentation, dues to the old portal being
    unavailable.

--- java/test/TestH4HCparams.java
+++ java/test/TestH4HCparams.java
@@ -7,8 +7,8 @@
  * notice, including terms governing use, modification, and redistribution,  *
  * is contained in the file, COPYING.  COPYING can be found at the root of   *
  * the source code distribution tree. You can also access it online  at      *
- * http://www.hdfgroup.org/products/licenses.html.  If you do not have       *
- * access to the file, you may request a copy from help@hdfgroup.org.        *
+ * https://www.hdfgroup.org/licenses.  If you do not have access to the      *
+ * file, you may request a copy from help@hdfgroup.org.                      *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 package test;
