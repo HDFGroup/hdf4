2400062b3d2f76006c9617d1cd1749a1568066bc
Allen Byrne <byrn@hdfgroup.org>
2024-02-26 14:44:30 -0600

    Update config with nvhpc compile flags

--- config/cmake/HDF4UseFortran.cmake
+++ config/cmake/HDF4UseFortran.cmake
@@ -1,27 +1,7 @@
 #
-# This file provides functions for Fortran support.
+# This file provides functions for additional Fortran support.
 #
 #-------------------------------------------------------------------------------
-enable_language (Fortran)
-
-#-----------------------------------------------------------------------------
-# Detect name mangling convention used between Fortran and C
-#-----------------------------------------------------------------------------
-include (FortranCInterface)
-FortranCInterface_HEADER (
-    ${CMAKE_BINARY_DIR}/F77Mangle.h
-    MACRO_NAMESPACE "H4_F77_"
-    SYMBOL_NAMESPACE "H4_F77_"
-    SYMBOLS mysub mymod:my_sub
-)
-
-file (STRINGS ${CMAKE_BINARY_DIR}/F77Mangle.h CONTENTS REGEX "H4_F77_GLOBAL\\(.*,.*\\) +(.*)")
-string (REGEX MATCH "H4_F77_GLOBAL\\(.*,.*\\) +(.*)" RESULT ${CONTENTS})
-set (H4_F77_FUNC "H4_F77_FUNC(name,NAME) ${CMAKE_MATCH_1}")
-
-file (STRINGS ${CMAKE_BINARY_DIR}/F77Mangle.h CONTENTS REGEX "H4_F77_GLOBAL_\\(.*,.*\\) +(.*)")
-string (REGEX MATCH "H4_F77_GLOBAL_\\(.*,.*\\) +(.*)" RESULT ${CONTENTS})
-set (H4_F77_FUNC_ "H4_F77_FUNC_(name,NAME) ${CMAKE_MATCH_1}")
 
 include (${HDF_RESOURCES_DIR}/HDFUseFortran.cmake)
 
