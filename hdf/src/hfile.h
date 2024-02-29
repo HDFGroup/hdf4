7b0b5960c809f75586b4f23de12bdec073836f98
Larry Knox <lrknox@hdfgroup.org>
2024-02-29 08:16:27 -0600

    Set version to 4.3.0 for HDF 4.3.0 release.

--- hdf/src/hfile.h
+++ hdf/src/hfile.h
@@ -26,9 +26,9 @@
 #define LIBVER_MAJOR      4
 #define LIBVER_MINOR      3
 #define LIBVER_RELEASE    0
-#define LIBVER_SUBRELEASE "2" /* For pre-releases like snap0 */
+#define LIBVER_SUBRELEASE "" /* For pre-releases like snap0 */
                               /* Empty string for real releases */
-#define LIBVER_STRING "HDF Version 4.3 Release 0-2, February 28, 2024"
+#define LIBVER_STRING "HDF Version 4.3 Release 0, February 29, 2024"
 #define LIBVSTR_LEN   80 /* Length of version string */
 #define LIBVER_LEN    92 /* 4+4+4+80 = 92 */
 /* end of version tags */
