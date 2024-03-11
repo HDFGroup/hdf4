9055b673990b2dc3c9248f720f3e8a03f8d86434
Dana Robinson <43805+derobins@users.noreply.github.com>
2024-03-10 20:11:48 -0700

    Re-enable test_sd() in hdftest (#700)
    This test was commented out due to failures many years ago. The failures
    only occurred when running the tests as root. New hacks have been
    added that should allow this test to pass.

--- mfhdf/test/tsd.c
+++ mfhdf/test/tsd.c
@@ -12,35 +12,65 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /****************************************************************************
- * tsd.c - tests SDstart for file with no write permission
+ * tsd.c    Tests SDstart for files with no write permission
+ *
+ * NOTE:    This test is very flaky since it depends on the user's
+ *          permissions. When run as root, for example, the call to
+ *          SDstart() with write permissions will pass.
+ *
  ****************************************************************************/
+
+#ifdef H4_HAVE_UNISTD_H
+#include <unistd.h.>
+#endif
 
 #include "mfhdf.h"
 
 #include "hdftest.h"
 #include "hfile_priv.h"
 
-#define FILE_NAME "sdtest.hdf" /* data file to test ID types */
+/* Data file to test ID types */
+#define FILE_NAME "sdtest.hdf"
 
-extern int
+int
 test_sd()
 {
     int32 fid;
     int   status;
-#if defined H4_HAVE_WIN32_API
+#if defined(H4_HAVE_WIN32_API)
     int mode;
 #else
     mode_t mode;
 #endif
 
     FILE *ff;
-    int   num_errs = 0; /* number of errors so far */
+    int   num_errs = 0; /* Number of errors so far */
 
     /* Output message about test being performed */
     TESTING("SDstart for file with no write permission (tsd.c)");
 
-    /* delete the file just to be sure */
-    unlink(FILE_NAME);
+#ifdef __CYGWIN__
+    /* Default Cygwin permissions will allow SDstart() to open read-only files
+     * for writing, so we'll skip the test.
+     */
+    SKIPPED();
+    return 0;
+#endif
+
+#if defined(H4_HAVE_UNISTD_H) && !defined(__MINGW32__)
+    /* Root users on POSIX systems may have privileges that allow SDstart()
+     * to open read-only files for writing, so we'll skip the test.
+     */
+    uid_t uid = getuid();
+
+    if (uid == 0) {
+        SKIPPED();
+        return 0;
+    }
+#endif
+
+    /* Delete the file just to be sure */
+    remove(FILE_NAME);
 
     /* Create a file */
     fid = SDstart(FILE_NAME, DFACC_CREATE);
@@ -49,7 +79,8 @@ test_sd()
     /* Close the file */
     status = SDend(fid);
     CHECK(status, FAIL, "SDend");
-#if defined H4_HAVE_WIN32_API
+
+#if defined(H4_HAVE_WIN32_API)
     mode = _S_IREAD;
 #else
     mode = S_IRUSR;
@@ -69,7 +100,7 @@ test_sd()
         HI_CLOSE(ff);
     }
 
-#if defined H4_HAVE_WIN32_API
+#if defined(H4_HAVE_WIN32_API)
     mode = _S_IWRITE;
 #else
     mode = S_IWUSR;
