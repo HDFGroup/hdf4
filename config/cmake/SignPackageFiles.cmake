50953eea63309c1ea30e4a0b38531bca6c1feb37
Allen Byrne <byrn@hdfgroup.org>
2024-08-29 16:10:58 -0500

    Use existing vars and cpack requires CPACK prefix

--- config/cmake/SignPackageFiles.cmake
+++ config/cmake/SignPackageFiles.cmake
@@ -1,12 +1,13 @@
 # This script signs the targets for the package
-foreach (target IN LISTS ${HDF4_INSTALL_TARGETS})
+message(STATUS "Signing script for ${CPACK_EXPORT_LIBRARIES} and ${CPACK_EXPORT_UTILS}")
+foreach (target "${CPACK_EXPORT_LIBRARIES}")
     if (WIN32)
         # Sign the targets
         execute_process(COMMAND ENV{SIGNTOOLDIR}/signtool
           sign /v /debug /fd SHA256 /tr http://timestamp.acs.microsoft.com /td SHA256
           /dlib "Microsoft.Trusted.Signing.Client\bin\x64\Azure.CodeSigning.Dlib.dll" /dmdf ${CMAKE_CURRENT_SOURCE_DIR}\credentials.json
           $<TARGET_FILE:${target}>
-          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/packages
+          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/lib
         )
         message(STATUS "Signing the target ${target}")
     elseif (APPLE)
@@ -15,8 +16,36 @@ foreach (target IN LISTS ${HDF4_INSTALL_TARGETS})
           --force --timestamp --options runtime --entitlements ${CMAKE_CURRENT_SOURCE_DIR}/config/cmake/distribution.entitlements 
           --verbose=4 --strict --sign "ENV{SIGNER}"
           $<TARGET_FILE:${target}>
-          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/packages
+          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/lib
         )
         message(STATUS "Signing the target ${target}")
+    else ()
+        set (target_path ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/lib)
+        message(STATUS "Signing the target ${target} using ${target_path}")
+    endif ()
+endforeach ()
+
+foreach (target "${CPACK_EXPORT_UTILS}")
+    if (WIN32)
+        # Sign the targets
+        execute_process(COMMAND ENV{SIGNTOOLDIR}/signtool
+          sign /v /debug /fd SHA256 /tr http://timestamp.acs.microsoft.com /td SHA256
+          /dlib "Microsoft.Trusted.Signing.Client\bin\x64\Azure.CodeSigning.Dlib.dll" /dmdf ${CMAKE_CURRENT_SOURCE_DIR}\credentials.json
+          $<TARGET_FILE:${target}>
+          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/bin
+        )
+        message(STATUS "Signing the target ${target}")
+    elseif (APPLE)
+        # Sign the targets
+        execute_process(COMMAND codesign
+          --force --timestamp --options runtime --entitlements ${CMAKE_CURRENT_SOURCE_DIR}/config/cmake/distribution.entitlements 
+          --verbose=4 --strict --sign "ENV{SIGNER}"
+          $<TARGET_FILE:${target}>
+          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/bin
+        )
+        message(STATUS "Signing the target ${target}")
+    else ()
+        set (target_path ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/bin)
+        message(DEBUG "Signing the target ${target} using ${target_path}")
     endif ()
 endforeach ()
