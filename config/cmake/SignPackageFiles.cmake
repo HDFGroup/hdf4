016deb037ea625e5dbf7fafbd90794db71baddcc
Allen Byrne <byrn@hdfgroup.org>
2024-09-03 14:16:41 -0500

    Update presets with latest and windows libs are in bin folder

--- config/cmake/SignPackageFiles.cmake
+++ config/cmake/SignPackageFiles.cmake
@@ -7,9 +7,9 @@ foreach (target "${CPACK_EXPORT_LIBRARIES}")
           sign /v /debug /fd SHA256 /tr http://timestamp.acs.microsoft.com /td SHA256
           /dlib "Microsoft.Trusted.Signing.Client/bin/x64/Azure.CodeSigning.Dlib.dll" /dmdf ${CMAKE_CURRENT_SOURCE_DIR}/credentials.json
           $<TARGET_FILE:${target}>
-          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/lib
+          WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/bin
         )
-        message(STATUS "Signing the target ${target}")
+        message(STATUS "Signing the target $<TARGET_FILE:${target}>")
     elseif (APPLE)
         # Sign the targets
         execute_process(COMMAND codesign
@@ -18,10 +18,10 @@ foreach (target "${CPACK_EXPORT_LIBRARIES}")
           $<TARGET_FILE:${target}>
           WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/lib
         )
-        message(STATUS "Signing the target ${target}")
+        message(STATUS "Signing the target $<TARGET_FILE:${target}>")
     else ()
         set (target_path ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/lib)
-        message(STATUS "Signing the target ${target} using ${target_path}")
+        message(STATUS "Signing the target $<TARGET_FILE:${target}> using ${target_path}")
     endif ()
 endforeach ()
 
@@ -34,7 +34,7 @@ foreach (target "${CPACK_EXPORT_UTILS}")
           $<TARGET_FILE:${target}>
           WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/bin
         )
-        message(STATUS "Signing the target ${target}")
+        message(STATUS "Signing the target $<TARGET_FILE:${target}>")
     elseif (APPLE)
         # Sign the targets
         execute_process(COMMAND codesign
@@ -43,9 +43,9 @@ foreach (target "${CPACK_EXPORT_UTILS}")
           $<TARGET_FILE:${target}>
           WORKING_DIRECTORY ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/bin
         )
-        message(STATUS "Signing the target ${target}")
+        message(STATUS "Signing the target $<TARGET_FILE:${target}>")
     else ()
         set (target_path ${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_COMPONENTS_IN_ONE/${CPACK_PACKAGE_INSTALL_DIRECTORY}/bin)
-        message(DEBUG "Signing the target ${target} using ${target_path}")
+        message(DEBUG "Signing the target $<TARGET_FILE:${target}> using ${target_path}")
     endif ()
 endforeach ()
